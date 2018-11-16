6d90dad721293538c83badbf28e41c01d8db31de
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-16 13:17:32 -0500

    r XMmsMessageType to use simpler return

--- lib/mm1/x_mms_message_type.ex
+++ lib/mm1/x_mms_message_type.ex
@@ -1,7 +1,7 @@
 defmodule MM1.XMmsMessageType do
   use MM1.BaseCodec
 
-  def decode <<0x8c, 0x80, rest::binary>> do
-    return %MM1.Result{bytes: <<0x8c, 0x80>>, value: :m_send_req, rest: rest}
+  def decode <<0x8c, 0x80, _::binary>> = bytes do
+    return :m_send_req, 2, bytes
   end
 end
