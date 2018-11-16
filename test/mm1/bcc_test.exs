e9b6e277cd0d39293ab85deea3241f0bea4eaa47
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-16 15:39:40 -0500

    t support focus tests

---
+++ test/mm1/bcc_test.exs
@@ -0,0 +1,36 @@
+defmodule MM1.HeadersTest do
+  use ExUnit.Case
+
+  import MM1.Headers
+  alias MM1.{Result, Headers, XMmsMessageType, Bcc}
+
+  describe "decode" do
+    test "should return a Headers Result" do
+      assert %Result{module: Headers} = decode <<>>
+    end
+
+    test "should decode Bcc" do
+      assert %{value: [%{module: MM1.Bcc}]} = decode <<129, 0>>
+    end
+
+    test "should decode XMmsMessageType" do
+      assert %{value: [%{module: MM1.XMmsMessageType}]} = decode <<140, 128>>
+    end
+
+    test "value should be an array of Headers" do
+      assert %{
+               value: [
+                 %{module: XMmsMessageType},
+                 %{module: Bcc},
+               ]
+             } = decode <<140, 128, 129, 0>>
+    end
+  end
+
+  describe "encode" do
+    @tag :skip
+    test "should encode Bcc" do
+      assert <<129, 0>> == encode 0
+    end
+  end
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,4 @@
-ExUnit.start()
+ExUnit.start exclude: [:skip], include: [
+  # Uncomment the line below to only run tests with: # @tag :focus"
+#     :focus], exclude: [:test
+  ]
