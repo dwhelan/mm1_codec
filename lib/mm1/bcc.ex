77c9515176ac873d21b12f66713da04ea75366b8
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-16 15:56:45 -0500

    F Bcc.encode/1

--- lib/mm1/bcc.ex
+++ lib/mm1/bcc.ex
@@ -4,4 +4,8 @@ defmodule MM1.Bcc do
   def decode <<129, 0, _::binary>>  = bytes do
     value 0, 2, bytes
   end
+
+  def encode value do
+    <<129, value>>
+  end
 end
--- test/mm1/bcc_test.exs
+++ test/mm1/bcc_test.exs
@@ -1,19 +1,18 @@
 defmodule MM1.BccTest do
   use ExUnit.Case
 
-  import MM1.Bcc
   alias MM1.{Result, Bcc}
+  import Bcc
 
   describe "decode" do
-    test "should return a Bcc" do
-      assert %Result{module: Bcc, value: 0} = decode <<129, 0>>
+    test "should decode" do
+      assert %Result{module: Bcc, value: 0, bytes: <<129, 0>>, rest: <<"rest">> } = decode <<129, 0, "rest">>
     end
   end
 
-#  describe "encode" do
-#    @tag :skip
-#    test "should encode Bcc" do
-#      assert <<129, 0>> == encode 0
-#    end
-#  end
+  describe "encode" do
+    test "should encode" do
+      assert <<129, 0>> == encode 0
+    end
+  end
 end
