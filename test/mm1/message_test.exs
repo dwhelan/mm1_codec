585cdf98b81a5aed70da6e338a5bb90e2d3a1a9c
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-18 00:07:08 -0500

    r made Message decode tests more focused

--- test/mm1/message_test.exs
+++ test/mm1/message_test.exs
@@ -1,14 +1,31 @@
 defmodule MM1.MessageTest do
   use ExUnit.Case
 
-  alias MM1.{Result, Message}
+  alias MM1.{Result, Message, Headers}
   import Message
 
-  test "result should be an MMS Message" do
-    assert %Result{module: Message} = decode <<>>
+  describe "decode" do
+    test "should return a Result" do
+      assert %Result{} = decode <<>>
+    end
+
+    test "module should be a Message" do
+      assert %{module: Message} = decode <<>>
+    end
+
+    test "value should be a Headers Result" do
+      headers_result = Headers.decode <<140, 0, 129, 0>>
+      assert %{value: headers_result} = decode <<140, 0, 129, 0>>
+    end
+
+    test "rest should be a zero length binary" do
+      assert %{rest: <<>>} = decode <<140, 0, 129, 0>>
+    end
   end
 
-  test "encode" do
-    assert <<0>> == encode 0
+  describe "encode" do
+    test "encode" do
+      assert <<0>> == encode 0
+    end
   end
 end
