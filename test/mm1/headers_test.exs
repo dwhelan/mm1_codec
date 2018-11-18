5113f7dc8b30de84a72b9ef9c15d20167950c8fa
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-18 17:24:22 -0500

    r simplified encode and decode tests

--- test/mm1/headers_test.exs
+++ test/mm1/headers_test.exs
@@ -5,39 +5,31 @@ defmodule MM1.HeadersTest do
   alias MM1.{Result, Headers}
   alias MM1.{Bcc, BccTest}
 
+  def bytes do
+    BccTest.bytes() <> BccTest.bytes()
+  end
+
+  def result do
+    %Result{
+      module: Headers,
+      bytes:  <<>>,
+      rest:   <<>>,
+      value:  [
+        %Result{BccTest.result | rest: BccTest.bytes()},
+        BccTest.result()
+      ]}
+  end
+
   describe "octet" do
     test "Bcc",             do: assert 129 == octet Bcc
     test "XMmsMessageType", do: assert 140 == octet XMmsMessageType
   end
 
-  describe "decode" do
-    test "module should be Headers" do
-      assert decode(<<>>).module == Headers
-    end
-
-    test "bytes should be <<>>" do
-      assert decode(<<>>).bytes == <<>>
-      assert decode(BccTest.bytes).bytes == <<>>
-    end
-
-    test "value with one header" do
-      assert decode(BccTest.bytes).value == [BccTest.result]
-    end
-
-    test "value with multiple headers" do
-      [bcc1, bcc2] = decode(BccTest.bytes <> BccTest.bytes).value
-      assert bcc1 == %Result{BccTest.result | rest: BccTest.bytes()}
-      assert bcc2 == BccTest.result
-    end
+  test "decode" do
+    assert decode(bytes()) === result()
   end
 
-  describe "encode" do
-    test "one header" do
-      assert encode(%{value: [BccTest.result]}) == BccTest.bytes
-    end
-
-    test "multiple headers" do
-      assert encode(%{module: Headers, value: [BccTest.result, BccTest.result]}) == BccTest.bytes() <> BccTest.bytes()
-    end
+  test "encode" do
+    assert encode(result()) === bytes()
   end
 end
--- test/mm1/message_test.exs
+++ test/mm1/message_test.exs
@@ -1,41 +1,27 @@
 defmodule MM1.MessageTest do
   use ExUnit.Case
 
-  alias MM1.{Result, Message, Headers}
+  alias MM1.{Result, Message, Headers, HeadersTest}
   import Message
 
-  describe "decode" do
-    test "should return a Result" do
-      assert %Result{} = decode <<>>
-    end
-
-    test "module should be a Message" do
-      assert %{module: Message} = decode <<>>
-    end
-
-    test "value should be a Headers Result" do
-      result = decode <<140, 0, 129, 0>>
-      headers_result = Headers.decode <<140, 0, 129, 0>>
-      assert result.value == headers_result
-    end
-
-    test "rest should be a zero length binary" do
-      assert %{rest: <<>>} = decode <<140, 0, 129, 0>>
-    end
+  def bytes do
+    HeadersTest.bytes()
   end
 
-  describe "encode" do
-    test "encode" do
-      result = encode %{
-        value: %{
-          value: [
-            %{module: MM1.XMmsMessageType, bytes: <<140, 0>>},
-            %{module: MM1.Bcc,             bytes: <<129, 0>>}
-          ]
-        }
+  def result do
+    %Result{
+      module: Message,
+      bytes:  <<>>,
+      rest:   <<>>,
+      value:  HeadersTest.result()
       }
+  end
 
-      assert <<140, 0, 129, 0>> == result
-    end
+  test "decode" do
+    assert decode(bytes()) === result()
+  end
+
+  test "encode" do
+    assert encode(result()) === bytes()
   end
 end
