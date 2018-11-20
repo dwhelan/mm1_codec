7b4212fa49bc00304db09a7688be3393cc10d98d
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-20 02:01:24 -0500

    r added @short_integer_zero

--- lib/wap/short_integer_map.ex
+++ lib/wap/short_integer_map.ex
@@ -1,7 +1,8 @@
 defmodule WAP.ShortIntegerMap do
+  @short_integer_zero 128
 
-  def map(byte, values) when byte >= 128 and byte < 128 + tuple_size(values) do
-    elem values, byte - 128
+  def map(byte, values) when byte >= @short_integer_zero and byte < @short_integer_zero + tuple_size(values) do
+    elem values, byte - @short_integer_zero
   end
 
   def map _, _ do
