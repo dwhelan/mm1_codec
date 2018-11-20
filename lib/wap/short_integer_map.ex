784e322ca73e5c038b9e2a44b0eaaecbbf8a116b
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-20 02:03:05 -0500

    r ranmed byte -> short_integer

--- lib/wap/short_integer_map.ex
+++ lib/wap/short_integer_map.ex
@@ -1,8 +1,8 @@
 defmodule WAP.ShortIntegerMap do
   @short_integer_zero 128
 
-  def map(byte, values) when byte >= @short_integer_zero and byte < @short_integer_zero + tuple_size(values) do
-    elem values, byte - @short_integer_zero
+  def map(short_integer, values) when short_integer >= @short_integer_zero and short_integer < @short_integer_zero + tuple_size(values) do
+    elem values, short_integer - @short_integer_zero
   end
 
   def map _, _ do
