10851bf92fad761897f6c9e2d44e5fb0498d943d
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-21 00:04:01 -0500

    f multi-byte uintvars

--- lib/wap/uintvar.ex
+++ lib/wap/uintvar.ex
@@ -1,7 +1,20 @@
 defmodule WAP.Uintvar do
   use MM1.BaseCodec
+  use Bitwise
 
-  def decode <<byte, rest::binary>> do
-    value byte, <<byte>>, rest
+  def decode(bytes) when is_binary(bytes) do
+    decode bytes, 0, <<>>
+  end
+
+  defp decode(<<byte, rest::binary>>, value, bytes) when byte >= 128 do
+    decode rest, add(byte-128, value), bytes <> <<byte>>
+  end
+
+  defp decode <<byte, rest::binary>>, value, bytes do
+    value add(byte, value), bytes <> <<byte>>, rest
+  end
+
+  defp add byte, value do
+    (value <<< 7) + byte
   end
 end
--- test/wap/uintvar_test.exs
+++ test/wap/uintvar_test.exs
@@ -14,8 +14,16 @@ defmodule WAP.UintvarTest do
   end
 
   describe "encode" do
-    test "one byte" do
-      assert decode(<<127>>).value === 127
-    end
+    test "one byte max",    do: assert decode(<<127>>).value === 127
+    test "two bytes min",   do: assert decode(<<129, 0>>).value === 128
+    test "two bytes max",   do: assert decode(<<255, 127>>).value === 16_383
+    test "three bytes min", do: assert decode(<<129, 128, 0>>).value === 16_384
+    test "three bytes max", do: assert decode(<<255, 255, 127>>).value === 2_097_151
+    test "four bytes min",  do: assert decode(<<129, 128, 128, 0>>).value === 2_097_152
+    test "four bytes max",  do: assert decode(<<255, 255, 255, 127>>).value === 268_435_455
+    test "five bytes min",  do: assert decode(<<129, 128, 128, 128, 0>>).value === 268_435_456
+    test "five bytes max",  do: assert decode(<<255, 255, 255, 255, 127>>).value === 34_359_738_367
+
+    test "uint32 max", do: assert decode(<<143, 255, 255, 255, 127>>).value === 0xffffffff
   end
 end
