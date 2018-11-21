8acc798920da1778a078e15c04d5f8c6386f2781
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-20 21:40:54 -0500

    r EncodedStringTest to use MM1.CodecTet

--- test/wap/encoded_string_test.exs
+++ test/wap/encoded_string_test.exs
@@ -1,20 +1,20 @@
 defmodule WAP.EncodedStringTest do
-  use ExUnit.Case
 
   alias WAP.EncodedString
   alias MM1.Result
-
   import EncodedString
 
-  describe "decode" do
-    test "unknown encoding", do: assert decode(<<6, 129, "text", 0, "rest">>) === %Result{module: EncodedString, value: %{charset: {:other, 1}, text: "text"}, bytes: <<6, 129, "text", 0>>, rest: <<"rest">> }
+  use MM1.CodecTest
 
-    test "no bytes",      do: assert %{value: {:err, :insufficient_bytes}, bytes: <<>>,       rest: <<>> } = decode <<>>
-    test "no terminator", do: assert %{value: {:err, :insufficient_bytes}, bytes: <<"text">>, rest: <<>> } = decode <<"text">>
+  def bytes do
+    <<6, 129, "text", 0, "rest">>
   end
 
-#  describe "encode" do
-#    test   "0", do: assert encode(%{bytes: <<  0>>}) === <<  0>>
-#    test "255", do: assert encode(%{bytes: <<255>>}) === <<255>>
-#  end
+  def result do
+    %Result{module: EncodedString, value: %{charset: {:other, 1}, text: "text"}, bytes: <<6, 129, "text", 0>>, rest: <<"rest">> }
+  end
+
+  describe "decode" do
+    test "no terminator", do: assert %{value: {:err, :insufficient_bytes}, bytes: <<"text">>, rest: <<>> } = decode <<"text">>
+  end
 end
