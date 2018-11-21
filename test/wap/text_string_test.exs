4413c265df1d205045147be77cbf01c45b08c806
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-20 21:25:15 -0500

    t simplified TextStringTest

--- test/wap/text_string_test.exs
+++ test/wap/text_string_test.exs
@@ -1,20 +1,19 @@
 defmodule WAP.TextStringTest do
-  use ExUnit.Case
-
   alias WAP.TextString
   alias MM1.Result
-
   import TextString
 
-  describe "decode" do
-    test "valid text",    do: assert decode(<<"text", 0, "rest">>) === %Result{module: TextString, value: "text", bytes: <<"text", 0>>, rest: <<"rest">> }
+  use MM1.CodecTest
 
-    test "no bytes",      do: assert %{value: {:err, :insufficient_bytes}, bytes: <<>>,       rest: <<>> } = decode <<>>
-    test "no terminator", do: assert %{value: {:err, :insufficient_bytes}, bytes: <<"text">>, rest: <<>> } = decode <<"text">>
+  def bytes do
+    <<"text", 0, "rest">>
   end
 
-#  describe "encode" do
-#    test   "0", do: assert encode(%{bytes: <<  0>>}) === <<  0>>
-#    test "255", do: assert encode(%{bytes: <<255>>}) === <<255>>
-#  end
+  def result do
+    %Result{module: TextString, value: "text", bytes: <<"text", 0>>, rest: <<"rest">> }
+  end
+
+  describe "decode" do
+    test "no terminator", do: assert %{value: {:err, :insufficient_bytes}, bytes: <<"text">>, rest: <<>> } = decode <<"text">>
+  end
 end
