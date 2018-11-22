a10c3c99b2880637cb02e4fda44ffbf9bacf26e9
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-21 20:14:01 -0500

    f initial cut at CharSet

---
+++ lib/wap/char_set.ex
@@ -0,0 +1,23 @@
+defmodule WAP.CharSet do
+  @moduledoc """
+  Specification: WAP-230-WSP-20010705-a, 7.2.9 Encoded-string-value
+
+  The Char-set values are registered by IANA as MIBEnum values.
+  """
+
+  use MM1.BaseCodec
+
+
+  def decode <<code, rest::binary>> do
+#    bytes ~> WAP.Integer ~> WAP.CharSets
+    value :other, <<code>>, rest
+  end
+
+  def size(code) when code < 128 do
+    1
+  end
+
+  def size _ do
+    3
+  end
+end
---
+++ test/wap/char_set_test.exs
@@ -0,0 +1,38 @@
+defmodule WAP.CharSetTest do
+  alias WAP.CharSet
+  alias MM1.Result
+
+  import CharSet
+  use MM1.CodecTest
+#      codec: CharSet,
+#      transforms: [
+#        # As a Short-integer
+#        { <<0x81>>,   [name: :other,  code:   1] },
+#        { <<0xEA>>,   [name: :csUTF8, code: 106] },
+#
+#        # As a Long-integer
+#        { <<2, 0x03, 0xe8>>, [name: :csUnicode, code: 1000] },
+#        { <<2, 0x0b, 0xb8>>, [name: :reserved,  code: 3000] },
+#      ],
+#      decode_errors: [
+#        { <<0>>,  :length_cannot_be_zero  },
+#        { <<31>>, :length_greater_than_30 },
+#      ]
+  def bytes do
+    <<0x81, "rest">>
+  end
+
+  def result do
+    %Result{module: CharSet, value: :other, bytes: <<0x81>>, rest: <<"rest">>}
+  end
+
+  describe "decode" do
+
+  end
+#  test "char_set_size" do
+#    assert CharSet.size(0)      === 1
+#    assert CharSet.size(127)    === 1
+#    assert CharSet.size(128)    === 3
+#    assert CharSet.size(65_535) === 3
+#  end
+end
