bbbd90a5a35eb25b0d8ee58333665ab0bd8a651f
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-21 20:03:05 -0500

    t added tests for up to uint32 max

--- test/wap/value_length_test.exs
+++ test/wap/value_length_test.exs
@@ -13,9 +13,13 @@ defmodule WAP.ValueLengthTest do
     %Result{module: ValueLength, value: 0, bytes: <<0>>, rest: <<"rest">>}
   end
 
-  describe "decode" do
+  describe "decode Short-length" do
     test "0..30 should be valid", do: assert decode(<<30>>).value === 30
-    test "31 (length quote)",     do: assert decode(<<31, 31>>).value === 31
     test "> 31 should not match", do: assert_raise FunctionClauseError, fn -> decode <<32>> end
+  end
+
+  describe "decode Length Quote Length (Uintvar)" do
+    test "31",         do: assert decode(<<31, 31>>).value === 31
+    test "max uint32", do: assert decode(<<31, 143, 255, 255, 255, 127>>).value === 0xffffffff
   end
 end
