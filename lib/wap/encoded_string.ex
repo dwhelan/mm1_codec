8546b55b94c637f451d7cf39813cbe3f1c33ef6f
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-22 00:18:07 -0500

    f EncodedString to chain when charsets present

--- lib/wap/encoded_string.ex
+++ lib/wap/encoded_string.ex
@@ -1,12 +1,23 @@
 defmodule WAP.EncodedString do
   use MM1.BaseCodec
 
-  def decode <<bytes::binary>> do
-    return String.split bytes, <<0>>, parts: 2
+  alias WAP.TextString
+  alias WAP.ValueLength
+
+  def decode(<<char, rest::binary>> = data) when char >= 32 do
+    return TextString.decode data
+  end
+
+  def decode data do
+    value_length = ValueLength.decode data
+    char_set = WAP.CharSet.decode value_length.rest
+    text_string = WAP.TextString.decode char_set.rest
+    value %{charset: :csUTF8, text: text_string.value},
+      value_length.bytes <> char_set.bytes <> text_string.bytes, text_string.rest
   end
 
   defp return [text | [rest]] do
-    value %{charset: {:other, 1}, text: "text"}, text <> <<0>>, rest
+    value %{charset: :other, text: "text"}, text <> <<0>>, rest
   end
 
   defp return [bytes | []] do
--- test/wap/encoded_string_test.exs
+++ test/wap/encoded_string_test.exs
@@ -7,14 +7,19 @@ defmodule WAP.EncodedStringTest do
   use MM1.CodecTest
 
   def bytes do
-    <<6, 129, "text", 0, "rest">>
+    <<"text", 0, "rest">>
   end
 
   def result do
-    %Result{module: EncodedString, value: %{charset: {:other, 1}, text: "text"}, bytes: <<6, 129, "text", 0>>, rest: <<"rest">> }
+    %Result{module: EncodedString, value: "text", bytes: <<"text", 0>>, rest: <<"rest">> }
   end
 
-  describe "decode" do
+  describe "decode Text-string" do
     test "no terminator", do: assert %{value: {:err, :insufficient_bytes}, bytes: <<"text">>, rest: <<>> } = decode <<"text">>
+  end
+
+  describe "decode Value-length Char-set Text-string" do
+    test "single byte charset", do: assert %{value: %{charset: :csUTF8, text: "text"}, bytes: <<3, 0xea, "text", 0>>, rest: <<"rest">>} = decode <<3, 0xea, "text", 0, "rest">>
+#    test "no terminator", do: assert %{value: {:err, :insufficient_bytes}, bytes: <<"text">>, rest: <<>> } = decode <<"text">>
   end
 end
