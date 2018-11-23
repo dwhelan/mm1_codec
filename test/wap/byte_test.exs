007b0d5b2eb1677953357c3d90578e63d50d29a1
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-23 16:12:38 -0500

    r ByteTest to use CodeExamples

--- test/wap/byte_test.exs
+++ test/wap/byte_test.exs
@@ -1,22 +1,9 @@
 defmodule WAP.ByteTest do
-  alias WAP.Byte
-  alias MM1.Result
-  import Byte
+  use ExUnit.Case
+  import MM1.CodecExamples
 
-  use MM1.CodecTest
-
-  def bytes do
-    <<0, "rest">>
-  end
-
-  def result do
-    %Result{module: Byte, value: 0, bytes: <<0>>, rest: <<"rest">>}
-  end
-
-  test "new" do
-    assert Byte.new( -1) === %Result{module: Byte, value: 255, bytes: <<255>>, rest: <<>>}
-    assert Byte.new(  0) === %Result{module: Byte, value:   0, bytes: <<  0>>, rest: <<>>}
-    assert Byte.new(255) === %Result{module: Byte, value: 255, bytes: <<255>>, rest: <<>>}
-    assert Byte.new(256) === %Result{module: Byte, value:   0, bytes: <<  0>>, rest: <<>>}
-  end
+  examples WAP.Byte, [
+    {"one byte min", <<0>>,     0},
+    {"one byte max", <<255>>, 255},
+  ]
 end
