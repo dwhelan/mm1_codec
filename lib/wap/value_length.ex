fcc9781a122106325eefddbf1868349a2d2ef0ca
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-23 21:52:33 -0500

    r ValueLengthTest to use CodeExamples

--- lib/wap/value_length.ex
+++ lib/wap/value_length.ex
@@ -1,14 +1,26 @@
 defmodule WAP.ValueLength do
   use MM1.BaseCodec
+  alias WAP.{ShortLength, Uintvar}
 
   @length_quote 31
 
   def decode(<<length, _::binary>> = bytes) when length <= 30 do
-    return WAP.ShortLength.decode bytes
+    bytes |> ShortLength.decode |> return
   end
 
   def decode <<@length_quote, bytes::binary>> do
-    result = WAP.Uintvar.decode bytes
-    return %Result{result | bytes: <<@length_quote>> <> result.bytes}
+    bytes |> Uintvar.decode |> prepend_length_quote |> return
+  end
+
+  def new(length) when length <= 30 do
+    length |> ShortLength.new |> return
+  end
+
+  def new length do
+    length |> Uintvar.new |> prepend_length_quote |> return
+  end
+
+  defp prepend_length_quote result do
+    %Result{result | bytes: <<@length_quote>> <> result.bytes}
   end
 end
--- test/wap/value_length_test.exs
+++ test/wap/value_length_test.exs
@@ -1,25 +1,17 @@
 defmodule WAP.ValueLengthTest do
+  use ExUnit.Case
+  import MM1.CodecExamples
+
   alias WAP.ValueLength
-  alias MM1.Result
-  import ValueLength
 
-  use MM1.CodecTest
+  examples ValueLength, [
+    {"0",  <<0>>,       0},
+    {"30", <<30>>,     30},
+    {"31", <<31, 31>>, 31},
+    {"32", <<31, 32>>, 32},
 
-  def bytes do
-    <<0, "rest">>
-  end
+    {"max uint32", <<31, 143, 255, 255, 255, 127>>, 0xffffffff},
+  ]
 
-  def result do
-    %Result{module: ValueLength, value: 0, bytes: <<0>>, rest: <<"rest">>}
-  end
-
-  describe "decode Short-length" do
-    test "0..30 should be valid", do: assert decode(<<30>>).value === 30
-    test "> 31 should not match", do: assert_raise FunctionClauseError, fn -> decode <<32>> end
-  end
-
-  describe "decode Length Quote Length (Uintvar)" do
-    test "31",         do: assert decode(<<31, 31>>).value === 31
-    test "max uint32", do: assert decode(<<31, 143, 255, 255, 255, 127>>).value === 0xffffffff
-  end
+  test "> 31 should not match", do: assert_raise FunctionClauseError, fn -> ValueLength.decode <<32>> end
 end
