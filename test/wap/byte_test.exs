c3f3dfe6c1dd414625b2ea4a0896a57bb6f35697
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-24 00:46:52 -0500

    t added tests for Byte.new() to wrap

--- test/wap/byte_test.exs
+++ test/wap/byte_test.exs
@@ -6,4 +6,12 @@ defmodule WAP.ByteTest do
     {<<0>>,     0},
     {<<255>>, 255},
   ]
+
+  test "should wrap numbers > 255" do
+    assert WAP.Byte.new(256).bytes() === <<0>>
+  end
+
+  test "should wrap numbers < 0" do
+    assert WAP.Byte.new(-1).bytes() === <<255>>
+  end
 end
