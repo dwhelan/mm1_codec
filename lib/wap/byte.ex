e9549ac8ce3c8197e3858f2cb75714cf2197f458
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-26 05:05:13 -0500

    r update Byte to use new Result macros

--- lib/wap/byte.ex
+++ lib/wap/byte.ex
@@ -1,15 +1,17 @@
 defmodule WAP.Byte do
   use MM1.BaseCodec
+  import WAP.Guards
+  import MM1.Result
 
   def decode <<value, rest::binary>> do
-    value value, <<value>>, rest
+    decode_ok value, <<value>>, rest
   end
 
-  def new(value) when is_integer(value) and value >= 0 and value <= 255 do
-    value value, <<value>>
+  def new(value) when is_byte(value) do
+    new_ok value, <<value>>
   end
 
   def new value do
-    error :must_be_an_integer_between_0_and_255, value
+    new_error value, :must_be_an_integer_between_0_and_255
   end
 end
--- lib/wap/guards.ex
+++ lib/wap/guards.ex
@@ -1,26 +1,25 @@
 defmodule WAP.Guards do
-  defmacro is_integer(value, min, max) do
+
+  defp is_unsigned_integer value, max do
     quote do
-      is_integer(unquote(value)) and unquote(value) >= unquote(min) and unquote(value) <= unquote(max)
+      is_integer(unquote value) and unquote(value) >= 0 and unquote(value) <= unquote(max)
     end
   end
 
-  defmacro is_short_length(value) do
-    quote do
-      is_integer(unquote(value), 0, 30)
-    end
+  defmacro is_short_length value do
+    is_unsigned_integer value, 30
   end
 
-  defmacro is_short_integer(value) do
-    quote do
-      is_integer(unquote(value), 0, 127)
-    end
+  defmacro is_short_integer value do
+    is_unsigned_integer value, 127
   end
 
-  defmacro is_uintvar(value) do
-    quote do
-      is_integer(unquote(value), 0, 0xffffffff)
-    end
+  defmacro is_byte value do
+    is_unsigned_integer value, 255
+  end
+
+  defmacro is_uintvar value do
+    is_unsigned_integer value, 0xffffffff
   end
 end
 
