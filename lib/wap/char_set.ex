afaf4a2d2adf9d97961b9c466da37d7d9908a0c0
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-25 22:30:18 -0500

    r better CharSet.new errors

--- lib/wap/char_set.ex
+++ lib/wap/char_set.ex
@@ -20,8 +20,24 @@ defmodule WAP.CharSet do
     result |> CharSets.map |> return
   end
 
-  def new name do
-    value name, bytes CharSets.unmap(name)
+  def new(name) when is_atom(name) do
+    _new CharSets.unmap name
+  end
+
+  def new(code) when is_integer(code) and code >= 0 do
+    _new code
+  end
+
+  def new code do
+    error :must_be_an_integer_greater_than_or_equal_to_0, code
+  end
+
+  defp _new(name) when is_atom(name) do
+    error :unknown_char_set, name
+  end
+
+  defp _new code do
+    value CharSets.map(code), bytes(code)
   end
 
   defp bytes(code) when code < 128 do
--- test/wap/char_set_test.exs
+++ test/wap/char_set_test.exs
@@ -3,8 +3,15 @@ defmodule WAP.CharSetTest do
 
   use MM1.CodecExamples, module: WAP.CharSet,
     examples: [
-      {<<0xea>>,          :csUTF8},
+      {<<0xea>>,          :csUTF8   },
       {<<2, 0x03, 0xe8>>, :csUnicode},
-      {<<2, 0x0b, 0xb8>>, :reserved},
+      {<<2, 0x0b, 0xb8>>, :reserved },
+    ],
+
+    new_errors: [
+      {  -1, :must_be_an_integer_greater_than_or_equal_to_0},
+      {1.23, :must_be_an_integer_greater_than_or_equal_to_0},
+      { "x", :must_be_an_integer_greater_than_or_equal_to_0},
+      {:foo, :unknown_char_set}
     ]
 end
