4e81c865ae8a9cf0c65178c1239fb754b3e2e82a
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-25 20:29:36 -0500

    t EncodeStringTest to use CodecExamples

--- test/wap/encoded_string_test.exs
+++ test/wap/encoded_string_test.exs
@@ -1,37 +1,16 @@
 defmodule WAP.EncodedStringTest do
   use ExUnit.Case
-  import MM1.CodecExamples
 
-  alias WAP.EncodedString
-  alias MM1.Result
-
-  examples EncodedString, [
-    {<<0>>,                                ""},
-    {<<"text", 0>>,                        "text"},
-    {<<6, 0xea, "text", 0>>,               { 6, :csUTF8,    "text"}},
-    {<<8, 2, 0x03, 0xe8, "text", 0>>,      { 8, :csUnicode, "text"}},
-    {<<31, 42, 2, 0x03, 0xe8, "text", 0>>, {42, :csUnicode, "text"}},
-  ]
-
-  test "missing terminator with just a TextString" do
-    assert EncodedString.decode(<<"text">>) ==
-      %Result{
-        module: EncodedString,
-        value: "text",
-        err: :missing_terminator,
-        bytes: "text",
-        rest: <<>>
-      }
-  end
-
-  test "missing terminator with an encoded TextString" do
-    assert EncodedString.decode(<<6, 0xea, "text">>) ==
-      %Result{
-        module: EncodedString,
-        value: {6, :csUTF8, "text"},
-        err: :missing_terminator,
-        bytes: <<6, 0xea, "text">>,
-        rest: <<>>
-      }
-  end
+  use MM1.CodecExamples, module: WAP.EncodedString,
+    examples: [
+      {<<0>>,                                ""},
+      {<<"text", 0>>,                        "text"},
+      {<<6, 0xea, "text", 0>>,               { 6, :csUTF8,    "text"}},
+      {<<8, 2, 0x03, 0xe8, "text", 0>>,      { 8, :csUnicode, "text"}},
+      {<<31, 42, 2, 0x03, 0xe8, "text", 0>>, {42, :csUnicode, "text"}},
+    ],
+    decode_errors: [
+      {<<"text">>,          :missing_terminator, "text"},
+      {<<6, 0xea, "text">>, :missing_terminator, {6, :csUTF8, "text"}},
+    ]
 end
