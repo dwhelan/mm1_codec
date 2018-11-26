bfc7d7107a5246c17e71f1fba81ea423ba86b09a
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-26 01:22:15 -0500

    t use CodeExamples

--- test/wap/text_string_test.exs
+++ test/wap/text_string_test.exs
@@ -1,22 +1,13 @@
 defmodule WAP.TextStringTest do
   use ExUnit.Case
-  import MM1.CodecExamples
 
-  alias WAP.TextString
+  use MM1.CodecExamples, module: WAP.TextString,
+    examples: [
+      {<<0>>,             ""},
+      {<<"text", 0>>, "text"},
+    ],
 
-  examples TextString, [
-    {<<0>>, ""},
-    {<<"text", 0>>, "text"},
-  ]
-
-  test "missing terminator" do
-    assert TextString.decode(<<"text">>) ==
-             %MM1.Result{
-               module: TextString,
-               value: "text",
-               err: :missing_terminator,
-               bytes: "text",
-               rest: <<>>
-             }
-  end
+    decode_errors: [
+      {<<"text">>, :missing_terminator, "text"}
+    ]
 end
