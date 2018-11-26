58c467f5fe8cdc76d1517eae919e64a03243e6e1
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-25 20:07:55 -0500

    t simplify use of CodeExamples ... Byte

--- test/shared/codec_examples.ex
+++ test/shared/codec_examples.ex
@@ -62,4 +62,42 @@ defmodule MM1.CodecExamples do
       end)
     end
   end
+
+  defmacro __using__(opts) do
+    quote bind_quoted: [opts: opts] do
+      @module opts[:module]
+      alias MM1.Result
+
+      test "insufficient bytes" do
+        assert @module.decode(<<>>) === %Result{module: @module, err: :insufficient_bytes}
+      end
+
+      Enum.each(opts[:examples], fn {bytes, value} ->
+        @bytes  bytes
+        @value  value
+        @result %Result{module: @module, value: value, bytes: bytes}
+
+        test "decode(#{inspect bytes}) == #{inspect value}" do
+          assert @module.decode(@bytes <> "rest") === %Result{@result | rest: "rest"}
+        end
+
+        test "encode(#{inspect value}) == #{inspect bytes}" do
+          assert @module.encode(@result) === @bytes
+        end
+
+        test "new(#{inspect value}) == %Result{value: #{inspect value}}" do
+          assert @module.new(@value) === @result
+        end
+      end)
+
+      Enum.each(opts[:new_errors], fn {value, error} ->
+        @value  value
+        @result %Result{module: @module, value: value, err: error}
+
+        test "new(#{inspect value}) == #{inspect @result}" do
+          assert @module.new(@value) === @result
+        end
+      end)
+    end
+  end
 end
--- test/wap/byte_test.exs
+++ test/wap/byte_test.exs
@@ -1,18 +1,15 @@
 defmodule WAP.ByteTest do
   use ExUnit.Case
-  import MM1.CodecExamples
 
-  alias WAP.Byte
-
-  examples Byte, [
-    {<<0>>,     0},
-    {<<255>>, 255},
-  ]
-
-  new_errors Byte, [
-    {  -1, :must_be_an_integer_between_0_and_255},
-    { 256, :must_be_an_integer_between_0_and_255},
-    {1.23, :must_be_an_integer_between_0_and_255},
-    {:foo, :must_be_an_integer_between_0_and_255},
-  ]
+  use MM1.CodecExamples, module: WAP.Byte,
+    examples: [
+      {<<0>>,     0},
+      {<<255>>, 255},
+    ],
+    new_errors: [
+      {  -1, :must_be_an_integer_between_0_and_255},
+      { 256, :must_be_an_integer_between_0_and_255},
+      {1.23, :must_be_an_integer_between_0_and_255},
+      { "x", :must_be_an_integer_between_0_and_255},
+    ]
 end
