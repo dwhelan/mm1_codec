f26a92cd2589be5b7ebe3e95fa14b097c96dfe26
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-26 04:49:50 -0500

    r extracted result macros into Result

--- lib/wap/short_length.ex
+++ lib/wap/short_length.ex
@@ -2,20 +2,21 @@ defmodule WAP.ShortLength do
   use MM1.BaseCodec
 
   import WAP.Guards
+  import MM1.Result
 
   def decode(<<value, rest::binary>>) when is_short_length(value) do
-    value value, <<value>>, rest
+    decode_ok value, <<value>>, rest
   end
 
   def decode <<value, rest::binary>> do
-    decode_error :must_be_an_integer_between_0_and_30, value, <<value>>, rest
+    decode_error value, <<value>>, rest, :must_be_an_integer_between_0_and_30
   end
 
   def new(value) when is_short_length(value) do
-    value value, <<value>>
+    new_ok value, <<value>>
   end
 
   def new value do
-    new_error :must_be_an_integer_between_0_and_30, value
+    new_error value, :must_be_an_integer_between_0_and_30
   end
 end
