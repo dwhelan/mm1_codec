10e1320b9002cd9da327138901f890df44caf6fa
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-25 19:21:09 -0500

    t added decode_errors macro

--- test/shared/codec_examples.ex
+++ test/shared/codec_examples.ex
@@ -5,14 +5,10 @@ defmodule MM1.CodecExamples do
 
       @module module
 
-      test "insufficient bytes" do
-        assert @module.decode(<<>>) === %Result{module: @module, err: :insufficient_bytes}
-      end
-
       Enum.each(examples, fn {bytes, value} ->
         @bytes  bytes
         @value  value
-        @result %Result{module: @module, value: @value, bytes: @bytes}
+        @result %Result{module: module, value: value, bytes: bytes}
 
         test "decode(#{inspect bytes}) == #{inspect value}" do
           assert @module.decode(@bytes <> "rest") === %Result{@result | rest: "rest"}
@@ -23,6 +19,44 @@ defmodule MM1.CodecExamples do
         end
 
         test "new(#{inspect value}) == %Result{value: #{inspect value}}" do
+          assert @module.new(@value) === @result
+        end
+      end)
+    end
+  end
+
+  defmacro decode_errors module, examples \\ [] do
+    quote bind_quoted: [module: module, examples: examples] do
+      alias MM1.Result
+
+      @module module
+
+      test "insufficient bytes" do
+        assert @module.decode(<<>>) === %Result{module: @module, err: :insufficient_bytes}
+      end
+
+      Enum.each(examples, fn {bytes, error, value} ->
+        @bytes  bytes
+        @result %Result{module: module, value: value, bytes: bytes, err: error}
+
+        test "decode(#{inspect bytes}) == #{inspect error}" do
+          assert @module.decode(@bytes <> "rest") === %Result{@result | rest: "rest"}
+        end
+      end)
+    end
+  end
+
+  defmacro new_errors module, examples do
+    quote bind_quoted: [module: module, examples: examples] do
+      alias MM1.Result
+
+      @module module
+
+      Enum.each(examples, fn {value, error} ->
+        @value  value
+        @result %Result{module: module, value: value, err: error}
+
+        test "new(#{inspect value}) == #{inspect @result}" do
           assert @module.new(@value) === @result
         end
       end)
