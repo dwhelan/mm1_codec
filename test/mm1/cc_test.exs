7df61c78a0fcdf8d758624f452a6e3cc3b524caa
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-26 01:24:10 -0500

    t use CodeExamples

--- test/mm1/cc_test.exs
+++ test/mm1/cc_test.exs
@@ -1,10 +1,10 @@
 defmodule MM1.CcTest do
   use ExUnit.Case
-  import MM1.CodecExamples
 
   alias MM1.Cc
 
-  examples Cc, [
-    {<<Cc.header_byte(), "abc", 0>>, "abc"}
-  ]
+  use MM1.CodecExamples, module: Cc,
+    examples: [
+      {<<Cc.header_byte(), "abc", 0>>, "abc"}
+    ]
 end
