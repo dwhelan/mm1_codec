998521994f70dc48745d16956ba0189abaa32823
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-26 01:26:19 -0500

    t use CodeExamples

--- test/mm1/x_mms_message_size_test.exs
+++ test/mm1/x_mms_message_size_test.exs
@@ -1,10 +1,10 @@
 defmodule MM1.XMmsMessageSizeTest do
   use ExUnit.Case
-  import MM1.CodecExamples
 
   alias MM1.XMmsMessageSize
 
-  examples XMmsMessageSize, [
-    {<<XMmsMessageSize.header_byte(), 1, 42>>, 42}
-  ]
+  use MM1.CodecExamples, module: XMmsMessageSize,
+      examples: [
+        {<<XMmsMessageSize.header_byte(), 1, 42>>, 42}
+      ]
 end
