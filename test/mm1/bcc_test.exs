861919d69507d8f6822f18885c2c23c5d937bd66
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-26 01:23:27 -0500

    t use CodeExamples

--- test/mm1/bcc_test.exs
+++ test/mm1/bcc_test.exs
@@ -1,10 +1,10 @@
 defmodule MM1.BccTest do
   use ExUnit.Case
-  import MM1.CodecExamples
 
   alias MM1.Bcc
 
-  examples Bcc, [
-    {<<Bcc.header_byte(), "abc", 0>>, "abc"}
-  ]
+  use MM1.CodecExamples, module: Bcc,
+    examples: [
+      {<<Bcc.header_byte(), "abc", 0>>, "abc"}
+    ]
 end
