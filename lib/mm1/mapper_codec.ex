1b720f99c67708db81f7ad28057245182f4df69a
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-29 20:42:40 -0500

    extracted lookup()

--- lib/mm1/mapper_codec.ex
+++ lib/mm1/mapper_codec.ex
@@ -13,63 +13,37 @@ defmodule MM1.MapperCodec do
       @map         map   || MM1.MapperCodec.missing_map()
       @reverse_map Enum.reduce @map, %{}, fn {k,v}, reverse_map -> Map.put(reverse_map, v, k) end
 
-      import MM1.Result
+      alias MM1.Result
+      import Result
 
       def decode bytes do
-        bytes |> @codec.decode |> map |> embed
+        bytes |> @codec.decode |> map
       end
 
-      def encode %MM1.Result{module: __MODULE__} = result do
-        @codec.encode %MM1.Result{result | module: @codec }
+      def encode %Result{module: __MODULE__} = result do
+        @codec.encode %Result{result | module: @codec }
       end
 
       def new value do
-        value |> reverse_map |> @codec.new |> map |> embed
-#        (@reverse_map[value] || value) |> @codec.new |> embed
+        value |> reverse_map |> @codec.new |> map
       end
 
       defp map result do
-        value = @map[result.value]
-        mapped_value = if is_nil(value) do
-          result.value
-        else
-          value
-        end
-        %MM1.Result{result | value: mapped_value}
+        %Result{result | value: lookup(result.value, @map), module: __MODULE__}
       end
 
       defp reverse_map value do
-        reversed_value = @reverse_map[value]
-        if is_nil(reversed_value) do
+        lookup value, @reverse_map
+      end
+
+      defp lookup value, map do
+        new_value = map[value]
+        if is_nil(new_value) do
           value
         else
-          reversed_value
+          new_value
         end
       end
     end
   end
 end
-#defmodule MM1.ComposeCodec do
-#  defmacro __using__(opts) do
-#    quote bind_quoted: [f: opts[:f], g: opts[:f]] do
-#      @f f
-#      @g g
-#
-#      def decode bytes do
-#        bytes |> @g.decode |> @f.decode
-#      end
-#
-#      def encode result do
-#       result |> @g.encode | @f.encode
-#      end
-#
-#      def new value do
-#        value |> @g.new | @f.new
-#      end
-#
-#      def map %Result{} = result do
-#        %Result{result | value: @map[value] || value}
-#      end
-#    end
-#  end
-#end
