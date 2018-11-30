4a4f58225a4c17f5155e368acc1cd689593e8f0b
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-29 23:20:16 -0500

    r inlined CoreMapper

--- lib/mm1/mapper_codec.ex
+++ lib/mm1/mapper_codec.ex
@@ -1,15 +1,3 @@
-defmodule MM1.CoreMapper do
-  alias MM1.Result
-
-  def map %Result{} = result, map do
-    %Result{result | value: map(result.value, map)}
-  end
-
-  def map value, map do
-    Map.get(map, value, value)
-  end
-end
-
 defmodule MM1.MapperCodec do
   def missing_codec do
     raise "Please provide a codec option: use MM1.MapperCode, codec: <codec>, map: <map>"
@@ -27,7 +15,6 @@ defmodule MM1.MapperCodec do
 
       alias MM1.Result
       import Result
-      import MM1.CoreMapper
 
       def decode bytes do
         bytes |> @codec.decode |> map
@@ -38,20 +25,11 @@ defmodule MM1.MapperCodec do
       end
 
       def new value do
-        value |> map(@reverse_map) |> @codec.new |> map
+        Map.get(@reverse_map, value, value) |> @codec.new |> map
       end
 
       def map result do
-        result |> map(@map) |> embed
-      end
-
-      defp map_result do
-      end
-
-      defp unmap_result do
-      end
-
-      defp map_new do
+        %Result{result | value: Map.get(@map, result.value, result.value)} |> embed
       end
     end
   end
