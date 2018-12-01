69dafb5af03fb0e4b774e1b8843af6b716595112
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-30 23:21:38 -0500

    r use new Mapper codec

--- lib/mm1/x_mms_message_type.ex
+++ lib/mm1/x_mms_message_type.ex
@@ -1,19 +1,23 @@
-defmodule MM1.XMmsMessageTypeMapper do
-  use MM1.OrdinalMapper, values: [
-    :m_send_conf,
-    :m_notification_ind,
-    :m_notifyresp_ind,
-    :m_send_req,
-    :m_retrieve_conf,
-    :m_acknowledge_ind,
-    :m_delivery_ind,
-    :m_read_rec_ind,
-    :m_read_orig_ind,
-    :m_forward_ind,
-    :m_forward_conf,
-  ]
+defmodule MM1.XMmsMessageType.Codec do
+  alias MM1.Codecs.Mapper
+
+  use Mapper,
+      codec: WAP.ShortInteger,
+      map: Mapper.ordinal_map [
+        :m_send_conf,
+        :m_notification_ind,
+        :m_notifyresp_ind,
+        :m_send_req,
+        :m_retrieve_conf,
+        :m_acknowledge_ind,
+        :m_delivery_ind,
+        :m_read_rec_ind,
+        :m_read_orig_ind,
+        :m_forward_ind,
+        :m_forward_conf,
+      ]
 end
 
 defmodule MM1.XMmsMessageType do
-  use MM1.Header, codec: WAP.ShortInteger, mapper: MM1.XMmsMessageTypeMapper
+  use MM1.Header, codec: MM1.XMmsMessageType.Codec
 end
