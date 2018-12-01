07c775397fed7f444f8ad46b3748c236417107e8
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-01 00:34:46 -0500

    r simplified decode()

--- lib/mm1/codecs/wrapper.ex
+++ lib/mm1/codecs/wrapper.ex
@@ -2,19 +2,27 @@ defmodule MM1.Codecs.Wrapper do
   defmacro __using__(opts) do
     quote bind_quoted: [codec: opts[:codec]] do
       import MM1.Codecs.Decorator
+      import MM1.Result
+      @codec       codec
 
-      decorate codec do
-        defp map_result result do
-          %MM1.Result{result | value: result, bytes: <<>>}
-        end
+      def decode bytes do
+        bytes |> @codec.decode |> wrap_result
+      end
 
-        defp encode_arg result do
-          result.value
-        end
+      def encode %MM1.Result{module: __MODULE__} = result do
+        result |> set_module(@codec) |> encode_arg |> @codec.encode
+      end
 
-        defp map_value value do
-          value
-        end
+      def new value do
+        value |> @codec.new |> wrap_result
+      end
+
+      defp wrap_result result do
+        %MM1.Result{result | module: __MODULE__, value: result, bytes: <<>>}
+      end
+
+      defp encode_arg result do
+        result.value
       end
     end
   end
