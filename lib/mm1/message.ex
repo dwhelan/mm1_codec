efec73de1ec0352a56fa8c8412a22fa8e6d6c703
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-01 15:00:29 -0500

    r extracted codec()´

--- lib/mm1/message.ex
+++ lib/mm1/message.ex
@@ -1,17 +1,22 @@
 defmodule MM1.Message do
+  alias MM1.Codecs.Wrapper
   import MM1.Codecs.Wrapper
 
   alias MM1.Headers
 
   def decode bytes do
-    decode bytes, Headers, __MODULE__
+    codec :decode, bytes
   end
 
   def encode result do
-    encode result, Headers, __MODULE__
+    codec :encode, result
   end
 
   def new value do
-    new value, Headers, __MODULE__
+    codec :new, value
+  end
+
+  def codec function_name, arg do
+    apply Wrapper, function_name, [arg, Headers, __MODULE__]
   end
 end
