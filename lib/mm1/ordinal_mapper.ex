b64158413fc6c388525fc071e6b9291ebddf9d25
Declan Whelan <dwhelan@dpwhelan.com>
2018-11-30 23:22:42 -0500

    r removed defunct OrdinalMapper

--- lib/mm1/ordinal_mapper.ex
+++
@@ -1,9 +0,0 @@
-defmodule MM1.OrdinalMapper do
-  defmacro __using__(opts) do
-    quote bind_quoted: [values: opts[:values]] do
-      map = values |> Enum.with_index |> Enum.reduce(%{}, fn {v, index}, acc -> Map.put(acc, index, v) end)
-
-      use MM1.Mapper, map: map
-    end
-  end
-end
