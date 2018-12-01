562ec6ce4348cba9df869599c459111761b796b9
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-01 16:29:08 -0500

    f Composer.new

--- lib/mm1/codecs/composer.ex
+++ lib/mm1/codecs/composer.ex
@@ -5,12 +5,14 @@ defmodule MM1.Codecs.Composer do
     %MM1.Result{module: module, value: [result1.value, result2.value], bytes: result1.bytes <> result2.bytes, rest: result2.rest}
   end
 
-  def encode result, codec, _module do
-    result.value |> codec.encode
+  def encode result, codec1, codec2, _module do
+    result.value |> codec1.encode
   end
 
-  def new value, codec, module do
-    value |> codec.new |> wrap(module)
+  def new [value1, value2], codec1, codec2, module do
+    result1 = value1 |> codec1.new
+    result2 = value2 |> codec2.new
+    %MM1.Result{module: module, value: [value1, value2], bytes: result1.bytes <> result2.bytes}
   end
 
   defp wrap result, module do
