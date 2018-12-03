bf22a0672a17efb73c7ae310ce58fd43df3c5bae
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-03 00:32:58 -0500

    r extracted Codec.Checks

--- lib/mm1/codecs/wrapper.ex
+++ lib/mm1/codecs/wrapper.ex
@@ -1,9 +1,23 @@
+defmodule MM1.Codecs.Checks do
+  defmacro __using__(_opts) do
+    quote do
+      alias MM1.Result
+
+      def decode <<>>, module do
+        %Result{module: module, err: :insufficient_bytes}
+      end
+
+      def new nil, module do
+        %Result{module: module, err: :value_cannot_be_nil}
+      end
+    end
+  end
+end
+
 defmodule MM1.Codecs.Wrapper do
   alias MM1.Result
 
-  def decode <<>>, module do
-    %Result{module: module, err: :insufficient_bytes}
-  end
+  use MM1.Codecs.Checks
 
   def decode bytes, module do
     bytes |> module.codec().decode |> wrap(module)
@@ -11,10 +25,6 @@ defmodule MM1.Codecs.Wrapper do
 
   def encode result, module do
     result.value |> module.codec().encode
-  end
-
-  def new nil, module do
-    %Result{module: module, err: :value_cannot_be_nil}
   end
 
   def new value, module do
