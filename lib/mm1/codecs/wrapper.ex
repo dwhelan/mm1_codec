9ea7cfdccd16832cab094806b107f3fa5d596dca
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-03 00:33:56 -0500

    r extracted Codec.Checks

--- lib/mm1/codecs/wrapper.ex
+++ lib/mm1/codecs/wrapper.ex
@@ -1,19 +1,3 @@
-defmodule MM1.Codecs.Checks do
-  defmacro __using__(_opts) do
-    quote do
-      alias MM1.Result
-
-      def decode <<>>, module do
-        %Result{module: module, err: :insufficient_bytes}
-      end
-
-      def new nil, module do
-        %Result{module: module, err: :value_cannot_be_nil}
-      end
-    end
-  end
-end
-
 defmodule MM1.Codecs.Wrapper do
   alias MM1.Result
 
--- lib/mm1/message.ex
+++ lib/mm1/message.ex
@@ -5,15 +5,15 @@ defmodule MM1.Message do
   alias MM1.Headers
 
   def decode bytes do
-    Wrapper.decode bytes, __MODULE__
+    decode bytes, __MODULE__
   end
 
   def encode result do
-    Wrapper.encode result, __MODULE__
+    encode result, __MODULE__
   end
 
   def new value do
-    Wrapper.new value, __MODULE__
+    new value, __MODULE__
   end
 
   def codec do
