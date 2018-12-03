bb92b62ec5747c03fd0893ede0b590425053dc60
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-03 18:44:00 -0500

    r replaced @headers with headers

--- lib/mm1/headers.ex
+++ lib/mm1/headers.ex
@@ -1,6 +1,41 @@
 defmodule MM1.Headers do
   # Based on OMA-WAP-MMS-ENC-V1_1-20040715-A: Table 12. Field Name Assignments
-  @headers [
+#  @headers [
+#    Bcc:                       0x01,
+#    Cc:                        0x02,
+#    #XMmsContentLocation:       0x03,
+#    #ContentType:               0x04,
+#    #Date:                      0x05,
+#    #XMmsDeliveryReport:        0x06,
+#    #M1:XMmsDeliveryTime,       0x07,
+#    #M1:XMmsExpiry,             0x08,
+#    #M1:From,                   0x09,
+#    #M1:XMmsMessageClass,       0x0a,
+#    #M1:MessageID,              0x0b,
+#    XMmsMessageType:           0x0c,
+#    #XMmsMMSVersion:            0x0d,
+#    XMmsMessageSize:           0x0e,
+#    #XMmsPriority:              0x0f,
+#    #XMmsReportAllowed:         0x11,
+#    #XMmsResponseStatus:        0x12,
+#    #XMmsResponseText:          0x13,
+#    #XMmsSenderVisibility:      0x14,
+#    #XMmsReadReport:            0x10,
+#    #XMmsStatus:                0x15,
+#    #Subject:                   0x16,
+#    #To:                        0x17,
+#    #XMmsTransactionId:         0x18,
+#    #XMmsRetrieveStatus:        0x19,
+#    #XMmsRetrieveText:          0x1a,
+#    #XMmsReadStatus:            0x1b,
+#    #XMmsReplyCharging:         0x1c,
+#    #XMmsReplyChargingDeadline: 0x1d,
+#    #XMmsReplyChargingID:       0x1e,
+#    #XMmsReplyChargingSize:     0x1f,
+#    #XMmsPreviouslySentBy:      0x20,
+#    #XMmsPreviouslySentDate:    0x21,
+#  ]
+  headers = [
     Bcc:                       0x01,
     Cc:                        0x02,
     #XMmsContentLocation:       0x03,
@@ -42,10 +77,10 @@ defmodule MM1.Headers do
     decode bytes, []
   end
 
-  @headers
+  headers
   |> Enum.each(fn {header, value} ->
     @module      :"Elixir.MM1.#{header}"
-    @header_byte value + 128
+    @header_byte @module.header_byte
 
     defp decode <<@header_byte, _::binary>> = bytes, headers do
       %{rest: rest} = header = @module.decode bytes
