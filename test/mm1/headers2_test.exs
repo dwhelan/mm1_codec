c109e0b2bc3203ee6a5024cc037a67bccb461147
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-03 22:06:54 -0500

    t added Bcc to Headers test

--- test/mm1/headers2_test.exs
+++ test/mm1/headers2_test.exs
@@ -1,11 +1,12 @@
 defmodule MM1.Headers2Test do
   use ExUnit.Case
   alias MM1.{Result, Headers}
-  alias MM1.{XMmsMessageSize}
+  alias MM1.{Bcc, XMmsMessageSize}
 
   use MM1.Codecs.TestExamples,
       codec: Headers,
       examples: [
-        {<<XMmsMessageSize.header_byte(), 1, 42>>, [%Result{bytes: <<142, 1, 42>>, module: XMmsMessageSize, rest: "rest", value: 42}]}
+        {<<            Bcc.header_byte(), "x", 0>>, [%Result{bytes: <<0x81, "x", 0>>, module: Bcc,             rest: "rest", value: "x"}]},
+        {<<XMmsMessageSize.header_byte(),   1, 0>>, [%Result{bytes: <<0x8e,   1, 0>>, module: XMmsMessageSize, rest: "rest", value:   0}]},
       ]
 end
