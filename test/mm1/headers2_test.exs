ad35c9e99a5df0f20b080c06cf87ff3aff29846a
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-03 23:23:09 -0500

    t test multiple headers

--- test/mm1/headers2_test.exs
+++ test/mm1/headers2_test.exs
@@ -10,5 +10,13 @@ defmodule MM1.Headers2Test do
         {<<0x82, "x", 0>>, [%Result{bytes: <<             Cc.header_byte(), "x", 0>>, module: Cc,              rest: "rest", value: "x"         }]},
         {<<0x8c,    128>>, [%Result{bytes: <<XMmsMessageType.header_byte(),    128>>, module: XMmsMessageType, rest: "rest", value: :m_send_conf}]},
         {<<0x8e,   1, 0>>, [%Result{bytes: <<XMmsMessageSize.header_byte(),   1, 0>>, module: XMmsMessageSize, rest: "rest", value:  0          }]},
+
+        {
+          <<0x81, "x", 0, 0x82, "x", 0>>,
+          [
+            %Result{bytes: <<Bcc.header_byte(), "x", 0>>, module: Bcc, rest: <<0x82, "x", 0, "rest">>, value: "x"},
+            %Result{bytes: << Cc.header_byte(), "x", 0>>, module: Cc,  rest: "rest", value: "x"}
+          ]
+        },
       ]
 end
