dd9327acbee9cd01a0208c1b94d5605ebf31e7bb
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-03 23:44:31 -0500

    r whitespace

--- test/mm1/headers_test.exs
+++ test/mm1/headers_test.exs
@@ -1,21 +1,20 @@
 defmodule MM1.HeadersTest do
   use ExUnit.Case
   alias MM1.{Result, Headers}
-  alias MM1.{Bcc, Cc, XMmsMessageType, XMmsMessageSize}
 
   use MM1.Codecs.TestExamples,
       codec: Headers,
       examples: [
-        {<<0x81, "x", 0>>, [%Result{bytes: <<            Bcc.header_byte(), "x", 0>>, module: Bcc,             rest: "rest", value: "x"         }]},
-        {<<0x82, "x", 0>>, [%Result{bytes: <<             Cc.header_byte(), "x", 0>>, module: Cc,              rest: "rest", value: "x"         }]},
-        {<<0x8c,    128>>, [%Result{bytes: <<XMmsMessageType.header_byte(),    128>>, module: XMmsMessageType, rest: "rest", value: :m_send_conf}]},
-        {<<0x8e,   1, 0>>, [%Result{bytes: <<XMmsMessageSize.header_byte(),   1, 0>>, module: XMmsMessageSize, rest: "rest", value:  0          }]},
+        {<<0x81, "x", 0>>, [%Result{bytes: <<0x81, "x", 0>>, module: MM1.Bcc,             rest: "rest", value: "x"         }]},
+        {<<0x82, "x", 0>>, [%Result{bytes: <<0x82, "x", 0>>, module: MM1.Cc,              rest: "rest", value: "x"         }]},
+        {<<0x8c,    128>>, [%Result{bytes: <<0x8c,    128>>, module: MM1.XMmsMessageType, rest: "rest", value: :m_send_conf}]},
+        {<<0x8e,   1, 0>>, [%Result{bytes: <<0x8e,   1, 0>>, module: MM1.XMmsMessageSize, rest: "rest", value:  0          }]},
 
         {
           <<0x81, "x", 0, 0x82, "x", 0>>,
           [
-            %Result{bytes: <<Bcc.header_byte(), "x", 0>>, module: Bcc, rest: <<0x82, "x", 0, "rest">>, value: "x"},
-            %Result{bytes: << Cc.header_byte(), "x", 0>>, module: Cc,  rest: "rest", value: "x"}
+            %Result{module: MM1.Bcc, value: "x", bytes: <<0x81, "x", 0>>, rest: <<0x82, "x", 0, "rest">>},
+            %Result{module: MM1.Cc,  value: "x", bytes: <<0x82, "x", 0>>, rest: "rest"                  }
           ]
         },
       ]
