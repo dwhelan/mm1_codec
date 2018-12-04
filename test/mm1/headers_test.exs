d7d7d81b1ad3210f75b7bc77fc3af847d8a7ded7
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-03 23:26:32 -0500

    r updated Headers tests to use TestExamples

--- test/mm1/headers_test.exs
+++ test/mm1/headers_test.exs
@@ -1,51 +1,28 @@
 defmodule MM1.HeadersTest do
   use ExUnit.Case
   alias MM1.{Result, Headers}
+  alias MM1.{Bcc, Cc, XMmsMessageType, XMmsMessageSize}
 
-  import Headers
+  use MM1.Codecs.TestExamples,
+      codec: Headers,
+      examples: [
+        {<<0x81, "x", 0>>, [%Result{bytes: <<            Bcc.header_byte(), "x", 0>>, module: Bcc,             rest: "rest", value: "x"         }]},
+        {<<0x82, "x", 0>>, [%Result{bytes: <<             Cc.header_byte(), "x", 0>>, module: Cc,              rest: "rest", value: "x"         }]},
+        {<<0x8c,    128>>, [%Result{bytes: <<XMmsMessageType.header_byte(),    128>>, module: XMmsMessageType, rest: "rest", value: :m_send_conf}]},
+        {<<0x8e,   1, 0>>, [%Result{bytes: <<XMmsMessageSize.header_byte(),   1, 0>>, module: XMmsMessageSize, rest: "rest", value:  0          }]},
 
-  #       header_byte -----+      +----- valid header value
-  #                        |      |
-  #                        V      V
-  headers = [
-   {MM1.Bcc,             <<129,   0>>},
-   {MM1.Cc,              <<130,   0>>},
-   {MM1.XMmsMessageType, <<140, 128>>},
-   {MM1.XMmsMessageSize, <<142,   0>>},
-  ]
-
-#  Enum.each(headers, fn {module, bytes} ->
-#    @module module
-#    @bytes  bytes
-#
-#    test "decode(#{inspect bytes}) => #{module}" do
-#      assert %{module: Headers, value: [%{module: @module}]} = decode @bytes
-#    end
-#
-#    test "encode([#{module}]) => #{inspect bytes}" do
-#      assert @bytes |> decode |> encode == @bytes
-#    end
-#  end)
-#
-#  describe "new" do
-#    test "new() returns a Result with empty headers" do
-#      assert new() == %Result{module: Headers, value: []}
-#    end
-#
-#    test "new([]) returns a Result with empty headers" do
-#      assert new([]) == %Result{module: Headers, value: []}
-#    end
-#
-#    test "new([%Result{value}]) should create new bytes" do
-#      bcc = MM1.Bcc.new "abc"
-#      assert new([bcc]).value == [%Result{bcc | bytes: <<129, "abc", 0>>}]
-#    end
-#  end
-#
-#  test "add" do
-#    bcc = MM1.Bcc.new "abc"
-#    headers = add new(), bcc
-#    assert headers.value == [bcc]
-#  end
-
+        {
+          <<0x81, "x", 0, 0x82, "x", 0>>,
+          [
+            %Result{bytes: <<Bcc.header_byte(), "x", 0>>, module: Bcc, rest: <<0x82, "x", 0, "rest">>, value: "x"},
+            %Result{bytes: << Cc.header_byte(), "x", 0>>, module: Cc,  rest: "rest", value: "x"}
+          ]
+        },
+      ]
+  test "add" do
+    bcc = MM1.Bcc.new "abc"
+    original = Headers.new
+    headers = Headers.add original, bcc
+    assert headers.value == [bcc]
+  end
 end
