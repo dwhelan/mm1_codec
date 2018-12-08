635716935ce68880a4bff5e01e89346e5986b538
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-07 23:59:58 -0500

    r Composer.decode with errors

---
+++ lib/mm1/codecs/composer2.ex
@@ -0,0 +1,78 @@
+defmodule MM1.Codecs2.Composer do
+
+  defmacro compose codecs do
+    quote do
+      import WAP.Guards
+
+      @codecs unquote(codecs)
+
+      def decode bytes do
+        results = @codecs
+                  |> List.foldl([ok(nil, bytes)], &decode_one/2)
+                  |> Enum.reverse
+                  |> tl
+
+        case result(results) do
+          :ok    -> ok    value(results), rest(results)
+          :error -> error value(results), bytes
+        end
+      end
+
+      def encode values do
+        results = @codecs
+                  |> Enum.with_index
+                  |> Enum.map(fn {codec, index} -> codec.encode(Enum.at(values, index)) end)
+        {:ok, {bytes(results), __MODULE__, values}}
+      end
+
+      defp decode_one codec, results do
+        previous = hd results
+        case previous do
+          {:ok, {_, _, rest}} -> [codec.decode(rest) | results]
+          error -> results
+        end
+      end
+
+      defp value results do
+        results |> Enum.map(fn {_, {value, _, _}} -> value end)
+      end
+
+      defp bytes results do
+        results |> Enum.reduce(<<>>, fn result, bytes ->
+          case result do
+            {:ok, {value, _, _}} -> bytes <> value
+            _ -> bytes
+          end
+          end)
+      end
+
+      defp rest results do
+        {_, {_, _, rest}} = Enum.at(results, -1)
+        rest
+      end
+
+      defp result results do
+        {result, _} = Enum.at(results, -1)
+        result
+      end
+#
+#      defp error results do
+#        errors = Enum.map(results, & &1.err)
+#        if Enum.all?(errors, &is_nil &1), do: nil, else: errors
+#      end
+#
+#      defp bytes results do
+#        {_, bytes} = List.foldl(results, {[%Result{}], <<>>},
+#          fn result, {results, bytes} ->
+#            if hd(results).err, do: {results, bytes}, else: {[result | results], bytes <> result.bytes}
+#          end)
+#        bytes
+#      end
+#
+#      defp result results do
+#        Enum.at(results, -1).rest
+#      end
+#
+    end
+  end
+end
--- test/mm1/codecs/composer_test.exs
+++ test/mm1/codecs/composer_test.exs
@@ -42,14 +42,13 @@ defmodule MM1.Codecs2.ComposerTest do
   use MM1.Codecs2.TestExamples,
       examples: [
         {<<128, 129, 130>>, [0, 1, 2]}
+      ],
+
+      decode_errors: [
+        {<<  2, 129, 130>>, [:most_signficant_bit_must_be_1] },
+        {<<128,   2, 130>>, [0, :most_signficant_bit_must_be_1]      },
+        {<<128, 129,   2>>, [0, 1, :most_signficant_bit_must_be_1]},
       ]
-#,
-#
-#      decode_errors: [
-#        {<<  2, 129, 130>>, [:most_signficant_bit_must_be_1],           [<<2>>],       <<2>>          },
-#        {<<128,   2, 130>>, [nil, :most_signficant_bit_must_be_1],      [0, <<2>>] ,   <<128, 2>>     },
-#        {<<128, 129,   2>>, [nil, nil, :most_signficant_bit_must_be_1], [0, 1, <<2>>], <<128, 129, 2>>},
-#      ],
 #
 #      new_errors: [
 #        { [-1,  1,  2], [:must_be_an_integer_between_0_and_127, nil, nil], <<>>        },
