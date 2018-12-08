730c8657ad42c95af8919141efb68aee84bf440d
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-07 22:20:41 -0500

    g cam “t add some errors tests”

--- test/mm1/codecs/wrapper_test.exs
+++ test/mm1/codecs/wrapper_test.exs
@@ -1,13 +1,23 @@
 defmodule MM.Codecs2.Wrapper.WrapperTest do
   use ExUnit.Case
 
-  alias WAP2.Byte
+  alias WAP2.ShortInteger
 
   import MM1.Codecs2.Wrapper
-  wrap Byte
+
+  wrap ShortInteger
 
   use MM1.Codecs2.TestExamples,
       examples: [
-        {<<0>>, {0, Byte}},
+        {<<128>>, {0, ShortInteger}},
+      ],
+
+      decode_errors: [
+        {<<127>>, {:most_signficant_bit_must_be_1, ShortInteger}},
+      ],
+
+      encode_errors: [
+        {{-1, ShortInteger}, :must_be_an_integer_between_0_and_127},
       ]
+
 end
