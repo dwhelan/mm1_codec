32fd19ee3a4f49b3617ee35fbb912d9007b9779d
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-11 21:43:20 -0500

    f Uintvar now supports new data types

--- lib/wap/uintvar.ex
+++ lib/wap/uintvar.ex
@@ -38,3 +38,46 @@ defmodule WAP.Uintvar do
     value + (total <<< 7)
   end
 end
+
+defmodule WAP2.Uintvar do
+  use Bitwise
+
+  import MM1.OkError
+  import WAP.Guards
+
+  def decode bytes do
+    _decode bytes, 0, <<>>
+  end
+
+  defp _decode<<1::1, value::7, rest::binary>>, total, bytes do
+    _decode rest, add(value, total), bytes <> <<1::1, value::7>>
+  end
+
+  defp _decode(<<value, rest::binary>>, total, bytes) when byte_size(bytes) > 4 do
+    error :uintvar_length_must_be_5_bytes_or_less
+  end
+
+  defp _decode <<value, rest::binary>>, total, bytes do
+    ok {add(value, total), rest}
+  end
+
+  def encode(value) when is_uintvar(value) do
+    ok bytes_for(value >>> 7, <<value &&& 0x7f>>)
+  end
+
+  def encode value do
+    error :must_be_an_unsigned_32_bit_integer
+  end
+
+  defp bytes_for 0, bytes do
+    bytes
+  end
+
+  defp bytes_for value, bytes do
+    bytes_for value >>> 7, <<1::1, (value &&& 0x7f)::7>> <> bytes
+  end
+
+  defp add value, total do
+    value + (total <<< 7)
+  end
+end
--- test/wap/uintvar_test.exs
+++ test/wap/uintvar_test.exs
@@ -3,7 +3,8 @@ defmodule WAP.UintvarTest do
 
   max_value = 0xffffffff
 
-  use MM1.Codecs.TestExamples, codec: WAP.Uintvar,
+  use MM1.Codecs.TestExamples,
+      codec: WAP.Uintvar,
       examples: [
         {<<0>>, 0},
         {<<127>>, 127},
@@ -18,14 +19,51 @@ defmodule WAP.UintvarTest do
       ],
 
       decode_errors: [
-        {<<128, 255, 255, 255, 255, 127>>, :uintvar_length_must_be_5_bytes_or_less, 34_359_738_367, <<128, 255, 255, 255, 255, 127>>}
+        {
+          <<128, 255, 255, 255, 255, 127>>,
+          :uintvar_length_must_be_5_bytes_or_less,
+          34_359_738_367,
+          <<128, 255, 255, 255, 255, 127>>
+        }
       ],
 
       new_errors: [
-        {         -1, :must_be_an_unsigned_32_bit_integer},
-        {       1.23, :must_be_an_unsigned_32_bit_integer},
-        {        "x", :must_be_an_unsigned_32_bit_integer},
-        {       :foo, :must_be_an_unsigned_32_bit_integer},
-        {max_value+1, :must_be_an_unsigned_32_bit_integer},
+        {-1, :must_be_an_unsigned_32_bit_integer},
+        {1.23, :must_be_an_unsigned_32_bit_integer},
+        {"x", :must_be_an_unsigned_32_bit_integer},
+        {:foo, :must_be_an_unsigned_32_bit_integer},
+        {max_value + 1, :must_be_an_unsigned_32_bit_integer},
       ]
- end
+end
+defmodule WAP2.UintvarTest do
+  use ExUnit.Case
+
+  max_value = 0xffffffff
+
+  use MM1.Codecs2.TestExamples,
+      codec: WAP2.Uintvar,
+      examples: [
+        {<<0>>, {0, <<>>}},
+        {<<127>>, {127, <<>>}},
+        {<<129, 0>>, {128, <<>>}},
+        {<<255, 127>>, {16_383, <<>>}},
+        {<<129, 128, 0>>, {16_384, <<>>}},
+        {<<255, 255, 127>>, {2_097_151, <<>>}},
+        {<<129, 128, 128, 0>>, {2_097_152, <<>>}},
+        {<<255, 255, 255, 127>>, {268_435_455, <<>>}},
+        {<<129, 128, 128, 128, 0>>, {268_435_456, <<>>}},
+        {<<143, 255, 255, 255, 127>>, {max_value, <<>>}},
+      ],
+
+      decode_errors: [
+        {
+          <<128, 255, 255, 255, 255, 127>>, :uintvar_length_must_be_5_bytes_or_less
+        }
+      ],
+
+      encode_errors: [
+        {-1,              :must_be_an_unsigned_32_bit_integer},
+        {max_value + 1,   :must_be_an_unsigned_32_bit_integer},
+        {:not_an_integer, :must_be_an_unsigned_32_bit_integer},
+      ]
+end
