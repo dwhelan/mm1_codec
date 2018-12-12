ae7d3ac780805de8adea5ea1a1eaea60df8f2763
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-11 21:22:58 -0500

    r started conversion of ValueLength but need to do Uintvar first

--- lib/wap/value_length.ex
+++ lib/wap/value_length.ex
@@ -33,3 +33,39 @@ defmodule WAP.ValueLength do
     %Result{result | bytes: <<@length_quote>> <> result.bytes}
   end
 end
+
+defmodule WAP2.ValueLength do
+  use MM1.Codecs.Base
+  alias WAP.{ShortLength, Uintvar}
+  alias MM1.Result
+
+  @length_quote 31
+
+  def decode(<<value, _::binary>> = bytes) when is_short_length(value) do
+    bytes |> ShortLength.decode |> set_module
+  end
+
+  def decode <<@length_quote, bytes::binary>> do
+    bytes |> Uintvar.decode |> prefix_with_length_quote |> set_module
+  end
+
+  def decode <<value, rest::binary>> do
+    decode_error :first_byte_must_be_less_than_32, value, <<value>>, rest
+  end
+
+  def new(value) when is_short_length(value) do
+    value |> ShortLength.new |> set_module
+  end
+
+  def new(value) when is_uintvar(value) do
+    value |> Uintvar.new |> prefix_with_length_quote |> set_module
+  end
+
+  def new value do
+    new_error :must_be_an_unsigned_32_bit_integer, value
+  end
+
+  defp prefix_with_length_quote result do
+    %Result{result | bytes: <<@length_quote>> <> result.bytes}
+  end
+end
