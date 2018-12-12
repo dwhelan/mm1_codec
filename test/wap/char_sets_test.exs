593a08c44cd44e7cf101406eb31eee6ad389ce9b
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-12 01:03:48 -0500

    r Simplified Charsets with new data types

--- test/wap/char_sets_test.exs
+++ test/wap/char_sets_test.exs
@@ -28,3 +28,26 @@ defmodule WAP.CharSetsTest do
     assert unmap(nil)        ===  nil
   end
 end
+
+defmodule WAP2.CharSetsTest do
+  import WAP2.CharSets
+  alias MM1.Result
+
+  use ExUnit.Case
+
+  test "map() should return charset atom when found" do
+    assert map(1) == :other
+  end
+
+  test "map() should return input value when not found" do
+    assert map(:not_found) == :not_found
+  end
+
+  test "unmap(mapped_value) should return charset code when found" do
+    assert unmap(:other)     ===    1
+    assert unmap(:csUTF8)    ===  106
+    assert unmap(:csUnicode) === 1000
+    assert unmap(9999)       === 9999
+    assert unmap(nil)        ===  nil
+  end
+end
