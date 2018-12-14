c0f043f3eb01e98fb1f91d4b4a0eff515098e4ce
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-13 20:34:57 -0500

    f XMmsMessageSize to new data types

--- lib/mm1/x_mms_message_size.ex
+++ lib/mm1/x_mms_message_size.ex
@@ -3,3 +3,15 @@ defmodule MM1.XMmsMessageSize do
       value: 0x8e,
       codec: WAP.LongInteger
 end
+
+defmodule MM2.XMmsMessageSize do
+  alias WAP2.LongInteger
+
+  def decode bytes do
+    bytes |> LongInteger.decode
+  end
+
+  def encode value do
+    value |> LongInteger.encode
+  end
+end
--- test/mm1/x_mms_message_size_test.exs
+++ test/mm1/x_mms_message_size_test.exs
@@ -3,8 +3,22 @@ defmodule MM1.XMmsMessageSizeTest do
 
   alias MM1.XMmsMessageSize
 
-  use MM1.Codecs.TestExamples, codec: XMmsMessageSize,
+  use MM1.Codecs.TestExamples,
+      codec: XMmsMessageSize,
       examples: [
         {<<0x8e, 1, 42>>, 42}
       ]
 end
+
+defmodule MM2.XMmsMessageSizeTest do
+  use ExUnit.Case
+
+  alias MM2.XMmsMessageSize
+
+  use MM1.Codecs2.TestExamples,
+      codec: XMmsMessageSize,
+      examples: [
+        {<<1, 42>>, {42, ""}}
+      ]
+end
+
