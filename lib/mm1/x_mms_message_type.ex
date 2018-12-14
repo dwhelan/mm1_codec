d9b59b98e17fda522737068b0b9c8c8e79db0bcb
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-13 20:40:44 -0500

    f First cut at XMmsMessageType to new data types

--- lib/mm1/x_mms_message_type.ex
+++ lib/mm1/x_mms_message_type.ex
@@ -16,3 +16,22 @@ defmodule MM1.XMmsMessageType do
         :m_forward_conf,
       ]
 end
+
+defmodule MM2.XMmsMessageType do
+  use MM1.Header,
+      value: 0x8c,
+      codec: WAP.ShortInteger,
+      map: [
+        :m_send_conf,
+        :m_notification_ind,
+        :m_notifyresp_ind,
+        :m_send_req,
+        :m_retrieve_conf,
+        :m_acknowledge_ind,
+        :m_delivery_ind,
+        :m_read_rec_ind,
+        :m_read_orig_ind,
+        :m_forward_ind,
+        :m_forward_conf,
+      ]
+end
--- test/mm1/x_mms_message_type_test.exs
+++ test/mm1/x_mms_message_type_test.exs
@@ -3,7 +3,8 @@ defmodule MM1.XMmsMessageTypeTest do
 
   alias MM1.XMmsMessageType
 
-  use MM1.Codecs.TestExamples, codec: XMmsMessageType,
+  use MM1.Codecs.TestExamples,
+      codec: XMmsMessageType,
       examples: [
         {<<0x8c,  128>>, :m_send_conf       },
         {<<0x8c,  129>>, :m_notification_ind},
@@ -19,4 +20,28 @@ defmodule MM1.XMmsMessageTypeTest do
         {<<0x8c,  139>>, 11                 },
       ]
 end
+
+defmodule MM2.XMmsMessageTypeTest do
+  use ExUnit.Case
+
+  alias MM2.XMmsMessageType
+
+  use MM1.Codecs.TestExamples,
+      codec: XMmsMessageType,
+      examples: [
+        {<<0x8c,  128>>, :m_send_conf       },
+        {<<0x8c,  129>>, :m_notification_ind},
+        {<<0x8c,  130>>, :m_notifyresp_ind  },
+        {<<0x8c,  131>>, :m_send_req        },
+        {<<0x8c,  132>>, :m_retrieve_conf   },
+        {<<0x8c,  133>>, :m_acknowledge_ind },
+        {<<0x8c,  134>>, :m_delivery_ind    },
+        {<<0x8c,  135>>, :m_read_rec_ind    },
+        {<<0x8c,  136>>, :m_read_orig_ind   },
+        {<<0x8c,  137>>, :m_forward_ind     },
+        {<<0x8c,  138>>, :m_forward_conf    },
+        {<<0x8c,  139>>, 11                 },
+      ]
+end
+
 
