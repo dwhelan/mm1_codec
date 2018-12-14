deeba68af1d9ceb2b30b617512ee721aad0c71f6
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-14 00:07:08 -0500

    t added MapperImportTest

--- test/mm1/codecs/mapper_test.exs
+++ test/mm1/codecs/mapper_test.exs
@@ -118,3 +118,33 @@ defmodule MM1.Codecs2.Mapper.ListTest do
         {-1, :must_be_an_integer_between_0_and_30},
       ]
 end
+
+defmodule MM1.Codecs2.Mapper.ImportTest do
+  use ExUnit.Case
+
+  alias WAP2.ShortLength
+  import MM1.Codecs2.Mapper
+
+  def decode bytes do
+    bytes |> decode(ShortLength, %{0 => false, 1 => true})
+  end
+
+  def encode value do
+    value |> encode(ShortLength, %{false => 0, true => 1})
+  end
+
+  use MM1.Codecs2.TestExamples,
+      examples: [
+        {<<0>>, {false, <<>>}},
+        {<<1>>, { true, <<>>}},
+        {<<2>>, {    2, <<>>}},
+      ],
+
+      decode_errors: [
+        {<<31>>, :must_be_an_integer_between_0_and_30},
+      ],
+
+      encode_errors: [
+        {-1, :must_be_an_integer_between_0_and_30},
+      ]
+end
