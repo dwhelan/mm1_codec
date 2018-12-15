fc8e645599710685401b5605ac5d08e5faab5eca
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-15 01:54:54 -0500

    r removed old CharSet

--- test/wap/char_set_test.exs
+++ test/wap/char_set_test.exs
@@ -1,22 +1,3 @@
-defmodule WAP.CharSetTest do
-  use ExUnit.Case
-
-  use MM1.Codecs.TestExamples,
-      codec: WAP.CharSet,
-      examples: [
-        {<<0xea>>, :csUTF8},
-        {<<2, 0x03, 0xe8>>, :csUnicode},
-        {<<2, 0x0b, 0xb8>>, :reserved},
-      ],
-
-      new_errors: [
-        {-1, :must_be_an_integer_greater_than_or_equal_to_0},
-        {1.23, :must_be_an_integer_greater_than_or_equal_to_0},
-        {"x", :must_be_an_integer_greater_than_or_equal_to_0},
-        {:foo, :unknown_char_set},
-      ]
-end
-
 defmodule WAP2.CharSetTest do
   use ExUnit.Case
 
