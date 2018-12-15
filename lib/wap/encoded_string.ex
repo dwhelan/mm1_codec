2eebc0aa04888f3de6a5ab5c193fa899b01154de
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-15 01:52:42 -0500

    r removed old EncodedString

--- lib/wap/encoded_string.ex
+++ lib/wap/encoded_string.ex
@@ -1,37 +1,3 @@
-defmodule WAP.EncodedString do
-  use MM1.Codecs.Base
-  alias MM1.Result
-
-  alias WAP.{ValueLength, CharSet, TextString}
-
-  def decode(<<value, _::binary>> = data) when is_text(value) do
-    data ~> TextString
-  end
-
-  def decode data do
-    data ~> ValueLength ~> CharSet ~> TextString
-  end
-
-  def new {length, char_set, text} = encoded_string do
-    bytes = ValueLength.new(length).bytes() <> CharSet.new(char_set).bytes() <> TextString.new(text).bytes
-    new_ok encoded_string, bytes
-  end
-
-  def new text do
-    set_module TextString.new text
-  end
-
-  defp bytes ~> codec when is_binary(bytes) do
-    set_module codec.decode bytes
-  end
-
-  defp previous ~> codec do
-    result = codec.decode previous.rest
-    previous_value = if is_tuple(previous.value), do: previous.value, else: {previous.value}
-    set_module %Result{result | value: Tuple.append(previous_value, result.value), bytes: previous.bytes <> result.bytes}
-  end
-end
-
 defmodule WAP2.EncodedString do
   import MM1.OkError
   import WAP.Guards
--- test/wap/encoded_string_test.exs
+++ test/wap/encoded_string_test.exs
@@ -1,22 +1,3 @@
-defmodule WAP.EncodedStringTest do
-  use ExUnit.Case
-
-  use MM1.Codecs.TestExamples,
-      codec: WAP.EncodedString,
-    examples: [
-      {<<0>>,                                ""},
-      {<<"text", 0>>,                        "text"},
-      {<<6, 0xea, "text", 0>>,               { 6, :csUTF8,    "text"}},
-      {<<8, 2, 0x03, 0xe8, "text", 0>>,      { 8, :csUnicode, "text"}},
-      {<<31, 42, 2, 0x03, 0xe8, "text", 0>>, {42, :csUnicode, "text"}},
-    ],
-
-    decode_errors: [
-      {<<"text">>,          :missing_terminator,              "text",           <<"text">>},
-      {<<6, 0xea, "text">>, :missing_terminator, {6, :csUTF8, "text"}, <<6, 0xea, "text">>},
-    ]
-end
-
 defmodule WAP2.EncodedStringTest do
   use ExUnit.Case
 
