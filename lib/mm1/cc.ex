8eb3f220aa2af5fd6f4bfb8ea286c9eca548bfce
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-14 22:09:51 -0500

    r Cc to use Embed

--- lib/mm1/cc.ex
+++ lib/mm1/cc.ex
@@ -3,3 +3,7 @@ defmodule MM1.Cc do
       value: 0x82,
       codec: WAP.EncodedString
 end
+
+defmodule MM2.Cc do
+  use MM2.Embed, codec: WAP2.EncodedString
+end
--- test/mm1/cc_test.exs
+++ test/mm1/cc_test.exs
@@ -3,8 +3,22 @@ defmodule MM1.CcTest do
 
   alias MM1.Cc
 
-  use MM1.Codecs.TestExamples, codec: Cc,
-    examples: [
-      {<<0x82, "abc", 0>>, "abc"}
-    ]
+  use MM1.Codecs.TestExamples,
+      codec: Cc,
+      examples: [
+        {<<0x82, "abc", 0>>, "abc"}
+      ]
+end
+
+
+defmodule MM2.CcTest do
+  use ExUnit.Case
+
+  alias MM2.Cc
+
+  use MM1.Codecs2.TestExamples,
+      codec: Cc,
+      examples: [
+        {<<"abc", 0>>, {"abc", <<>>}}
+      ]
 end
