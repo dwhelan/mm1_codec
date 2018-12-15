6a282d00d31246ba8babc75fa98a9ea3fb8a5461
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-15 02:06:29 -0500

    r removed old Mapper and Byte

--- lib/wap/byte.ex
+++ lib/wap/byte.ex
@@ -1,19 +1,3 @@
-defmodule WAP.Byte do
-  use MM1.Codecs.Base
-
-  def decode <<value, rest::binary>> do
-    decode_ok value, <<value>>, rest
-  end
-
-  def new(value) when is_byte(value) do
-    new_ok value, <<value>>
-  end
-
-  def new value do
-    new_error :must_be_an_integer_between_0_and_255, value
-  end
-end
-
 defmodule WAP2.Byte do
   import MM1.OkError
   import WAP.Guards
