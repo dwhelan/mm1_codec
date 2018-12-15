c685ec4ff36cede4289833755a7b46725592c550
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-15 01:47:16 -0500

    r removed old Headers

--- test/mm1/headers_test.exs
+++ test/mm1/headers_test.exs
@@ -1,31 +1,3 @@
-defmodule MM1.HeadersTest do
-  use ExUnit.Case
-  alias MM1.{Result, Headers}
-
-  use MM1.Codecs.TestExamples,
-      codec: Headers,
-      examples: [
-        {<<0x81, "x", 0>>, [%Result{bytes: <<0x81, "x", 0>>, module: MM1.Bcc,             rest: "rest", value: "x"         }]},
-        {<<0x82, "x", 0>>, [%Result{bytes: <<0x82, "x", 0>>, module: MM1.Cc,              rest: "rest", value: "x"         }]},
-        {<<0x8c,    128>>, [%Result{bytes: <<0x8c,    128>>, module: MM1.XMmsMessageType, rest: "rest", value: :m_send_conf}]},
-        {<<0x8e,   1, 0>>, [%Result{bytes: <<0x8e,   1, 0>>, module: MM1.XMmsMessageSize, rest: "rest", value:  0          }]},
-
-        {
-          <<0x81, "x", 0, 0x82, "x", 0>>,
-          [
-            %Result{module: MM1.Bcc, value: "x", bytes: <<0x81, "x", 0>>, rest: <<0x82, "x", 0, "rest">>},
-            %Result{module: MM1.Cc,  value: "x", bytes: <<0x82, "x", 0>>, rest: "rest"                  }
-          ]
-        },
-      ]
-  test "add" do
-    bcc = MM1.Bcc.new "abc"
-    original = Headers.new
-    headers = Headers.add original, bcc
-    assert headers.value == [bcc]
-  end
-end
-
 defmodule MM2.HeadersTest do
   use ExUnit.Case
   alias MM2.Headers
