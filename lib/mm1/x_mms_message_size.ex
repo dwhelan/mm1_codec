e4b55dbc294f9a07de6866cc2e78473040478f53
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-15 01:48:41 -0500

    r removed old XMmmsMessageSize

--- lib/mm1/x_mms_message_size.ex
+++ lib/mm1/x_mms_message_size.ex
@@ -1,9 +1,3 @@
-defmodule MM1.XMmsMessageSize do
-  use MM1.Header,
-      value: 0x8e,
-      codec: WAP.LongInteger
-end
-
 defmodule MM2.XMmsMessageSize do
   use MM2.Embed, codec: WAP2.LongInteger
 end
--- test/mm1/x_mms_message_size_test.exs
+++ test/mm1/x_mms_message_size_test.exs
@@ -1,15 +1,3 @@
-defmodule MM1.XMmsMessageSizeTest do
-  use ExUnit.Case
-
-  alias MM1.XMmsMessageSize
-
-  use MM1.Codecs.TestExamples,
-      codec: XMmsMessageSize,
-      examples: [
-        {<<0x8e, 1, 42>>, 42}
-      ]
-end
-
 defmodule MM2.XMmsMessageSizeTest do
   use ExUnit.Case
 
