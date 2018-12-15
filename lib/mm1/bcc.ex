680f36458dde3c1bee06cf6aeda9e2160f8bb71f
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-14 22:06:16 -0500

    r Bcc to use Mapper

--- lib/mm1/bcc.ex
+++ lib/mm1/bcc.ex
@@ -3,3 +3,7 @@ defmodule MM1.Bcc do
       value: 0x81,
       codec: WAP.EncodedString
 end
+
+defmodule MM2.Bcc do
+  use MM2.Embed, codec: WAP2.EncodedString
+end
--- test/mm1/bcc_test.exs
+++ test/mm1/bcc_test.exs
@@ -3,8 +3,22 @@ defmodule MM1.BccTest do
 
   alias MM1.Bcc
 
-  use MM1.Codecs.TestExamples, codec: Bcc,
-    examples: [
-      {<<0x81, "abc", 0>>, "abc"}
-    ]
+  use MM1.Codecs.TestExamples,
+      codec: Bcc,
+      examples: [
+        {<<0x81, "abc", 0>>, "abc"}
+      ]
 end
+
+defmodule MM2.BccTest do
+  use ExUnit.Case
+
+  alias MM2.Bcc
+
+  use MM1.Codecs2.TestExamples,
+      codec: Bcc,
+      examples: [
+        {<<"abc", 0>>, {"abc", <<>>}}
+      ]
+end
+
