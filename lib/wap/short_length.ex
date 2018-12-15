e53e03addcfe71bfd4c42404fd2a0d65f22f6b07
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-15 02:00:34 -0500

    r removed old ShortLength

--- lib/wap/short_length.ex
+++ lib/wap/short_length.ex
@@ -1,23 +1,3 @@
-defmodule WAP.ShortLength do
-  use MM1.Codecs.Base
-
-  def decode(<<value, rest::binary>>) when is_short_length(value) do
-    decode_ok value, <<value>>, rest
-  end
-
-  def decode <<value, rest::binary>> do
-    decode_error :must_be_an_integer_between_0_and_30, value, <<value>>, rest
-  end
-
-  def new(value) when is_short_length(value) do
-    new_ok value, <<value>>
-  end
-
-  def new value do
-    new_error :must_be_an_integer_between_0_and_30, value
-  end
-end
-
 defmodule WAP2.ShortLength do
   import MM1.OkError
   import WAP.Guards
--- test/wap/short_length_test.exs
+++ test/wap/short_length_test.exs
@@ -1,25 +1,3 @@
-defmodule WAP.ShortLengthTest do
-  use ExUnit.Case
-
-  use MM1.Codecs.TestExamples, codec: WAP.ShortLength,
-    examples: [
-      {<< 0>>,  0},
-      {<<30>>, 30},
-    ],
-
-    decode_errors: [
-      {<<31>>, :must_be_an_integer_between_0_and_30, 31, <<31>>},
-    ],
-
-    new_errors: [
-      {  -1, :must_be_an_integer_between_0_and_30},
-      {  31, :must_be_an_integer_between_0_and_30},
-      {1.23, :must_be_an_integer_between_0_and_30},
-      { "x", :must_be_an_integer_between_0_and_30},
-      {:foo, :must_be_an_integer_between_0_and_30},
-    ]
-end
-
 defmodule WAP2.ShortLengthTest do
   use ExUnit.Case
 
