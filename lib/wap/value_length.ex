e8985f8b2ef597473f3a34fc86bbe3f3d03e4f2f
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-15 01:58:24 -0500

    r removed old ValueLength

--- lib/wap/value_length.ex
+++ lib/wap/value_length.ex
@@ -1,39 +1,3 @@
-defmodule WAP.ValueLength do
-  use MM1.Codecs.Base
-  alias WAP.{ShortLength, Uintvar}
-  alias MM1.Result
-
-  @length_quote 31
-
-  def decode(<<value, _::binary>> = bytes) when is_short_length(value) do
-    bytes |> ShortLength.decode |> set_module
-  end
-
-  def decode <<@length_quote, bytes::binary>> do
-    bytes |> Uintvar.decode |> prefix_with_length_quote |> set_module
-  end
-
-  def decode <<value, rest::binary>> do
-    decode_error :first_byte_must_be_less_than_32, value, <<value>>, rest
-  end
-
-  def new(value) when is_short_length(value) do
-    value |> ShortLength.new |> set_module
-  end
-
-  def new(value) when is_uintvar(value) do
-    value |> Uintvar.new |> prefix_with_length_quote |> set_module
-  end
-
-  def new value do
-    new_error :must_be_an_unsigned_32_bit_integer, value
-  end
-
-  defp prefix_with_length_quote result do
-    %Result{result | bytes: <<@length_quote>> <> result.bytes}
-  end
-end
-
 defmodule WAP2.ValueLength do
   alias WAP2.{ShortLength, Uintvar}
 
@@ -59,7 +23,7 @@ defmodule WAP2.ValueLength do
   end
 
   def encode(value) when is_uintvar(value) do
-    value |> Uintvar.encode |> prefix_with_length_quote
+    ok value |> Uintvar.encode |> prefix_with_length_quote
   end
 
   def encode value do
@@ -67,6 +31,6 @@ defmodule WAP2.ValueLength do
   end
 
   defp prefix_with_length_quote {:ok, bytes} do
-    ok <<@length_quote>> <> bytes
+    <<@length_quote>> <> bytes
   end
 end
