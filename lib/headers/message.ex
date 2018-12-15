15aa418f6fb6a0320c04b092a9b2c483be373023
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-15 15:24:39 -0500

    r removed Wrapper

--- lib/headers/message.ex
+++ lib/headers/message.ex
@@ -1,5 +1,21 @@
 defmodule MMS.Message do
-  import MM1.Codecs.Wrapper
+  import MMS.OkError
 
-  wrap MMS.Headers
+  alias MMS.Headers
+
+  def decode bytes do
+    bytes |> Headers.decode |> wrap
+  end
+
+  def encode {Headers, value} do
+    value |> Headers.encode
+  end
+
+  defp wrap {:ok, {value, rest}} do
+    ok {{Headers, value}, rest}
+  end
+
+  defp wrap {:error, reason} do
+    error {Headers, reason}
+  end
 end
