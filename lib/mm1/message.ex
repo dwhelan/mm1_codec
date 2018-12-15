b41bb0ea45bcfcd4214614b5c1a0777ac3948be0
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-15 01:46:43 -0500

    r removed old Message

--- lib/mm1/message.ex
+++ lib/mm1/message.ex
@@ -1,11 +1,3 @@
-defmodule MM1.Message do
-  use MM1.Codecs.Wrapper
-
-  def codec do
-    MM1.Headers
-  end
-end
-
 defmodule MM2.Message do
   import MM1.Codecs2.Wrapper
 
--- test/mm1/message_test.exs
+++ test/mm1/message_test.exs
@@ -1,25 +1,3 @@
-defmodule MM1.MessageTest do
-  use ExUnit.Case
-
-  alias MM1.{Result, Headers, Message}
-
-  use MM1.Codecs.TestExamples,
-      codec: Message,
-      examples: [
-        {
-          <<0x81, "x", 0, 0x82, "x", 0>>,
-          %Result{
-            bytes: <<0x81, "x", 0, 0x82, "x", 0>>,
-            module: Headers,
-            value: [
-              %Result{module: MM1.Bcc, value: "x", bytes: <<0x81, "x", 0>>, rest: <<0x82, "x", 0, "rest">>},
-              %Result{module: MM1.Cc,  value: "x", bytes: <<0x82, "x", 0>>, rest: "rest"                  }
-            ]
-          }
-        },
-      ]
-end
-
 defmodule MM2.MessageTest do
   use ExUnit.Case
 
