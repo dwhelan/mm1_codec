7fbce15d1d52214c97ba115e54d32501e4574536
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-15 01:56:15 -0500

    r removed old TextString

--- lib/wap/text_string.ex
+++ lib/wap/text_string.ex
@@ -1,23 +1,3 @@
-defmodule WAP.TextString do
-  use MM1.Codecs.Base
-
-  def decode <<bytes::binary>> do
-    _decode String.split bytes, <<0>>, parts: 2
-  end
-
-  defp _decode [text | [rest]] do
-    decode_ok text, text <> <<0>>, rest
-  end
-
-  defp _decode [text | []] do
-    decode_error :missing_terminator, text, text, <<>>
-  end
-
-  def new text do
-    new_ok text, text <> <<0>>
-  end
-end
-
 defmodule WAP2.TextString do
   import MM1.OkError
 
--- test/wap/text_string_test.exs
+++ test/wap/text_string_test.exs
@@ -1,17 +1,3 @@
-defmodule WAP.TextStringTest do
-  use ExUnit.Case
-
-  use MM1.Codecs.TestExamples, codec: WAP.TextString,
-    examples: [
-      {<<0>>,             ""},
-      {<<"text", 0>>, "text"},
-    ],
-
-    decode_errors: [
-      {<<"text">>, :missing_terminator, "text", <<"text">>}
-    ]
-end
-
 defmodule WAP2.TextStringTest do
   use ExUnit.Case
 
