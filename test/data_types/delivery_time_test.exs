e8d73accf3c347fea54da7b9ced864ad365280e4
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-16 14:59:00 -0500

    t DeliveryTime.encode errors

--- test/data_types/delivery_time_test.exs
+++ test/data_types/delivery_time_test.exs
@@ -16,6 +16,12 @@ defmodule MMS.DeliveryTimeTest do
         {<<32>>,            :first_byte_must_be_less_than_32}, # length error
         {<<3,   0,  1, 0>>, :most_signficant_bit_must_be_1  }, # absolute/relative error
         {<<3, 128, 31, 0>>, :length_must_be_between_1_and_30}, # value error
+      ],
+
+      encode_errors: [
+        {{0,         :absolute, -1}, :must_be_an_unsigned_32_bit_integer            }, # length error
+        {{0,         128,        3}, :must_be_an_integer_between_0_and_127          }, # absolute/relative error
+        {{:not_long, :absolute,  3}, :must_be_an_integer_between_1_and_30_bytes_long}, # value error
       ]
 end
 
