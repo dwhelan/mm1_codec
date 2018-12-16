995c8f29d71d3e29d092f6eb77b0abd04527fe52
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-16 02:38:55 -0500

    w

--- test/headers_test.exs
+++ test/headers_test.exs
@@ -5,16 +5,16 @@ defmodule MMS.HeadersTest do
   use MMS.TestExamples,
       codec: Headers,
       examples: [
-        {<<0x81,   "x", 0>>, [{MMS.Bcc,             "x"         }]},
-        {<<0x82,   "x", 0>>, [{MMS.Cc,              "x"         }]},
-        {<<0x83,   "x", 0>>, [{MMS.ContentLocation, "x"         }]},
+        {<<0x81, "x", 0>>, [{MMS.Bcc,             "x"         }]},
+        {<<0x82, "x", 0>>, [{MMS.Cc,              "x"         }]},
+        {<<0x83, "x", 0>>, [{MMS.ContentLocation, "x"         }]},
 
-        {<<0x85,     1, 0>>, [{MMS.Date,     0}]},
-        {<<0x86,  128   >>, [{MMS.DeliveryReport,     :yes}]},
+        {<<0x85,   1, 0>>, [{MMS.Date,            0           }]},
+        {<<0x86, 128   >>, [{MMS.DeliveryReport,  :yes        }]},
 
-        {<<0x8c,  128    >>, [{MMS.MessageType,     :m_send_conf}]},
-        {<<0x8e,    1,  0>>, [{MMS.MessageSize,     0           }]},
-        {<<0x8f,  128    >>, [{MMS.Priority,        :low        }]},
+        {<<0x8c, 128   >>, [{MMS.MessageType,     :m_send_conf}]},
+        {<<0x8e,   1, 0>>, [{MMS.MessageSize,     0           }]},
+        {<<0x8f, 128   >>, [{MMS.Priority,        :low        }]},
 
         # Multiple headers
         {<<0x81, "x", 0, 0x81, "y", 0>>, [{MMS.Bcc, "x"}, {MMS.Bcc, "y"}]},
