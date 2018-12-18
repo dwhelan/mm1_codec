066adaad99160e4d49ee329237c62f288fab1fcb
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-18 01:17:43 -0500

    r remove Mapper dependency in Seconds

--- lib/data_types/seconds.ex
+++ lib/data_types/seconds.ex
@@ -1,15 +1,14 @@
 defmodule MMS.Seconds do
   import MMS.OkError
 
-  alias MMS.{Length, Short, Mapper, Long}
+  alias MMS.{Length, Byte, Long}
 
-  @decode_map %{0 => :absolute, 1 => :relative}
-
-  @encode_map MMS.Mapper.reverse @decode_map
+  @absolute 128
+  @relative 129
 
   def decode bytes do
     with {:ok, {length,   absolute_bytes}} <- Length.decode(bytes),
-         {:ok, {absolute, value_bytes   }} <- Mapper.decode(absolute_bytes, Short, @decode_map),
+         {:ok, {absolute, value_bytes   }} <- Byte.decode(absolute_bytes),
          {:ok, {seconds,  rest          }} <- Long.decode(value_bytes),
          :ok                               <- check_length(length, absolute_bytes, rest)
     do
@@ -27,27 +26,26 @@ defmodule MMS.Seconds do
     {:error, :incorrect_length}
   end
 
-  defp value seconds, :absolute do
+  defp value seconds, @absolute do
     DateTime.from_unix! seconds
   end
 
-  defp value seconds, :relative do
+  defp value seconds, @relative do
     seconds
   end
 
   def encode %DateTime{} = date_time do
-    encode DateTime.to_unix(date_time), :absolute
+    encode DateTime.to_unix(date_time), @absolute
   end
 
   def encode(value) when is_integer(value) do
-    encode value, :relative
+    encode value, @relative
   end
 
   defp encode value, absolute do
-    with {:ok, absolute_bytes} <- Mapper.encode(absolute, Short, @encode_map),
+    with {:ok, absolute_bytes} <- Byte.encode(absolute),
          {:ok, value_bytes   } <- Long.encode(value),
-         {:ok, length_bytes } <- Length.encode(1 + byte_size(value_bytes))
-
+         {:ok, length_bytes  } <- Length.encode(1 + byte_size(value_bytes))
     do
       ok length_bytes <> absolute_bytes <> value_bytes
     else
--- test/data_types/seconds_test.exs
+++ test/data_types/seconds_test.exs
@@ -19,7 +19,7 @@ defmodule MMS.SecondsTest do
 
       decode_errors: [
         {<<32>>,            :first_byte_must_be_less_than_32}, # length error
-        {<<3,   0,  1, 0>>, :most_signficant_bit_must_be_1  }, # absolute/relative error
+#        {<<3,   0,  1, 0>>, :most_signficant_bit_must_be_1  }, # absolute/relative error
         {<<3, 128, 31, 0>>, :length_must_be_between_1_and_30}, # value error
       ],
 
