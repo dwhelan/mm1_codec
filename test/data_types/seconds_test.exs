30ef92255151b61f26d32c08e0e06f7dc762d363
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-18 01:37:25 -0500

    t test Long length

--- test/data_types/seconds_test.exs
+++ test/data_types/seconds_test.exs
@@ -6,15 +6,15 @@ defmodule MMS.SecondsTest do
   time_zero    = DateTime.from_unix!(0)
   length_quote = 31
 
+  thirty_0xffs = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+
   use MMS.TestExamples,
       codec: Seconds,
       examples: [
         {<<3, 128, 1, 0>>, time_zero}, # short length, absolute
         {<<3, 129, 1, 0>>, 0        }, # short length, relative
 
-#        {<<3, 128, 1, 0>>, time_zero}, # uint32 length, absolute
-        # uint32 length
-#        {<<31, 32, 128, 1, 0>>, {time_zero, 32}},
+        {<<length_quote, 32, 129, 30, thirty_0xffs::240>>, thirty_0xffs}, # uint32 length, absolute
       ],
 
       decode_errors: [
