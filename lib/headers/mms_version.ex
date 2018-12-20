f5e1428d5b85f3c89812d271891694266cd01c45
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-20 02:41:56 -0500

    f MMSVersion fully working

--- lib/headers/mms_version.ex
+++ lib/headers/mms_version.ex
@@ -15,18 +15,29 @@ defmodule MMS.MMSVersion do
   end
 
   defp do_encode [major, minor] do
-    ok <<1::1, major(major)::3, minor(minor)::4>>
+    with major when is_integer(major) <- int(major,  7),
+         minor when is_integer(minor) <- int(minor, 14)
+    do
+      ok <<1::1, major::3, minor::4>>
+    else
+      _ -> error :invalid_version_string
+    end
   end
 
   defp do_encode [major] do
-    ok <<1::1, major(major)::3, 15::4>>
+    with major when is_integer(major) <- int(major,  7)
+    do
+      ok <<1::1, major::3, 15::4>>
+    else
+      _ -> error :invalid_version_string
+    end
   end
 
-  defp major string do
-    major = String.to_integer string
+  defp int string, max do
+    string |> String.to_integer |> check_max(max)
   end
 
-  defp minor string do
-    minor = String.to_integer string
+  defp check_max value, max do
+    if value >=0 && value <= max, do: value, else: :error
   end
 end
--- test/headers/mms_version_test.exs
+++ test/headers/mms_version_test.exs
@@ -4,13 +4,18 @@ defmodule MMS.MMSVersionTest do
   use MMS.TestExamples,
       codec: MMS.MMSVersion,
       examples: [
-        #    s
-        {<<0b10010000>>, "1.0"},
-        {<<0b10010001>>, "1.1"},
-        {<<0b10011111>>, "1"  },
+        #     ___ major - 3 bits
+        #        ____ minor - 4 bits
+        {<<0b10011111>>, "1"   },
+        {<<0b10010000>>, "1.0" },
+        {<<0b10010001>>, "1.1" },
+        {<<0b11111110>>, "7.14"}, # max version
       ],
 
-      decode_errors: [
+      encode_errors: [
+        {"8",    :invalid_version_string},
+        {"8.0",  :invalid_version_string},
+        {"0.15", :invalid_version_string},
       ]
 end
 
