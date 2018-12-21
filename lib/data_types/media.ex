fa409ad8a6b10e1d3a3da7138c6c6f5bd39df92b
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-21 15:44:38 -0500

    f Media now supports extended media

--- lib/data_types/media.ex
+++ lib/data_types/media.ex
@@ -1,30 +1,21 @@
 defmodule MMS.Media do
-  alias MMS.{WellKnownMedia, Short, Long}
+  alias MMS.{WellKnownMedia, String, Short, Long}
 
   import MMS.OkError
   import MMS.DataTypes
 
-  def decode(<<byte, _::binary>> = bytes) when is_short(byte) do
+  def decode(<<byte, _::binary>> = bytes) when is_short_byte(byte) do
     WellKnownMedia.decode bytes
   end
 
-#  def decode(<<byte, _::binary>> = bytes) when is_long_byte(byte) do
-#    Long.decode bytes
-#  end
-#
-#  def decode _ do
-#    error :invalid_integer
-#  end
-
-  def encode(value) do
-    WellKnownMedia.encode value
+  def decode bytes do
+    String.decode bytes
   end
 
-#  def encode(value) when is_long(value) do
-#    Long.encode value
-#  end
-#
-#  def encode _ do
-#    error :invalid_integer
-#  end
+  def encode value do
+    case WellKnownMedia.encode value do
+      {:error, _} -> String.encode value
+      ok -> ok
+    end
+  end
 end
--- test/data_types/media_test.exs
+++ test/data_types/media_test.exs
@@ -4,6 +4,17 @@ defmodule MMS.MediaTest do
   use MMS.TestExamples,
       codec: MMS.Media,
       examples: [
-        {<<0x00>>, "*/*"},
+        # Well known media
+        {<<0x80>>, "*/*"},
+        {<<0xff>>, 0x7f },
+
+        # Extension media
+        {<<0>>,                <<>>         },
+        {<<0x7f, 0>>,          <<0x7f>>     },
+        {<<"other/other", 0>>, "other/other"},
+      ],
+
+      decode_errors: [
+        {<<"x">>, :missing_terminator},
       ]
 end
