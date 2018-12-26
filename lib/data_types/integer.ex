45eaa58e3d99e7723dc084b33f1d647e0ecd0b68
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-26 01:07:55 -0500

    r Integer to use OneOf

--- lib/data_types/integer.ex
+++ lib/data_types/integer.ex
@@ -1,30 +1,3 @@
 defmodule MMS.Integer do
-  alias MMS.{Short, Long}
-
-  import MMS.OkError
-  import MMS.DataTypes
-
-  def decode(<<byte, _::binary>> = bytes) when is_short_byte(byte) do
-    Short.decode bytes
-  end
-
-  def decode(<<byte, _::binary>> = bytes) when is_long_byte(byte) do
-    Long.decode bytes
-  end
-
-  def decode _ do
-    error :invalid_integer
-  end
-
-  def encode(value) when is_short(value) do
-    Short.encode value
-  end
-
-  def encode(value) when is_long(value) do
-    Long.encode value
-  end
-
-  def encode _ do
-    error :invalid_integer
-  end
+  use MMS.OneOf, codecs: [MMS.Short, MMS.Long]
 end
