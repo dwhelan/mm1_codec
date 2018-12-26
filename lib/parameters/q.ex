b05936d5f689e17898fae2bb858faf3ccad81d0a
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-26 01:45:37 -0500

    w

--- lib/parameters/q.ex
+++ lib/parameters/q.ex
@@ -1,10 +1,7 @@
 defmodule MMS.Q do
-  use Bitwise
+  import MMS.OkError
 
   alias MMS.Uint32
-
-  import MMS.OkError
-  import MMS.DataTypes
 
   def decode bytes do
     case_ok Uint32.decode bytes do
@@ -31,10 +28,10 @@ defmodule MMS.Q do
   end
 
   defp unmap(string) when byte_size(string) <= 4 do
-    round(:erlang.binary_to_float(string) * 100 + 1)
+    round :erlang.binary_to_float(string) * 100 + 1
   end
 
   defp unmap(string) do
-    round(:erlang.binary_to_float(string) * 1000 + 100)
+    round :erlang.binary_to_float(string) * 1000 + 100
   end
 end
