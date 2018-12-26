7e17a090a4381ffe7d55b03eabf7a62f22a9cd9b
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-26 17:11:03 -0500

    r WellKnownParameters values to be a keyword list

--- lib/well_known_parameters.ex
+++ lib/well_known_parameters.ex
@@ -1,9 +1,14 @@
 defmodule MMS.CodecMapper2 do
+  def indexed(values) when is_list(values) do
+    values |> Enum.with_index(128) |> Enum.reduce(%{}, fn {v, i}, map -> Map.put(map, i, v) end)
+  end
+
   defmacro __using__ opts \\ [] do
     quote bind_quoted: [opts: opts] do
+      import MMS.CodecMapper2
       import MMS.OkError
 
-      @decode_map  opts[:map]
+      @decode_map opts[:map] || indexed(opts[:values])
       @codec_bytes Map.keys @decode_map
       @modules       @decode_map |> Map.values |> Enum.map(& elem(&1, 0))
 
@@ -59,9 +64,9 @@ defmodule MMS.WellKnownParameters do
   # The byte keys below are expressed as integers so they start at 128 (short-integer 0)
 
   use MMS.CodecMapper2,
-      map: %{
-         128 => {:q, MMS.Q},
-         129 => {:charset, MMS.Charset},
-      },
+      values: [
+        q:       MMS.Q,
+        charset: MMS.Charset,
+      ],
       error: :invalid_well_known_parameter
 end
