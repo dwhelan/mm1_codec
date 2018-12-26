0e4d54dd70b0bc488459feddd55d865c7e4eb26b
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-26 01:29:15 -0500

    r ReplyCharging to use values rather than map

--- lib/data_types/string.ex
+++ lib/data_types/string.ex
@@ -3,15 +3,11 @@ defmodule MMS.String do
   import MMS.DataTypes
 
   def decode(<<byte, _::binary>> = bytes) when is_char (byte) do
-    do_decode bytes
+    bytes |> String.split(<<0>>, parts: 2) |> decode_parts
   end
 
   def decode _ do
     error :invalid_string
-  end
-
-  def do_decode(<<byte, _::binary>> = bytes) when is_char (byte) do
-    bytes |> String.split(<<0>>, parts: 2) |> decode_parts
   end
 
   defp decode_parts [string | [rest]] do
@@ -23,14 +19,10 @@ defmodule MMS.String do
   end
 
   def encode(string) when is_binary(string) do
-    do_encode string
+    ok string <> <<0>>
   end
 
   def encode _ do
     error :invalid_string
-  end
-
-  def do_encode(string) when is_binary(string) do
-    ok string <> <<0>>
   end
 end
--- lib/headers/reply_charging.ex
+++ lib/headers/reply_charging.ex
@@ -1,10 +1,11 @@
 defmodule MMS.ReplyCharging do
   use MMS.Mapper,
       codec: MMS.Byte,
-      map: %{
-        128 => :requested,
-        129 => :requested_text_only,
-        130 => :accepted,
-        131 => :accepted_text_only,
-      }
+      values: [
+        :requested,
+        :requested_text_only,
+        :accepted,
+        :accepted_text_only,
+      ],
+      offset: 128
 end
