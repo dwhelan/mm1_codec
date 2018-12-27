e34830c0221d02a1d8045a9a6119eb8502d7392f
Declan Whelan <dwhelan@dpwhelan.com>
2018-12-27 01:01:56 -0500

    r extracted DateTime data type

--- lib/data_types/seconds.ex
+++ lib/data_types/seconds.ex
@@ -1,19 +1,3 @@
-defmodule MMS.DateTime do
-  import MMS.OkError
-
-  alias MMS.Long
-
-  def decode bytes do
-    case_ok Long.decode bytes do
-      {seconds, rest} -> ok DateTime.from_unix!(seconds), rest
-    end
-  end
-
-  def encode %DateTime{} = date_time do
-    date_time |> DateTime.to_unix |> Long.encode
-  end
-end
-
 defmodule MMS.Seconds do
   import MMS.OkError
 
