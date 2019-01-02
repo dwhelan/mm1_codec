8b30ae08fe19336d33cbc86deaf5f23aec384bba
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-02 16:58:20 -0500

    w

--- test/data_types/byte_test.exs
+++ test/data_types/byte_test.exs
@@ -3,14 +3,15 @@ defmodule MMS.ByteTest do
 
   use MMS.TestExamples,
       codec: MMS.Byte,
+
       examples: [
-        {<<0>>,     0},
-        {<<255>>, 255},
+        { <<0>>,   0   },
+        { <<255>>, 255 },
       ],
 
       encode_errors: [
-        {  -1, :invalid_byte},
-        { 256, :invalid_byte},
-        { "x", :invalid_byte},
+        { -1,  :invalid_byte },
+        { 256, :invalid_byte },
+        { "x", :invalid_byte },
       ]
 end
