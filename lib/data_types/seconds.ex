0aceebcc59bb4653dbf6966da839e3c5153052dc
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-03 18:22:16 -0500

    r reformatting

--- lib/data_types/seconds.ex
+++ lib/data_types/seconds.ex
@@ -1,38 +1,21 @@
 defmodule MMS.Seconds do
   use MMS.Codec
 
-  alias MMS.Composer
+  import MMS.Composer
 
   @absolute 0
   @relative 1
 
   @codecs [MMS.Short, MMS.Long]
 
-  def decode bytes do
-    bytes |> Composer.decode(@codecs) <~> to_seconds
-  end
+  def decode(bytes), do: bytes |> decode(@codecs) <~> to_seconds
 
-  defp to_seconds {seconds, @absolute} do
-    DateTime.from_unix! seconds
-  end
+  def encode(%DateTime{} = date_time), do: date_time |> DateTime.to_unix |> from_seconds(@absolute)
+  def encode(seconds),                 do: seconds |> from_seconds(@relative)
 
-  defp to_seconds {seconds, @relative} do
-    seconds
-  end
+  defp to_seconds({seconds, @absolute}), do: seconds |> DateTime.from_unix!
+  defp to_seconds({seconds, @relative}), do: seconds
+  defp to_seconds(_),                    do: error()
 
-  defp to_seconds _ do
-    error()
-  end
-
-  def encode %DateTime{} = date_time do
-    from_seconds DateTime.to_unix(date_time), @absolute
-  end
-
-  def encode seconds do
-    from_seconds seconds, @relative
-  end
-
-  defp from_seconds seconds, absolute do
-    Composer.encode [absolute, seconds], @codecs
-  end
+  defp from_seconds(seconds, absolute),  do: [absolute, seconds] |> encode(@codecs) ~>> module_error
 end
--- test/data_types/seconds_test.exs
+++ test/data_types/seconds_test.exs
@@ -1,29 +1,31 @@
 defmodule MMS.SecondsTest do
   use MMS.Test
 
-  alias MMS.Seconds
-
   length_quote = 31
 
+  abs = s(0)
+  rel = s(1)
+
   use MMS.TestExamples,
-      codec: Seconds,
+      codec: MMS.Seconds,
 
       examples: [
-        {<<3, 128, 1, 0>>, DateTime.from_unix!(0)}, # short length, absolute
-        {<<3, 129, 1, 0>>, 0                     }, # short length, relative
+        { << l(3), abs, l(1), 0 >>, DateTime.from_unix!(0) }, # short length, absolute
+        { << l(3), rel, l(1), 0 >>, 0                      }, # short length, relative
 
-        {<<length_quote, 32, 129>> <> max_long_bytes(), max_long()}, # uint32 length, relative
+        { <<length_quote, l(32), rel>> <> max_long_bytes(), max_long() }, # uint32 length, relative
+
         # Note: cannot test an absolute max_long converted to a DateTime because it is too large for DateTime.from_unix
       ],
 
       decode_errors: [
-        {<<32>>,            :invalid_seconds }, # length error
-        {<<3,   0,  1, 0>>, :invalid_seconds }, # absolute/relative token error
-        {<<3, 128,  0, 0>>, :invalid_seconds }, # value error
+        { << l(32) >>,               :invalid_seconds }, # length error
+        { << l(3), 0,    l(1), 0 >>, :invalid_seconds }, # absolute/relative token error
+        { << l(3), abs, l(0)    >>, :invalid_seconds }, # value error
       ],
 
       encode_errors: [
-        {-1,             :invalid_long},
-        {max_long() + 1, :invalid_long},
+        { -1,             :invalid_seconds },
+        { max_long() + 1, :invalid_seconds },
       ]
 end
