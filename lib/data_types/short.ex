f41f1aaf67faa7cb7561a879489e0f25abe8e1a3
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-03 04:24:16 -0500

    r use Codec

--- lib/data_types/short.ex
+++ lib/data_types/short.ex
@@ -8,30 +8,15 @@ defmodule MMS.Short do
     significant bit set to one (1xxx xxxx) and with the value in the remaining
     least significant bits.
   """
-  import MMS.OkError
-  import MMS.DataTypes
+  use MMS.Codec
 
-  def decode <<1::1, _::bitstring>> = bytes do
-    do_decode bytes
-  end
-
-  def decode _ do
-    error :invalid_short
-  end
-
-  def do_decode <<1::1, value::7, rest::binary>> do
+  def decode <<1::1, value::7, rest::binary>> do
     ok value, rest
   end
 
   def encode(value) when is_short(value) do
-    do_encode value
-  end
-
-  def encode _ do
-    error :invalid_short
-  end
-
-  def do_encode(value) when is_short(value) do
     ok <<value+128>>
   end
+
+  defaults()
 end
