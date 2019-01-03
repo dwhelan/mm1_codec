538a19fd28e8f81cd40a157eff687338829d26e6
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-03 15:58:39 -0500

    r clean up tests

--- test/data_types/version_test.exs
+++ test/data_types/version_test.exs
@@ -3,15 +3,21 @@ defmodule MMS.VersionTest do
 
   use MMS.TestExamples,
       codec: MMS.Version,
+
       examples: [
-        #     ___ major - 3 bits
-        #        ____ minor - 4 bits - all ones means major only
-        {<<0b10000000>>,  {0,  0}},
-        {<<"beta 1", 0>>,   "beta 1"},
+        #     _ '1' indicates short byte
+        #      ___ major - 3 bits
+        #         ____ minor - 4 bits - all ones means major only
+        { <<0b10000000>>, {0, 0}   },
+        { "beta 1\0",     "beta 1" },
+      ],
+
+      decode_errors: [
+        { "no terminator", :invalid_version},
       ],
 
       encode_errors: [
-        {:x,       :invalid_version},
+        {:not_a_version, :invalid_version},
       ]
 end
 
