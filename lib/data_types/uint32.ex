2ba5cad557dd8a52900e05bc458c489d78c31d24
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-03 14:47:43 -0500

    r minor refactoring

--- lib/data_types/uint32.ex
+++ lib/data_types/uint32.ex
@@ -10,28 +10,24 @@ defmodule MMS.Uint32 do
     do_decode bytes, []
   end
 
-  defp do_decode<<1::1, value::7, rest::binary>>, values do
-    do_decode rest, [value | values]
+  defp do_decode<<1::1, next::7, rest::binary>>, values do
+    do_decode rest, [next | values]
   end
 
-  defp do_decode <<value, rest::binary>>, values do
-    ok_if_uint32 value |> total(values), rest
+  defp do_decode <<last, rest::binary>>, values do
+    sum([last |values]) |> ensure_uint32 ~> ok(rest)
   end
 
-  defp total value, values do
-    [value | values] |> Enum.reverse |> Enum.reduce(& &1 + (&2 <<< 7))
+  defp sum values do
+    values |> Enum.reverse |> Enum.reduce(& &1 + (&2 <<< 7))
   end
 
-  defp ok_if_uint32(value, rest) when is_uint32(value) do
-    ok value, rest
-  end
-
-  defp ok_if_uint32 _, _ do
-    error()
+  defp ensure_uint32 value do
+    if is_uint32(value), do: value, else: error()
   end
 
   def encode(value) when is_uint32(value) do
-    ok do_encode value |> shift7, value |> encode_lsb7(0)
+    ok do_encode shift7(value), encode_lsb7(value, 0)
   end
 
   defp do_encode 0, bytes do
@@ -39,7 +35,7 @@ defmodule MMS.Uint32 do
   end
 
   defp do_encode value, bytes do
-    do_encode value |> shift7, (value |> encode_lsb7(1)) <> bytes
+    do_encode shift7(value), encode_lsb7(value, 1) <> bytes
   end
 
   defp encode_lsb7 value, continue do
