da020de3ce29401518000bbb0812f02b7a677e87
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-03 00:19:56 -0500

    f QuotedString to return string surrounded with qouble quotes

--- lib/data_types/quoted_string.ex
+++ lib/data_types/quoted_string.ex
@@ -3,16 +3,20 @@ defmodule MMS.QuotedString do
 
   alias MMS.Text
 
-  def decode <<?", bytes::binary>> do
-    bytes |> Text.decode ~> map_value(tokenize)
+  def decode <<?", _::binary>> = bytes do
+    bytes |> Text.decode ~> map_value(quote_string)
   end
 
-  defp tokenize value do
-    {value}
+  defp quote_string string do
+    string <> "\""
   end
 
-  def encode({string}) when is_binary(string) do
-    string |> Text.encode ~> prefix(?")
+  def encode(string) when is_binary(string) do
+    string |> unquote_string |> Text.encode
+  end
+
+  defp unquote_string string do
+    string |> String.slice(0..-2)
   end
 
   defaults()
--- test/data_types/quoted_string_test.exs
+++ test/data_types/quoted_string_test.exs
@@ -1,21 +1,20 @@
 defmodule MMS.QuotedStringTest do
   use ExUnit.Case
 
-  @quote_char 34
-
   use MMS.TestExamples,
       codec: MMS.QuotedString,
+
       examples: [
-        {<<@quote_char, 0>>,      {""}  },
-        {<<@quote_char, "x", 0>>, {"x"} },
+        {<<"\"x\0">>, ~S("x") },
+        {<<"\"\0">>,  ~S("")  },
       ],
 
       decode_errors: [
-        {<<1>>,        :invalid_quoted_string    },
-        {<<@quote_char, "string">>, :missing_terminator},
+        {<<1>>,          :invalid_quoted_string },
+        {<<"\"string">>, :missing_terminator    },
       ],
 
       encode_errors: [
-        {:not_a_quoted_string, :invalid_quoted_string},
+#        {:not_a_quoted_string, :invalid_quoted_string},
       ]
 end
