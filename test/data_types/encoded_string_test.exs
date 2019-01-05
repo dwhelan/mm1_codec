51297f600a95455956c21c67de78636c6210ce97
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-04 21:47:42 -0500

    r whitespace and renames

--- test/data_types/encoded_string_test.exs
+++ test/data_types/encoded_string_test.exs
@@ -1,11 +1,13 @@
 defmodule MMS.EncodedStringTest do
   use MMS.Test
 
-  string30     = String.duplicate "x", 30
+  alias MMS.EncodedString
+
+string30     = String.duplicate "x", 30
   length_quote = 31
 
   use MMS.TestExamples,
-      codec: MMS.EncodedString,
+      codec: EncodedString,
 
       examples: [
         # Not encoded
@@ -13,8 +15,8 @@ defmodule MMS.EncodedStringTest do
         { <<"x\0">>, "x" },
 
         # Encoded with short length
-        { << l(3), s(106), "x\0" >>,            {"x", :csUTF8,  } },
-        { << l(5), l(2), 1000::16, "x\0" >>, {"x", :csUnicode} },
+        { << l(3), s(106), "x\0" >>,         {"x", :csUTF8   } }, # short charset
+        { << l(5), l(2), 1000::16, "x\0" >>, {"x", :csUnicode} }, # long charset
 
         # Encoded with uint32 length
         { << length_quote, l(32), s(106) >> <> string30 <> <<0>>, {string30, :csUTF8} },
@@ -28,12 +30,12 @@ defmodule MMS.EncodedStringTest do
         { << length_quote, l(33), s(106)>> <> string30 <> <<0>>, :invalid_encoded_string }, # uint32 length
       ]
 
-  test "map(string)" do
-    assert MMS.EncodedString.map("x", &String.upcase/1) == ok "X"
+  test "map(string, fun)" do
+    assert EncodedString.map("x", &String.upcase/1) == ok "X"
   end
 
-  test "map({string, charset})" do
-    assert MMS.EncodedString.map({"x", :utf8}, &String.upcase/1) == ok {"X", :utf8}
+  test "map({string, charset}, fun)" do
+    assert EncodedString.map({"x", :utf8}, &String.upcase/1) == ok {"X", :utf8}
   end
 end
 
