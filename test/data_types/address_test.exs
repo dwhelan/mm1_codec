ccfeca566bb1ddb2deb414671f41c4c70f7dd954
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-04 21:42:47 -0500

    t added tests for all address types with charset

--- test/data_types/address_test.exs
+++ test/data_types/address_test.exs
@@ -5,21 +5,28 @@ defmodule MMS.AddressTest do
       codec: MMS.Address,
 
       examples: [
+        # address
         { "email@address\0",         "email@address"          },
         { "1234567890/TYPE=PLMN\0",  "1234567890"             },
         { "value/TYPE=unknown\0",    {"value", "unknown"}     },
         { "0.0.0.0/TYPE=IPv4\0",     {0, 0, 0, 0}             },
         { ":/TYPE=IPv6\0",           {0, 0, 0, 0, 0, 0, 0, 0} },
-        { << l(3), s(106), "@\0" >>, {"@", :csUTF8}           },
+
+        #    length charset address
+        { << l(3),  s(106), "@\0" >>,                 {"@",                      :csUTF8} },
+        { << l(13), s(106), "1/TYPE=PLMN\0" >>,       {"1",                      :csUTF8} },
+        { << l(10), s(106), "v/TYPE=t\0" >>,          {{"v", "t"},               :csUTF8} },
+        { << l(19), s(106), "0.0.0.0/TYPE=IPv4\0" >>, {{0, 0, 0, 0},             :csUTF8} },
+        { << l(13), s(106), ":/TYPE=IPv6\0" >>,       {{0, 0, 0, 0, 0, 0, 0, 0}, :csUTF8} },
       ],
 
       decode_errors: [
-        { "x\0",                    :invalid_address },
-        { "@/TYPE=PLMN\0",          :invalid_address },
-        { "x.0.0.0/TYPE=IPv4\0",    :invalid_address },
-        { "::x/TYPE=IPv6\0",        :invalid_address },
-        { "@",                      :invalid_address },
-        { << l(3), s(106), "@"  >>, :invalid_address },
+        { "x\0",                   :invalid_address },
+        { "@/TYPE=PLMN\0",         :invalid_address },
+        { "x.0.0.0/TYPE=IPv4\0",   :invalid_address },
+        { "::x/TYPE=IPv6\0",       :invalid_address },
+        { "@",                     :invalid_address },
+        { << l(3), s(106), "@" >>, :invalid_address },
       ],
 
       encode_errors: [
