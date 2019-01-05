9884bdad679c9b425652a4d3cca60b6c52ef8b9e
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-05 03:41:38 -0500

    r use while_error macro

--- lib/either.ex
+++ lib/either.ex
@@ -6,27 +6,11 @@ defmodule MMS.Either do
       @codecs opts[:codecs] || []
 
       def decode bytes do
-        decode bytes, @codecs
-      end
-
-      defp decode _, [] do
-        error()
-      end
-
-      defp decode bytes, [codec | codecs] do
-        bytes |> codec.decode ~>> cons decode(bytes, codecs)
+        while_error @codecs, & &1.decode(bytes) ~>> module_error()
       end
 
       def encode value do
-        do_encode value, @codecs
-      end
-
-      defp do_encode _, [] do
-        error()
-      end
-
-      defp do_encode value, [codec | codecs] do
-        value |> codec.encode ~>> cons do_encode(value, codecs)
+        while_error @codecs, & &1.encode(value) ~>> module_error()
       end
     end
   end
