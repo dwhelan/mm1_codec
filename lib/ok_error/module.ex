b7b312895b532af005e7dbf5d30f2e793cd65fa1
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-06 18:19:49 -0500

    r simplified atom declaration

--- lib/ok_error/module.ex
+++ lib/ok_error/module.ex
@@ -2,13 +2,20 @@ defmodule OkError.Module do
 
   defmacro atom module \\ __CALLER__.module do
     quote do
-      unquote(module) |> to_string |> String.split(".") |> List.last |> Macro.underscore |> String.to_atom
+      unquote(module)
+      |> to_string
+      |> String.split(".")
+      |> List.last
+      |> Macro.underscore
+      |> String.replace(~r/^([a-z)])_/, "\\1")
+      |> String.replace(~r/(_[a-z)])_/, "\\1")
+      |> String.to_atom
     end
   end
 
   defmacro error_atom module \\ __CALLER__.module do
     quote do
-      "invalid_#{OkError.Module.atom unquote(module)}" |> String.replace(~r/(_[a-z)])_/, "\\1") |> String.to_atom
+      "invalid_#{OkError.Module.atom unquote(module)}" |> String.to_atom
     end
   end
 end
--- test/ok_error/module_test.exs
+++ test/ok_error/module_test.exs
@@ -3,10 +3,6 @@ defmodule OkError.ModuleTest do
 
   import OkError.Module
 
-  test "atom() macro should return module atom of caller" do
-    assert atom() == :module_test
-  end
-
   describe "atom(module) macro should" do
     test "return lower case atom" do
       assert atom(ABC) == :abc
@@ -16,13 +12,30 @@ defmodule OkError.ModuleTest do
       assert atom(A.B.C) == :c
     end
 
-    test "preserve acronyms" do
-      assert atom(GDP) == :gdp
+    test "convert to underscore formar" do
+      assert atom(OkError) == :ok_error
+    end
+
+    test "merge initial single letter with next part" do
+      assert atom(IPv4) == :ipv4
+    end
+
+    test "merge other single letters with next part" do
+      assert atom(ProtocolIPv4) == :protocol_ipv4
+    end
+
+    test "default to caller module" do
+      assert atom() == :module_test
     end
   end
 
-  test "error_atom() macro should prepend 'invalid_' to module atom" do
-    assert error_atom() == :invalid_module_test
-  end
+  describe "error atom(module) macro should" do
+    test "prepend 'invalid_' to module atom" do
+      assert error_atom(ABC) == :invalid_abc
+    end
 
+    test "default to caller module" do
+      assert error_atom() == :invalid_module_test
+    end
+  end
 end
