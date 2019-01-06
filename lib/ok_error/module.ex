5483ce9d4c1b3390638005e456bc4b94557d4242
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-06 18:01:05 -0500

    r simplified atom/1 and error_atom/1

--- lib/ok_error/module.ex
+++ lib/ok_error/module.ex
@@ -1,32 +1,14 @@
 defmodule OkError.Module do
 
-  defmacro atom module do
-    build_atom module
-  end
-
-  defmacro atom do
-    build_atom __CALLER__.module
-  end
-
-  defp build_atom module do
+  defmacro atom module \\ __CALLER__.module do
     quote do
       unquote(module) |> to_string |> String.split(".") |> List.last |> Macro.underscore |> String.to_atom
     end
   end
 
-  defmacro error_atom module do
-    build_error_atom module
-  end
-
-  defmacro error_atom do
-    build_error_atom __CALLER__.module
-  end
-
-  defp build_error_atom module do
-    atom = build_atom module
-
+  defmacro error_atom module \\ __CALLER__.module do
     quote do
-      "invalid_#{unquote atom}" |> String.replace(~r/(_[a-z)])_/, "\\1") |> String.to_atom
+      "invalid_#{OkError.Module.atom unquote(module)}" |> String.replace(~r/(_[a-z)])_/, "\\1") |> String.to_atom
     end
   end
 end
