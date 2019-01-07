77447a0b6f8d461b073a91ba4491738848aa8ba0
Declan Whelan <dwhelan@dpwhelan.com>
2019-01-06 20:55:42 -0500

    r atom -> name

--- lib/ok_error/module.ex
+++ lib/ok_error/module.ex
@@ -1,21 +1,21 @@
 defmodule OkError.Module do
+  require OkError.String
 
-  defmacro atom module \\ __CALLER__.module do
+  defmacro name module \\ __CALLER__.module do
     quote do
       unquote(module)
       |> to_string
       |> String.split(".")
       |> List.last
+      |> OkError.String.pascalcase
       |> Macro.underscore
-      |> String.replace(~r/^([a-z)])_/, "\\1")
-      |> String.replace(~r/(_[a-z)])_/, "\\1")
       |> String.to_atom
     end
   end
 
-  defmacro error_atom module \\ __CALLER__.module do
+  defmacro error_name module \\ __CALLER__.module do
     quote do
-      "invalid_#{OkError.Module.atom unquote(module)}" |> String.to_atom
+      "invalid_#{OkError.Module.name unquote(module)}" |> String.to_atom
     end
   end
 end
--- test/ok_error/module_test.exs
+++ test/ok_error/module_test.exs
@@ -3,40 +3,40 @@ defmodule OkError.ModuleTest do
 
   import OkError.Module
 
-  describe "atom(module) macro should" do
-    test "return lower case atom" do
-      assert atom(A) == :a
+  describe "name(module) macro should" do
+    test "return lower case name" do
+      assert name(A) == :a
     end
 
     test "only consider part after last '.'" do
-      assert atom(A.B.C) == :c
+      assert name(A.B.C) == :c
     end
 
     test "convert to underscore formay" do
-      assert atom(OkError) == :ok_error
+      assert name(OkError) == :ok_error
     end
 
     test "keep consecutive upper case letters together" do
-      assert atom(ABc) == :abc
-#      assert atom(ABCd) == :abcd
+      assert name(ABc) == :abc
+#      assert name(ABCd) == :abcd
     end
 
     test "merge other single letters with next part" do
-      assert atom(ProtocolIPv4) == :protocol_ipv4
+      assert name(ProtocolIPv4) == :protocol_ipv4
     end
 
     test "default to caller module" do
-      assert atom() == :module_test
+      assert name() == :module_test
     end
   end
 
-  describe "error atom(module) macro should" do
-    test "prepend 'invalid_' to module atom" do
-      assert error_atom(ABC) == :invalid_abc
+  describe "error name(module) macro should" do
+    test "prepend 'invalid_' to module name" do
+      assert error_name(ABC) == :invalid_abc
     end
 
     test "default to caller module" do
-      assert error_atom() == :invalid_module_test
+      assert error_name() == :invalid_module_test
     end
   end
 end
