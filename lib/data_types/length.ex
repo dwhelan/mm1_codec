341d42f57788d85f7a3b4f2f3dfccf0af6efb246
Declan Whelan <dwhelan@dpwhelan.com>
2019-02-10 21:31:45 -0500

    r removed explicit error code

--- lib/data_types/length.ex
+++ lib/data_types/length.ex
@@ -1,5 +1,5 @@
 defmodule MMS.Length do
-  use MMS.Codec2, error: :invalid_length
+  use MMS.Codec2
 
   @length_quote 31
 
@@ -8,17 +8,17 @@ defmodule MMS.Length do
   def decode bytes = <<@length_quote, rest::binary>> do
     rest
     |> Uint32.decode
-    ~>> fn details -> error :invalid_length, bytes, details end
+    ~>> fn details -> error bytes, details end
   end
 
   def decode(bytes) when is_binary(bytes) do
-    error :invalid_length, bytes, :missing_length_quote
+    error bytes, :missing_length_quote
   end
 
   def encode value do
     value
     |> Uint32.encode
     ~> fn bytes -> <<@length_quote>> <> bytes end
-    ~>> fn details -> error :invalid_length, value, details end
+    ~>> fn details -> error value, details end
   end
 end
