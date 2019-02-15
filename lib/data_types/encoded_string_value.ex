1d96b6fb66a4a84ed8626694967de408ad16f4e5
Declan Whelan <dwhelan@dpwhelan.com>
2019-02-15 14:59:17 -0500

    r nest TextStringWithCharset inside MMS.EncodedStringValue2

--- lib/data_types/encoded_string_value.ex
+++ lib/data_types/encoded_string_value.ex
@@ -12,7 +12,7 @@ defmodule MMS.EncodedStringValue do
   [RFC2047] encoding for UTF-8 character- set encoding MAY be supported in the MMS Client and/or MMS Proxy-Relay.
   """
 
-  use MMS.Either, [MMS.Text, MMS.TextStringWithCharset]
+  use MMS.Either, [MMS.Text, MMS.EncodedStringValue2.TextStringWithCharset]
 
   def map([charset, string], fun) do
     string |> map(fun) ~> OldOkError.Tuple.insert_at({charset}, 0)
@@ -26,6 +26,8 @@ end
 defmodule MMS.EncodedStringValue2 do
   use MMS.Codec2
 
+  alias MMS.EncodedStringValue2.TextStringWithCharset
+
   def decode(<<byte, _::binary>> = bytes) when is_char(byte) do
     bytes
     |> MMS.Text.decode
@@ -34,7 +36,7 @@ defmodule MMS.EncodedStringValue2 do
 
   def decode(bytes) when is_binary(bytes) do
     bytes
-    |> MMS.TextStringWithCharset.decode
+    |> TextStringWithCharset.decode
     ~>> fn details -> error bytes, details end
   end
 
@@ -43,21 +45,22 @@ defmodule MMS.EncodedStringValue2 do
   end
 
   def encode({text, charset}) when is_binary(text) do
-    {text, charset} |> MMS.TextStringWithCharset.encode
+    {text, charset} |> TextStringWithCharset.encode
+  end
+
+  defmodule TextStringWithCharset do
+    use MMS.Codec2
+
+    alias MMS.{ValueLengthList, Charset, Text}
+
+    def decode bytes do
+      ValueLengthList.decode(bytes, [&(Charset.decode/1), &(Text.decode/1)])
+      ~> fn {[charset, text], rest} -> ok {text, charset}, rest end
+    end
+
+    def encode {text, charset} do
+      ValueLengthList.encode([charset, text], [&(Charset.encode/1), &(Text.encode/1)])
+    end
   end
 end
 
-defmodule MMS.TextStringWithCharset do
-  use MMS.Codec2
-
-  alias MMS.{ValueLengthList, Charset, Text}
-
-  def decode bytes do
-    ValueLengthList.decode(bytes, [&(Charset.decode/1), &(Text.decode/1)])
-    ~> fn {[charset, text], rest} -> ok {text, charset}, rest end
-  end
-
-  def encode {text, charset} do
-    ValueLengthList.encode([charset, text], [&(Charset.encode/1), &(Text.encode/1)])
-  end
-end
