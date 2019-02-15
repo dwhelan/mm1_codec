4a4efcbf4cfad79a2ca2668f6c542d33687ff528
Declan Whelan <dwhelan@dpwhelan.com>
2019-02-15 00:33:04 -0500

    r TextStringWithCharset to use Codec2

--- lib/data_types/text_string_with_charset.ex
+++ lib/data_types/text_string_with_charset.ex
@@ -12,6 +12,16 @@ defmodule MMS.TextStringWithCharset do
   [RFC2047] encoding for UTF-8 character- set encoding MAY be supported in the MMS Client and/or MMS Proxy-Relay.
   """
 
-  use MMS.Composer, codecs: [MMS.Charset, MMS.Text]
+  use MMS.Codec2
+#
+  alias MMS.{ValueLengthList, Charset, Text}
+
+  def decode bytes do
+    ValueLengthList.decode(bytes, [&(Charset.decode/1), &(Text.decode/1)])
+  end
+
+  def encode values = [charset, text] do
+    ValueLengthList.encode(values, [&(Charset.encode/1), &(Text.encode/1)])
+  end
 end
 
