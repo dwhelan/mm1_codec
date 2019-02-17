38966c290ec5c88f8bd32d69d5ce29f57918dc85
Declan Whelan <dwhelan@dpwhelan.com>
2019-02-17 02:40:21 -0500

    r removed unused Time modules

--- lib/data_types/time.ex
+++ lib/data_types/time.ex
@@ -1,17 +1,4 @@
-defmodule MMS.AbsoluteTime do
-  use MMS.Prefix, prefix: 128, codec: MMS.DateTime
-end
-
-defmodule MMS.RelativeTime do
-  use MMS.Prefix, prefix: 129, codec: MMS.Long
-end
-
-defmodule MMS.EitherTime do
-  use MMS.Either, [MMS.AbsoluteTime, MMS.RelativeTime]
-end
-
 defmodule MMS.Time do
-#  use MMS.Composer, codecs: [MMS.EitherTime]
   use MMS.Codec
 
   import MMS.Composer
@@ -23,12 +10,12 @@ defmodule MMS.Time do
 
   def decode(bytes), do: bytes |> decode(@codecs) <~> to_time
 
-  def encode(%DateTime{} = date_time), do: date_time |> DateTime.to_unix |> do_encode(@absolute)
-  def encode(seconds),                 do: seconds |> do_encode(@relative)
-
   defp to_time({seconds, @absolute}), do: seconds |> DateTime.from_unix!
   defp to_time({seconds, @relative}), do: seconds
   defp to_time(_),                    do: module_error()
+
+  def encode(%DateTime{} = date_time), do: date_time |> DateTime.to_unix |> do_encode(@absolute)
+  def encode(seconds),                 do: seconds |> do_encode(@relative)
 
   defp do_encode(seconds, absolute),  do: [absolute, seconds] |> encode(@codecs) ~>> module_error
 end
