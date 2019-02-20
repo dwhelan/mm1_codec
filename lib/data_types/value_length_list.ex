0f6a0f19cff04c394bb2fc6dc29d72a689280bb2
Declan Whelan <dwhelan@dpwhelan.com>
2019-02-20 00:00:35 -0500

    r Inlined do_decode/3

--- lib/data_types/value_length_list.ex
+++ lib/data_types/value_length_list.ex
@@ -6,14 +6,12 @@ defmodule MMS.ValueLengthList do
   def decode(bytes, functions) when is_binary(bytes) and is_list(functions) do
     bytes
     |> ValueLength.decode
-    ~> fn result -> result |> do_decode(functions, bytes) end
-  end
-
-  defp do_decode {length, value_bytes}, functions, bytes do
-    value_bytes
-    |> List.decode(functions)
-    ~>> fn {data_type, _, details} -> error bytes, [data_type, Map.merge(details, %{length: length})] end
-    ~> fn {values, rest} -> ensure_correct_length(length, value_bytes, values, rest, bytes) end
+    ~> fn {length, value_bytes} ->
+         value_bytes
+         |> List.decode(functions)
+         ~>> fn {data_type, _, details} -> error bytes, [data_type, Map.merge(details, %{length: length})] end
+         ~> fn {values, rest} -> ensure_correct_length(length, value_bytes, values, rest, bytes) end
+       end
   end
 
   defp ensure_correct_length(length, value_bytes, values, rest, _bytes) when length == byte_size(value_bytes) - byte_size(rest) do
@@ -21,8 +19,7 @@ defmodule MMS.ValueLengthList do
   end
 
   defp ensure_correct_length(length, value_bytes, values, rest, bytes) do
-    bytes_used = byte_size(value_bytes) - byte_size(rest)
-    error bytes, %{length: length, bytes_used: bytes_used, values: values}
+    error bytes, %{length: length, bytes_used: byte_size(value_bytes) - byte_size(rest), values: values}
   end
 
   def encode(values, functions) when is_list(values) and is_list(functions) do
