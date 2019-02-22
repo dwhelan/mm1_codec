ada8791e64bbcaf6f9a59ce6c3e5899962868296
Declan Whelan <dwhelan@dpwhelan.com>
2019-02-21 23:08:27 -0500

    r Finalize unit tests

--- test/data_types/address_test.exs
+++ test/data_types/address_test.exs
@@ -5,26 +5,18 @@ defmodule MMS.AddressTest do
       codec: MMS.Address,
 
       examples: [
-#        { "email@address\0",         {"email@address", :email}          },
-        { "1234567890/TYPE=PLMN\0",  {"1234567890", "PLMN"}             },
-        { "0.0.0.0/TYPE=IPv4\0",     {"0.0.0.0", "IPv4"}             },
-#        { ":/TYPE=IPv6\0",           {":", :ipv6} },
-#        { "address/TYPE=other\0",    {"address", "other"}     },
+        { "email@address\0",        {"email@address", ""}  },
+        { "1234567890/TYPE=PLMN\0", {"1234567890", "PLMN"} },
+        { "0.0.0.0/TYPE=IPv4\0",    {"0.0.0.0", "IPv4"}    },
+        { ":/TYPE=IPv6\0",          {":", "IPv6"}          },
+        { "address/TYPE=other\0",   {"address", "other"}   },
       ],
 
       decode_errors: [
-#        { << "x" >>,                   {:address, "x",                    [:text, :missing_end_of_string_0]} },
-#        { << "x\0" >>,                 {:address, "x\0",                 :invalid_email_address}        },
-#        { << "x.0.0.0/TYPE=IPv4\0" >>, {:address, "x.0.0.0/TYPE=IPv4\0", :ipv4_address }        },
-#        { << "::x/TYPE=IPv6\0" >>,     {:address, "::x/TYPE=IPv6\0",     :ipv6_address }        },
-        ]    ,
+        { << "x" >>, {:address, "x", [:text, :missing_end_of_string_0]} },
+      ],
 
       encode_errors: [
-#        { "email@address\0",      {:address, "email@address\0",      [:text, :contains_end_of_string_0]} },
-#        { {"x", "PLMN"},          {:address, {"x", "PLMN"},          :reserved_type} },
-#        { {"x", "IPv4"},          {:address, {"x", "IPv4"},          :reserved_type} },
-#        { {"x", "IPv6"},          {:address, {"x", "IPv6"},          :reserved_type} },
-#        { {"x", 0, 0, 0},             {:address, {"x", 0, 0, 0},             :ipv4_address}     },
-#        { {"x", 0, 0, 0, 0, 0, 0, 0}, {:address, {"x", 0, 0, 0, 0, 0, 0, 0}, :ipv6_address}     },
+        { {"email@address\0", ""}, {:address, {"email@address\0", ""}, [:text, :contains_end_of_string_0]} },
       ]
 end
