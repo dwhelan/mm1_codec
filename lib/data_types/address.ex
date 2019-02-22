26e9451b6ac52f1bee88f0ca97f74d737c2a10f7
Declan Whelan <dwhelan@dpwhelan.com>
2019-02-21 22:17:25 -0500

    r Extract proceed/1

--- lib/data_types/address.ex
+++ lib/data_types/address.ex
@@ -34,38 +34,41 @@ defmodule MMS.Address do
     ~>> fn details -> decode_error bytes, details end
   end
 
-  defp do_decode([email]),         do: {email, :email}
   defp do_decode([phone, "PLMN"]), do: phone
-  defp do_decode([ipv4, "IPv4"]),  do: {ipv4, :ipv4}
-  defp do_decode([ipv6, "IPv6"]),  do: {ipv6, :ipv6}
+  defp do_decode([ipv4,  "IPv4"]), do: {ipv4, :ipv4}
+  defp do_decode([ipv6,  "IPv6"]), do: {ipv6, :ipv6}
+  defp do_decode([email]),         do: {email, :email}
   defp do_decode([other, type]),   do: {other, type}
 
-  def encode(address = {string, type}) when is_binary(string) and type in ["PLMN", "IPv4", "IPv6"] do
-    encode_error address, :reserved_type
+  def encode(phone) when is_binary(phone) do
+    phone
+    |> proceed
   end
 
-  def encode(address = {string, type}) when is_binary(string) and type in [:email, :ipv4, :ipv6] do
-    address
-    |> do_encode
-    ~> Text.encode
-    ~>> fn details -> encode_error string, details end
+  def encode(special = {string, type}) when is_binary(string) and type in [:ipv4, :ipv6, :email] do
+    special
+    |> proceed
+  end
+
+  def encode(reserved = {string, type}) when is_binary(string) and type in ["IPv4", "IPv6", "PLMN"] do
+    encode_error reserved, :reserved_type
   end
 
   def encode(address = {string, type}) when is_binary(string) and is_binary(type) do
     address
-    |> do_encode
-    ~> Text.encode
-    ~>> fn details -> encode_error string, details end
+    |> proceed
   end
 
-  def encode(phone) when is_binary(phone) do
-    "#{phone}/TYPE=PLMN"
-    |> Text.encode
-    ~>> fn details -> encode_error phone, details end
+  def proceed address do
+    address
+    |> do_encode
+    ~> Text.encode
+    ~>> fn details -> encode_error address, details end
   end
 
   def do_encode({email, :email}), do: ok email
   def do_encode({ipv4, :ipv4})  ,  do: ok "#{ipv4}/TYPE=IPv4"
   def do_encode({ipv6, :ipv6})  ,  do: ok "#{ipv6}/TYPE=IPv6"
-#  def do_encode({address, type}) when is_binary(address) and is_binary(type),  do: ok "#{address}/TYPE=#{type}"
+  def do_encode({address, type}) when is_binary(address) and is_binary(type),  do: ok "#{address}/TYPE=#{type}"
+  def do_encode(phone)  ,  do: ok "#{phone}/TYPE=PLMN"
 end
