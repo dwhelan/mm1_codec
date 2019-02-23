5df30bd549d73ba7915824d56201d4dd6f48ff48
Declan Whelan <dwhelan@dpwhelan.com>
2019-02-22 20:24:01 -0500

    r Use decode_with, encode_with

--- lib/data_types/media.ex
+++ lib/data_types/media.ex
@@ -3,26 +3,21 @@ defmodule MMS.Media do
 
   alias MMS.{Text, WellKnownMedia}
 
-  def decode(<<char, _::binary>> = bytes) when is_text(char) do
+  def decode(<<char, _ :: binary>> = bytes) when is_text(char) do
     bytes
-    |> do_decode(Text)
+    |> decode_with(Text)
   end
 
   def decode bytes do
     bytes
-    |> do_decode(WellKnownMedia)
-  end
-
-  defp do_decode bytes, codec do
-    bytes
-    |> codec.decode
-    ~>> fn details -> decode_error bytes, details end
+    |> decode_with(WellKnownMedia)
   end
 
   def encode(string) when is_binary(string) do
     string
-    |> WellKnownMedia.encode
-    ~>> fn _ -> string |> Text.encode end
-    ~>> fn details -> encode_error string, details end
+    |> encode_with(WellKnownMedia)
+    ~>> fn _ ->
+      string
+      |> encode_with(Text) end
   end
 end
