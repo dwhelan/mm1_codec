58561b06394bbfa794a0c2865f033522c8e323c1
Declan Whelan <dwhelan@dpwhelan.com>
2019-02-23 23:33:50 -0500

    r Whitespace

--- lib/data_types/media.ex
+++ lib/data_types/media.ex
@@ -4,20 +4,16 @@ defmodule MMS.Media do
   alias MMS.{Text, WellKnownMedia}
 
   def decode(<<char, _ :: binary>> = bytes) when is_text(char) do
-    bytes
-    |> decode_with(Text)
+    bytes |> decode_with(Text)
   end
 
   def decode bytes do
-    bytes
-    |> decode_with(WellKnownMedia)
+    bytes |> decode_with(WellKnownMedia)
   end
 
   def encode(string) when is_binary(string) do
     string
     |> encode_with(WellKnownMedia)
-    ~>> fn _ ->
-      string
-      |> encode_with(Text) end
+    ~>> fn _ -> string |> encode_with(Text) end
   end
 end
