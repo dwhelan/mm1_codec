50f6fd1050d61cc3a6067797b83a60ab2482dd44
Declan Whelan <dwhelan@dpwhelan.com>
2019-02-24 16:10:21 -0500

    d Add module docs

--- lib/data_types/version.ex
+++ lib/data_types/version.ex
@@ -1,4 +1,11 @@
 defmodule MMS.Version do
+  @moduledoc """
+  Specification: WAP-230-WSP-20010705-a, 8.4.2.3 Parameter Values
+
+  Version-value = Short-integer | Text-string
+
+  The VersionInteger codec is responsible for interpreting the major and minor verions from a ShortInteger
+  """
   use MMS.Codec2
 
   alias MMS.{VersionInteger, Text}
--- lib/data_types/version_integer.ex
+++ lib/data_types/version_integer.ex
@@ -1,4 +1,17 @@
 defmodule MMS.VersionInteger do
+  @moduledoc """
+  Specification: WAP-230-WSP-20010705-a, 8.4.2.3 Parameter Values
+
+  Version-value = Short-integer | Text-string
+
+  This codec is responsible for interpreting the major and minor versions for a Short-integer version.
+
+  The three most significant bits of the Short-integer value are interpreted to encode a major version number in the range 1-7,
+  and the four least significant bits contain a minor version number in the range 0-14.
+
+  If there is only a major version number, this is encoded by placing the value 15 in the four least significant bits.
+  If the version to be encoded fits these constraints, a Short-integer must be used, otherwise a Text-string shall be used.
+  """
   use MMS.Codec2
 
   @major_range 0..7
