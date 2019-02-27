7b0d769e592a9fc97086ab43f0c736d5ca0ff96b
Declan Whelan <dwhelan@dpwhelan.com>
2019-02-27 18:02:53 -0500

    r Remove FromAddress.Old

--- lib/headers/from.ex
+++ lib/headers/from.ex
@@ -38,28 +38,3 @@ defmodule MMS.From do
     end
   end
 end
-
-defmodule MMS.From.Old do
-  use MMS.Codec
-
-  alias MMS.{Composer, ShortInteger, Address}
-
-  @address_present_token 0
-  @insert_address_token  1
-
-  def decode bytes do
-    bytes |> Composer.decode([ShortInteger, Address]) <~> address
-  end
-
-  defp address({address, @address_present_token}), do: address
-  defp address({@insert_address_token}),           do: :insert_address_token
-  defp address(_),                                 do: module_error()
-
-  def encode :insert_address_token do
-    ok <<1, @insert_address_token + 128>>
-  end
-
-  def encode string do
-    [@address_present_token, string] |> Composer.encode([ShortInteger, Address]) ~>> module_error
-  end
-end
