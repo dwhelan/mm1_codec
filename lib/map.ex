425c928ea9838a4e14b687b9448dc3224abb65db
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-01 01:10:22 -0500

    r Removed duplication in encode

--- lib/map.ex
+++ lib/map.ex
@@ -30,7 +30,6 @@ defmodule Codec.Map do
   end
 
   defmacro decode bytes, codec, mapper do
-    data_type = data_type(__CALLER__.module)
     quote bind_quoted: [bytes: bytes, mapper: mapper, codec: codec, module: __CALLER__.module] do
       bytes
       |> codec.decode
@@ -75,8 +74,8 @@ defmodule Codec.Map do
     map_codec = Macro.expand(map_codec, __CALLER__)
 
     quote bind_quoted: [value: value, map: map, map_codec: map_codec, module: __CALLER__.module] do
-      value
-      |> map_value_to_encode(map)
+      Map.get(map, value)
+      ~>> fn _ -> error :out_of_range end
       ~> fn map_value -> map_value |> map_codec.encode end
       ~>> fn details -> error data_type(module), value, nest_error(details) end
     end
@@ -88,19 +87,20 @@ defmodule Codec.Map do
     map_codec = Macro.expand(map_codec, __CALLER__)
 
     quote bind_quoted: [value: value, map: map, codec: codec, map_codec: map_codec, module: __CALLER__.module] do
-      case value |> map_value_to_encode(map) do
-        {:error, _} -> Map.get(map, codec)
-                       ~> fn map_value ->
-                            map_value
-                            |> map_codec.encode
-                            ~> fn map_bytes ->
-                                value
-                                |> codec.encode
-                                ~> fn value_bytes -> map_bytes <> value_bytes end
-                               end
-                          end
-        map_value -> map_value |> map_codec.encode
-      end
+      Map.get(map, value)
+      ~> fn mapped_value -> mapped_value |> map_codec.encode end
+      ~>> fn _ ->
+            Map.get(map, codec)
+            ~> fn map_value ->
+                 map_value
+                 |> map_codec.encode
+                 ~> fn map_bytes ->
+                      value
+                      |> codec.encode
+                      ~> fn value_bytes -> map_bytes <> value_bytes end
+                     end
+               end
+          end
       ~>> fn details -> error data_type(module), value, nest_error(details) end
     end
   end
@@ -108,12 +108,5 @@ defmodule Codec.Map do
   defp invert_map map, env do
     {:%{}, context, pairs} = Macro.expand(map, env)
     {:%{}, context, pairs |> Enum.map(fn {k, v} -> {v, k} end)}
-  end
-
-  def map_value_to_encode(value, inverted_map) when is_map(inverted_map) do
-    case Map.get(inverted_map, value) do
-      nil -> error :out_of_range
-      value_to_encode -> value_to_encode
-    end
   end
 end
