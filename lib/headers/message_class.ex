0718458ceb16d33a6587c0998aa996f18585d4eb
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-03 02:48:18 -0500

    r Use Codec2 for KnownMessageClass

--- lib/headers/message_class.ex
+++ lib/headers/message_class.ex
@@ -3,11 +3,22 @@ defmodule MMS.MessageClass do
 end
 
 defmodule MMS.KnownMessageClass do
-  use MMS.Lookup,
-    values: [
-      :personal,
-      :advertisement,
-      :informational,
-      :auto
-    ]
+  use MMS.Codec2
+  import Codec.Map
+  alias MMS.Byte
+
+  @map %{
+    128 => :personal,
+    129 => :advertisement,
+    130 => :informational,
+    131 => :auto
+  }
+
+  def decode(bytes) when is_binary(bytes) do
+    bytes |> decode(Byte, @map)
+  end
+
+  def encode value do
+    value |> encode(Byte, @map)
+  end
 end
