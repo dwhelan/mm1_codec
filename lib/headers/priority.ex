0cd8c3e9bcc367c6b84df68b501a6a36993979e2
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-03 02:14:24 -0500

    r Use Codec2

--- lib/headers/priority.ex
+++ lib/headers/priority.ex
@@ -1,8 +1,26 @@
 defmodule MMS.Priority do
-  use MMS.Lookup,
-      values: [
-        :low,
-        :normal,
-        :high,
-      ]
+  @moduledoc """
+  Priority-value = Low | Normal | High
+
+  Low    = <Octet 128>
+  Normal = <Octet 129>
+  High   = <Octet 130>
+  """
+  use MMS.Codec2
+  import Codec.Map
+  alias MMS.ShortInteger
+
+  @values [
+    :low,
+    :normal,
+    :high,
+  ]
+
+  def decode(bytes) when is_binary(bytes) do
+    bytes |> decode(ShortInteger, @values)
+  end
+
+  def encode(value) when is_atom(value) do
+    value |> encode(ShortInteger, @values)
+  end
 end
