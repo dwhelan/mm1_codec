6f26ef001641ece81620ae936c8b7b8143c63eb5
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-06 00:55:54 -0500

    r Use Codec in CodecMapper

--- lib/codec_mapper.ex
+++ lib/codec_mapper.ex
@@ -6,7 +6,7 @@ defmodule MMS.CodecMapper do
   defmacro __using__ opts \\ [] do
     quote bind_quoted: [opts: opts] do
       import MMS.CodecMapper
-      import OldOkError
+      use MMS.Codec
 
       @decode opts[:map] || with_index(opts[:values])
       @codec_bytes @decode |> Map.keys   |> Enum.reject(& elem(@decode[&1], 0) == :_unassigned)
@@ -19,16 +19,18 @@ defmodule MMS.CodecMapper do
       end
 
       defp do_decode(<<byte, bytes:: binary>>, codecs) when byte in @codec_bytes do
-        {module, codec} = @decode[byte]
+        {name, codec} = @decode[byte]
 
-        case codec.decode bytes do
-          {:ok,    {value, rest}} -> do_decode rest, [{module, value} | codecs]
-          {:error,        reason} -> error codec, reason
-        end
+        bytes
+        |> codec.decode
+        ~> fn {value, rest} -> do_decode(rest, [{name, value} | codecs]) end
+        ~>> fn details -> error {codec, details} end
       end
 
       defp do_decode rest, values do
-        ok Enum.reverse(values), rest
+        values
+        |> Enum.reverse
+        |> decode_ok(rest)
       end
 
       def encode values do
@@ -40,14 +42,17 @@ defmodule MMS.CodecMapper do
       defp do_encode([{name, value} | values], results) when name in @names do
         {byte, codec} = @encode_map[name]
 
-        case codec.encode value do
-          {:ok,     bytes} -> do_encode values, [<<byte>> <> bytes | results]
-          {:error, reason} -> error name, reason
-        end
+        value
+        |> codec.encode
+        ~> fn bytes -> do_encode values, [<<byte>> <> bytes | results] end
+        ~>> fn details -> error {name, details} end
       end
 
       defp do_encode [], results do
-        ok results |> Enum.reverse |> Enum.join
+        results
+        |> Enum.reverse
+        |> Enum.join
+        |> ok
       end
 
       defp do_encode [{name, value} | _], _  do
