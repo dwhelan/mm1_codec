3b97f85250d02a0db9989924cd908c648b86e6a5
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-06 00:11:59 -0500

    r Remove unused Delegate

--- lib/delegate.ex
+++
@@ -1,8 +0,0 @@
-defmodule MMS.Delegate do
-  defmacro __using__(opts) do
-    quote bind_quoted: [codec: opts[:codec]] do
-      defdelegate decode(bytes), to: codec
-      defdelegate encode(value), to: codec
-    end
-  end
-end
