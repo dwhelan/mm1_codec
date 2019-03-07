4ec6be3657fdba04e54982d715e2f6ab161ee910
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-07 01:09:06 -0500

    w

--- lib/data_types/text_value.ex
+++ lib/data_types/text_value.ex
@@ -8,35 +8,43 @@ defmodule MMS.TextValue do
 
   alias MMS.{NoValue, QuotedString, Text}
 
-  def decode(no_value = <<byte, _::binary>>) when is_no_value_byte(byte) do
-    no_value |> decode_as(NoValue)
+  def decode(no_value = <<byte, _ :: binary>>) when is_no_value_byte(byte) do
+    no_value
+    |> decode_as(NoValue)
   end
 
-  def decode(quoted_string = <<quote, _::binary >>) when is_quote(quote) do
-    quoted_string |> decode_as(QuotedString)
+  def decode(quoted_string = <<quote, _ :: binary>>) when is_quote(quote) do
+    quoted_string
+    |> decode_as(QuotedString)
   end
 
-  def decode(text = <<char, _::binary>>) when is_char(char) do
-    text |> decode_as(Text)
+  def decode(text = <<char, _ :: binary>>) when is_char(char) do
+    text
+    |> decode_as(Text)
   end
 
   def decode(bytes) when is_binary(bytes) do
-    bytes |> decode_error(:first_byte_must_be_no_value_or_quote_or_char)
+    bytes
+    |> decode_error(:first_byte_must_be_no_value_or_quote_or_char)
   end
 
   def encode(no_value) when is_no_value(no_value) do
-    no_value |> encode_as(NoValue)
+    no_value
+    |> encode_as(NoValue)
   end
 
-  def encode(quoted_string = << quote, _::binary >>) when is_quote(quote) do
-    quoted_string |> encode_as(QuotedString)
+  def encode(quoted_string = <<quote, _ :: binary>>) when is_quote(quote) do
+    quoted_string
+    |> encode_as(QuotedString)
   end
 
-  def encode(text = <<char, _::binary>>) when is_char(char) do
-    text |> encode_as(Text)
+  def encode(text = <<char, _ :: binary>>) when is_char(char) do
+    text
+    |> encode_as(Text)
   end
 
   def encode(string) when is_binary(string) do
-    string |> encode_error(:first_byte_must_be_no_value_or_quote_or_char)
+    string
+    |> encode_error(:first_byte_must_be_no_value_or_quote_or_char)
   end
 end
