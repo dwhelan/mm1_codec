207d9712c2d872d047535a8c2cba0d25fb01c759
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-12 15:42:04 -0400

    d Add spec reference for untyped parameter

--- lib/data_types/untyped_parameter_value.ex
+++ lib/data_types/untyped_parameter_value.ex
@@ -1,15 +1,8 @@
 defmodule MMS.UntypedParameter do
   @moduledoc """
+  Based on WAP-230-WSP-20010705-a: 8.4.2.4 Parameter
 
-  Encoded-string-value = Text-string | Value-length Char-set Text-string
-
-  The Char-set values are registered by IANA as MIBEnum value.
-  UTF-8 character-set encoding SHOULD be supported in Encoded-string-value.
-  If the MMS Client uses UTF-8 character-set encoding, the Char-set parameter SHOULD be used to indicate its usage.
-
-  Encoding according to [RFC2047] MAY be supported in the MMS Client and/or MMS Proxy-Relay.
-  Encoding according to [RFC2047] SHOULD only be used without “Value-length Char-set” parameters.
-  [RFC2047] encoding for UTF-8 character- set encoding MAY be supported in the MMS Client and/or MMS Proxy-Relay.
+  Untyped-parameter = Token-text Untyped-value
   """
 
   use MMS.Codec
