a618e4fed6ee3e60d07c16b0c2af34a83e4da6be
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-15 23:29:46 -0400

    d Added docs for Boolean

--- lib/data_types/boolean.ex
+++ lib/data_types/boolean.ex
@@ -1,11 +1,25 @@
 defmodule MMS.Boolean do
+  @moduledoc """
+  Specification: OMA-WAP-MMS-ENC-V1_1-20040715-A
+
+  7.2.6 X-Mms-Delivery-Report field
+    Delivery-report-value = Yes | No
+    Yes = <Octet 128>
+    No  = <Octet 129>
+
+  7.2.20 X-Mms-Read-Report field
+    Read-report-value = Yes | No
+    Yes = <Octet 128>
+    No  = <Octet 129>
+
+  7.2.26 X-Mms-Report-Allowed field
+    Report-allowed-value = Yes | No
+    Yes = <Octet 128>
+    No  = <Octet 129>
+  """
   use MMS.Codec
   alias MMS.Byte
 
-  [
-    { 128, true},
-    { 129, false}
-  ]
   @map %{
     128 => true,
     129 => false,
