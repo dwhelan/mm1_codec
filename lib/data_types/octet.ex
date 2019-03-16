87677a4ef6cafe8f70aa8f198fb0a21568f8f230
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-16 00:03:05 -0400

    d Add Octet documentation

--- lib/data_types/octet.ex
+++ lib/data_types/octet.ex
@@ -1,4 +1,11 @@
 defmodule MMS.Octet do
+  @moduledoc """
+  Specification WAP-230-WSP-20010705-a
+
+  8.1.1 Primitive Data Types
+
+  octet : 8 bits of opaque data
+  """
   use MMS.Codec
 
   def decode <<byte, rest::binary>> do
