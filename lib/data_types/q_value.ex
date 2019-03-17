507745508fe01979f9e6c87ef3f9127f330c19a0
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-17 01:37:23 -0400

    d Add spec reference

--- lib/data_types/q_value.ex
+++ lib/data_types/q_value.ex
@@ -1,10 +1,27 @@
 defmodule MMS.QValue do
+  @moduledoc """
+  8.4.2.3 Parameter Values
+
+  Q-value = 1*2 OCTET
+  The encoding is the same as in Uintvar-integer, but with restricted size.
+  When quality factor 0 ; and quality factors with one or two decimal digits are encoded,
+  they shall be multiplied by 100 ; and incremented by one,
+  so that they encode as a one-octet value in range 1-100,
+
+  ie, 0.1 is encoded as 11 (0x0B) and 0.99 encoded as 100 (0x64).
+  Three decimal quality factors shall be multiplied with 1000 and incremented by 100,
+  and the result shall be encoded ; as a one-octet or two-octet uintvar,
+
+  eg, 0.333 shall be encoded as 0x83 0x31.
+  Quality factor 1 is the default value and shall never be sent.
+  """
   use MMS.Codec, error: :q_value
 
   alias MMS.Uint32
 
-  def decode(bytes) when is_binary(bytes) do
-    bytes |> decode_as(Uint32, &to_q_string/1)
+  def decode bytes do
+    bytes
+    |> decode_as(Uint32, &to_q_string/1)
   end
 
   defp to_q_string(uint32) when is_2_digit_q_value(uint32), do: format(uint32 - 1,   2)
@@ -18,7 +35,8 @@ defmodule MMS.QValue do
   end
 
   def encode(string) when is_binary(string) do
-    string |> encode_as(Uint32, &parse/1)
+    string
+    |> encode_as(Uint32, &parse/1)
   end
 
   defp parse string do
