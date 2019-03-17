ecde268acf19e697ad01fa26b8f4874ae649b8bf
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-17 01:30:13 -0400

    d Add spec reference

--- lib/data_types/no_value.ex
+++ lib/data_types/no_value.ex
@@ -1,15 +1,24 @@
 defmodule MMS.NoValue do
+  @moduledoc """
+  8.4.2.3 Parameter Values
+
+  The following rules are used in encoding parameter values.
+  No-value = <Octet 0>
+  """
   use MMS.Codec
 
   def decode(<<byte, rest::binary>>) when is_no_value_byte(byte) do
-    no_value() |> decode_ok(rest)
+    no_value()
+    |> decode_ok(rest)
   end
 
   def decode bytes = <<value, _::binary>> do
-    bytes |> decode_error(%{out_of_range: value})
+    bytes
+    |> decode_error(%{out_of_range: value})
   end
 
   def encode(no_value) when is_no_value(no_value) do
-    <<no_value_byte()>> |> ok
+    <<no_value_byte()>>
+    |> ok
   end
 end
