d5832e8d5b550681ef8b132c5e034740014e5187
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-17 21:09:59 -0400

    d Add spec reference

--- lib/data_types/value_length.ex
+++ lib/data_types/value_length.ex
@@ -1,4 +1,13 @@
 defmodule MMS.ValueLength do
+  @moduledoc """
+  8.4.2.2 Length
+
+  The following rules are used to encode length indicators.
+
+  Value-length = Short-length | (Length-quote Length)
+
+  Value length is used to indicate the length of the value to follow
+  """
   use MMS.Codec
   import MMS.Prefix
   alias MMS.{ShortLength, UintvarInteger}
