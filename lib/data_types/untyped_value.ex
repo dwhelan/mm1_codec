8b9a40c312c63fb2da41eb903571bd8765299a1d
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-17 21:03:34 -0400

    r Use TextValue instead of Text

--- lib/data_types/untyped_value.ex
+++ lib/data_types/untyped_value.ex
@@ -6,10 +6,10 @@ defmodule MMS.UntypedValue do
   """
   use MMS.Codec
 
-  alias MMS.{IntegerValue, Text}
+  alias MMS.{IntegerValue, TextValue}
 
   def decode(bytes = <<char, _::binary>>) when is_char(char) do
-    bytes |> decode_as(Text)
+    bytes |> decode_as(TextValue)
   end
 
   def decode(bytes) when is_binary(bytes) do
@@ -17,7 +17,7 @@ defmodule MMS.UntypedValue do
   end
 
   def encode(string) when is_binary(string) do
-    string |> encode_as(Text)
+    string |> encode_as(TextValue)
   end
 
   def encode(integer) when is_integer(integer) do
