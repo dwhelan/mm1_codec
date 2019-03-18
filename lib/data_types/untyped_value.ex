a4d430d2ca9c859651561f57481924e349b7255a
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-17 21:07:39 -0400

    w Whitespace

--- lib/data_types/untyped_value.ex
+++ lib/data_types/untyped_value.ex
@@ -2,25 +2,29 @@ defmodule MMS.UntypedValue do
   @moduledoc """
   8.4.2.4 Parameter
 
-  Untyped-value = IntegerValue-value | Text-value
+  Untyped-value = Integer-value | Text-value
   """
   use MMS.Codec
 
   alias MMS.{IntegerValue, TextValue}
 
   def decode(bytes = <<char, _::binary>>) when is_char(char) do
-    bytes |> decode_as(TextValue)
+    bytes
+    |> decode_as(TextValue)
   end
 
   def decode(bytes) when is_binary(bytes) do
-    bytes |> decode_as(IntegerValue)
+    bytes
+    |> decode_as(IntegerValue)
   end
 
   def encode(string) when is_binary(string) do
-    string |> encode_as(TextValue)
+    string
+    |> encode_as(TextValue)
   end
 
   def encode(integer) when is_integer(integer) do
-    integer |> encode_as(IntegerValue)
+    integer
+    |> encode_as(IntegerValue)
   end
 end
