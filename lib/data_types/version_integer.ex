eaa3125737e9921f44bba1bbc6c9391904a78646
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-17 21:13:45 -0400

    w Fix whitepace

--- lib/data_types/version_integer.ex
+++ lib/data_types/version_integer.ex
@@ -1,6 +1,6 @@
 defmodule MMS.VersionInteger do
   @moduledoc """
-  Specification: WAP-230-WSP-20010705-a, 8.4.2.3 Parameter Values
+  8.4.2.3 Parameter Values
 
   Version-value = Short-integer | Text-string
 
@@ -19,38 +19,47 @@ defmodule MMS.VersionInteger do
   @no_minor    15
 
   def decode <<1::1, major::3, @no_minor::4, rest::binary>> do
-    major |> decode_ok(rest)
+    major
+    |> decode_ok(rest)
   end
 
   def decode <<1::1, major::3, minor::4, rest::binary>> do
-    {major, minor} |> decode_ok(rest)
+    {major, minor}
+    |> decode_ok(rest)
   end
 
   def decode bytes = <<version, _::binary>> do
-    bytes |> decode_error(%{out_of_range: version})
+    bytes
+    |> decode_error(%{out_of_range: version})
   end
 
   def encode(major) when major in @major_range do
-    {major, @no_minor} |> do_encode
+    {major, @no_minor}
+    |> do_encode
   end
 
   def encode(major) when is_integer(major) do
-    major |> encode_error(:out_of_range)
+    major
+    |> encode_error(:out_of_range)
   end
 
   def encode({major, minor}) when major in @major_range and minor in @minor_range do
-    {major, minor} |> do_encode
+    {major, minor}
+    |> do_encode
   end
 
   def encode({major, minor}) when is_integer(major) and minor in @minor_range do
-    {major, minor} |> encode_error(:major_out_of_range)
+    {major, minor}
+    |> encode_error(:major_out_of_range)
   end
 
   def encode({major, minor}) when is_integer(major) and is_integer(minor) do
-    {major, minor} |> encode_error(:minor_out_of_range)
+    {major, minor}
+    |> encode_error(:minor_out_of_range)
   end
 
   def do_encode {major, minor} do
-    <<1::1, major::3, minor::4>> |> ok
+    <<1::1, major::3, minor::4>>
+    |> ok
   end
 end
