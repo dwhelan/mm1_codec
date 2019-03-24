6a40cbd4a7c6cb251564a0b5f36682807296e5fe
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-24 01:22:29 -0400

    Add specs

--- lib/data_types/content_type.ex
+++ lib/data_types/content_type.ex
@@ -1,4 +1,19 @@
 defmodule MMS.ContentType do
+  @moduledoc """
+  8.4.2.24 Content type field
+
+  The following rules are used to encode content type values.
+  The short form of the Content-type-value MUST only be used when the well-known media is in the range of 0-127 or a text string.
+
+  In all other cases the general form MUST be used.
+
+  Content-type-value = Constrained-media | Content-general-form
+  Content-general-form = Value-length Media-type
+
+  We simplify this to:
+
+  Content-type-value = Constrained-media | Value-length Media-type
+  """
   use MMS.Codec
 
   alias MMS.{ValueLength, Media}
