5226bf17d143d8d54d07b4600b263a5caaeaf3b3
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-24 17:32:16 -0400

    Add parameter assignment table to docs

--- lib/data_types/typed_parameter.ex
+++ lib/data_types/typed_parameter.ex
@@ -3,10 +3,46 @@ defmodule MMS.TypedParameter do
   8.4.2.4 Parameter
 
   Typed-parameter = Well-known-parameter-token Typed-value
+  Well-known-parameter-token = Integer-value
 
   The actual expected type of the value is implied by the well-known parameter
 
   Table 38. Well-Known Parameter Assignments
+
+  |Token            |Assigned Number |BNF Value Rule      |
+  |-----------------|----------------|--------------------|
+  |Q                |0x00            |Q-value             |
+  |Charset          |0x01            |Well-known-charset  |
+  |Level            |0x02            |Version-value       |
+  |Type             |0x03            |Integer-value       |
+  |Name*            |0x05            |Text-string         |
+  |Filename*        |0x06            |Text-string         |
+  |Differences      |0x07            |Field-name          |
+  |Padding          |0x08            |Short-integer       |
+  |Type             |0x09            |Constrained-encoding|
+  |Start*           |0x0A            |Text-string         |
+  |Start-info*      |0x0B            |Text-string         |
+  |Comment*         |0x0C            |Text-string         |
+  |Domain*          |0x0D            |Text-string         |
+  |Max-Age          |0x0E            |Delta-seconds-value |
+  |Path*            |0x0F            |Text-string         |
+  |Secure           |0x10            |No-value            |
+  |SEC              |0x11            |Short-integer       |
+  |MAC              |0x12            |Text-value          |
+  |Creation-date    |0x13            |Date-value          |
+  |Modification-date|0x14            |Date-value          |
+  |Read-date        |0x15            |Date-value          |
+  |Size             |0x16            |Integer-value       |
+  |Name             |0x17            |Text-value          |
+  |Filename         |0x18            |Text-value          |
+  |Start            |0x19            |Text-value          |
+  |Start-info       |0x1A            |Text-value          |
+  |Comment          |0x1B            |Text-value          |
+  |Domain           |0x1C            |Text-value          |
+  |Path             |0x1D            |Text-value          |
+
+  Note: * These numbers have been deprecated and should not be used.
+
   """
 
   alias MMS.{WellKnownCharset, DateValue, IntegerValue, MediaType, NoValue, QValue, ShortInteger, TextString, TextValue, Version}
