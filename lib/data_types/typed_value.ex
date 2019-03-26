3fede7f0b7d44a18000d264c2fed5ebe448043c7
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-25 23:03:27 -0400

    Update WellKnownParameter to just encode the token

--- lib/data_types/typed_value.ex
+++ lib/data_types/typed_value.ex
@@ -10,10 +10,79 @@ defmodule MMS.TypedValue do
 
   Compact-value = Integer-value | Date-value | Delta-seconds-value | Q-value | Version-value | Uri-value
 
+  Table 38. Well-Known Parameter Assignments (excerpts)
+
+  |-----------------|----------------|--------------------|
+  |Token            |Assigned Number |BNF Rule for Value  |
+  |-----------------|----------------|--------------------|
+  |Q                |0x00            |Q-value             |
+  |Charset          |0x01            |Well-known-charset  |
+  |Level            |0x02            |Version-value       |
+  |Type             |0x03            |Integer-value       |
+  |Name*            |0x05            |Text-string         |
+  |Filename*        |0x06            |Text-string         |
+  |Differences      |0x07            |Field-name          |
+  |Padding          |0x08            |Short-integer       |
+  |Type             |0x09            |Constrained-encoding|
+  |Start*           |0x0A            |Text-string         |
+  |Start-info*      |0x0B            |Text-string         |
+  |Comment*         |0x0C            |Text-string         |
+  |Domain*          |0x0D            |Text-string         |
+  |Max-Age          |0x0E            |Delta-seconds-value |
+  |Path*            |0x0F            |Text-string         |
+  |Secure           |0x10            |No-value            |
+  |SEC              |0x11            |Short-integer       |
+  |MAC              |0x12            |Text-value          |
+  |Creation-date    |0x13            |Date-value          |
+  |Modification-date|0x14            |Date-value          |
+  |Read-date        |0x15            |Date-value          |
+  |Size             |0x16            |Integer-value       |
+  |Name             |0x17            |Text-value          |
+  |Filename         |0x18            |Text-value          |
+  |Start            |0x19            |Text-value          |
+  |Start-info       |0x1A            |Text-value          |
+  |Comment          |0x1B            |Text-value          |
+  |Domain           |0x1C            |Text-value          |
+  |Path             |0x1D            |Text-value          |
+  |-----------------|----------------|--------------------|
+
   Note that the second argument, `codec` to `decode` and `encode`, must be a Codec which can decode and encode
   values of the expected type. So, we do not need a `MMS.CompactValue` to implement this.
   """
   use MMS.Codec
+
+#    @map %{
+#    # Assigned Number => {Token                       Codec}
+#    0x00              => {:q,                         QValue},
+#    0x01              => {:charset,                   WellKnownCharset},
+#    0x02              => {:level,                     VersionValue},
+#    0x03              => {:type,                      IntegerValue},
+#    0x05              => {:"name (deprecated)",       TextString},
+#    0x06              => {:"file_name (deprecated)",  TextString},
+#    0x07              => {:differences,               FieldName},
+#    0x08              => {:padding,                   ShortInteger},
+#    0x09              => {:type_multipart,            ConstrainedEncoding},
+#    0x0A              => {:"start (deprecated)",      TextString},
+#    0x0B              => {:"start_info (deprecated)", TextString},
+#    0x0C              => {:"comment (deprecated)",    TextString},
+#    0x0D              => {:"domain (deprecated)",     TextString},
+#    0x0E              => {:max_age,                   DeltaSecondsValue},
+#    0x0F              => {:"path (deprecated)",       TextString},
+#    0x10              => {:secure,                    NoValue},
+#    0x11              => {:sec,                       ShortInteger},
+#    0x12              => {:mac,                       TextValue},
+#    0x13              => {:creation_date,             DateValue},
+#    0x14              => {:modification_date,         DateValue},
+#    0x15              => {:read_date,                 DateValue},
+#    0x16              => {:size,                      IntegerValue},
+#    0x17              => {:name,                      TextValue},
+#    0x18              => {:file_name,                 TextValue},
+#    0x19              => {:start,                     TextValue},
+#    0x1A              => {:start_info,                TextValue},
+#    0x1B              => {:comment,                   TextValue},
+#    0x1C              => {:domain,                    TextValue},
+#    0x1D              => {:path,                      TextValue},
+#  }
 
   alias MMS.{TextValue}
 
--- lib/data_types/well_known_parameter_token.ex
+++ lib/data_types/well_known_parameter_token.ex
@@ -6,47 +6,46 @@ defmodule MMS.WellKnownParameterToken do
 
   The code values used for parameters are specified in the Assigned Numbers appendix.
 
-  Table 38. Well-Known Parameter Assignments
+  Table 38. Well-Known Parameter Assignments (excerpts)
 
   The table is shown without the `Encoding Version`
-  |-----------------|----------------|--------------------|
-  |Token            |Assigned Number |BNF Rule for Value  |
-  |-----------------|----------------|--------------------|
-  |Q                |0x00            |Q-value             |
-  |Charset          |0x01            |Well-known-charset  |
-  |Level            |0x02            |Version-value       |
-  |Type             |0x03            |Integer-value       |
-  |Name*            |0x05            |Text-string         |
-  |Filename*        |0x06            |Text-string         |
-  |Differences      |0x07            |Field-name          |
-  |Padding          |0x08            |Short-integer       |
-  |Type             |0x09            |Constrained-encoding|
-  |Start*           |0x0A            |Text-string         |
-  |Start-info*      |0x0B            |Text-string         |
-  |Comment*         |0x0C            |Text-string         |
-  |Domain*          |0x0D            |Text-string         |
-  |Max-Age          |0x0E            |Delta-seconds-value |
-  |Path*            |0x0F            |Text-string         |
-  |Secure           |0x10            |No-value            |
-  |SEC              |0x11            |Short-integer       |
-  |MAC              |0x12            |Text-value          |
-  |Creation-date    |0x13            |Date-value          |
-  |Modification-date|0x14            |Date-value          |
-  |Read-date        |0x15            |Date-value          |
-  |Size             |0x16            |Integer-value       |
-  |Name             |0x17            |Text-value          |
-  |Filename         |0x18            |Text-value          |
-  |Start            |0x19            |Text-value          |
-  |Start-info       |0x1A            |Text-value          |
-  |Comment          |0x1B            |Text-value          |
-  |Domain           |0x1C            |Text-value          |
-  |Path             |0x1D            |Text-value          |
-  |-----------------|----------------|--------------------|
+  |----------------|-----------------|
+  |Assigned Number |Token            |
+  |----------------|-----------------|
+  |0x00            |Q                |
+  |0x01            |Charset          |
+  |0x02            |Level            |
+  |0x03            |Type             |
+  |0x05            |Name*            |
+  |0x06            |Filename*        |
+  |0x07            |Differences      |
+  |0x08            |Padding          |
+  |0x09            |Type             |
+  |0x0A            |Start*           |
+  |0x0B            |Start-info*      |
+  |0x0C            |Comment*         |
+  |0x0D            |Domain*          |
+  |0x0E            |Max-Age          |
+  |0x0F            |Path*            |
+  |0x10            |Secure           |
+  |0x11            |SEC              |
+  |0x12            |MAC              |
+  |0x13            |Creation-date    |
+  |0x14            |Modification-date|
+  |0x15            |Read-date        |
+  |0x16            |Size             |
+  |0x17            |Name             |
+  |0x18            |Filename         |
+  |0x19            |Start            |
+  |0x1A            |Start-info       |
+  |0x1B            |Comment          |
+  |0x1C            |Domain           |
+  |0x1D            |Path             |
+  |----------------|-----------------|
 
   Note: * These numbers have been deprecated and should not be used.
 
-  This codec represents a well known parameter token as a `Tuple`
-  of `{token, codec}`.
+  This codec represents a well known parameter token as a Pascal cased `atom`.
   """
 
   use MMS.Codec
@@ -56,36 +55,35 @@ defmodule MMS.WellKnownParameterToken do
   alias MMS.{NoValue, DateValue, TextValue}
 
   @map %{
-    # Assigned Number => {Token                       Codec}
-    0x00              => {:q,                         QValue},
-    0x01              => {:charset,                   WellKnownCharset},
-    0x02              => {:level,                     VersionValue},
-    0x03              => {:type,                      IntegerValue},
-    0x05              => {:"name (deprecated)",       TextString},
-    0x06              => {:"file_name (deprecated)",  TextString},
-    0x07              => {:differences,               FieldName},
-    0x08              => {:padding,                   ShortInteger},
-    0x09              => {:type_multipart,            ConstrainedEncoding},
-    0x0A              => {:"start (deprecated)",      TextString},
-    0x0B              => {:"start_info (deprecated)", TextString},
-    0x0C              => {:"comment (deprecated)",    TextString},
-    0x0D              => {:"domain (deprecated)",     TextString},
-    0x0E              => {:max_age,                   DeltaSecondsValue},
-    0x0F              => {:"path (deprecated)",       TextString},
-    0x10              => {:secure,                    NoValue},
-    0x11              => {:sec,                       ShortInteger},
-    0x12              => {:mac,                       TextValue},
-    0x13              => {:creation_date,             DateValue},
-    0x14              => {:modification_date,         DateValue},
-    0x15              => {:read_date,                 DateValue},
-    0x16              => {:size,                      IntegerValue},
-    0x17              => {:name,                      TextValue},
-    0x18              => {:file_name,                 TextValue},
-    0x19              => {:start,                     TextValue},
-    0x1A              => {:start_info,                TextValue},
-    0x1B              => {:comment,                   TextValue},
-    0x1C              => {:domain,                    TextValue},
-    0x1D              => {:path,                      TextValue},
+    0x00 => :q,
+    0x01 => :charset,
+    0x02 => :level,
+    0x03 => :type,
+    0x05 => :"name (deprecated)",
+    0x06 => :"file_name (deprecated)",
+    0x07 => :differences,
+    0x08 => :padding,
+    0x09 => :type_multipart,
+    0x0A => :"start (deprecated)",
+    0x0B => :"start_info (deprecated)",
+    0x0C => :"comment (deprecated)",
+    0x0D => :"domain (deprecated)",
+    0x0E => :max_age,
+    0x0F => :"path (deprecated)",
+    0x10 => :secure,
+    0x11 => :sec,
+    0x12 => :mac,
+    0x13 => :creation_date,
+    0x14 => :modification_date,
+    0x15 => :read_date,
+    0x16 => :size,
+    0x17 => :name,
+    0x18 => :file_name,
+    0x19 => :start,
+    0x1A => :start_info,
+    0x1B => :comment,
+    0x1C => :domain,
+    0x1D => :path,
   }
 
   def decode bytes do
@@ -93,8 +91,8 @@ defmodule MMS.WellKnownParameterToken do
     |> decode_as(IntegerValue, @map)
   end
 
-  def encode {token, codec} do
-    {token, codec}
+  def encode token do
+    token
     |> encode_as(IntegerValue, @map)
   end
 end
