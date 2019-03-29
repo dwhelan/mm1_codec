0710c5b76dcd6f6b2e378767ba39d659e0843261
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-29 00:45:42 -0400

    Use decode_either

--- lib/data_types/media_type.ex
+++ lib/data_types/media_type.ex
@@ -8,19 +8,9 @@ defmodule MMS.MediaType do
 
   alias MMS.{WellKnownMedia, ExtensionMedia}
 
-  def decode(bytes = <<well_known_media, _::binary>>) when is_short_integer_byte(well_known_media) do
-    bytes
-    |> decode_as(WellKnownMedia)
-  end
-
-  def decode(<<char, _ :: binary>> = bytes) when is_text(char) do
-    bytes
-    |> decode_as(ExtensionMedia)
-  end
-
   def decode bytes do
     bytes
-    |> decode_error(:must_start_with_a_short_integer_or_char)
+    |> decode_either([WellKnownMedia, ExtensionMedia])
   end
 
   def encode(atom) when is_atom(atom) do
