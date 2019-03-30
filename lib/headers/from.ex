b90f03b3dc06b97791fcffce6175d8c359218d6d
Declan Whelan <dwhelan@dpwhelan.com>
2019-03-29 21:29:24 -0400

    Remove IO.inspect

--- lib/headers/from.ex
+++ lib/headers/from.ex
@@ -10,14 +10,20 @@ defmodule MMS.From do
   def decode(bytes) when is_binary(bytes) do
     bytes
     |> ValueLength.decode(
-         fn value_bytes -> value_bytes |> decode_as(ShortInteger, @map) end
+         fn value_bytes ->
+           value_bytes
+           |> decode_as(ShortInteger, @map)
+         end
        )
   end
 
   def encode(from) when is_address(from) or from == :insert_address_token do
     from
     |> ValueLength.encode(
-         fn value -> value |> encode_as(ShortInteger, @map, Address) |> IO.inspect end
+         fn value ->
+           value
+           |> encode_as(ShortInteger, @map, Address)
+         end
        )
   end
 end
--- test/data_types/typed_parameter_test.exs
+++ test/data_types/typed_parameter_test.exs
@@ -12,11 +12,11 @@ defmodule MMS.TypedParameterTest do
 
       decode_errors: [
         { << s 30 >>,    {:typed_parameter, << s 30 >>,    [:well_known_parameter_token, %{out_of_range: 30}]} },
-        { << s(8), 1 >>, {:typed_parameter, << s(8), 1 >>, [:typed_value, %{cannot_be_decoded_as: [MMS.CompactValue, MMS.TextValue]}] } },
+        { << s(8), 1 >>, {:typed_parameter, << s(8), 1 >>, [:typed_value, %{cannot_be_decoded_as: [CompactValue, TextValue]}] } },
       ],
 
       encode_errors: [
         { {:foo, 0},      {:typed_parameter, :foo,           [:well_known_parameter_token, :out_of_range]} },
-        { {:padding, :a}, {:typed_parameter, {:padding, :a}, [:typed_value, %{cannot_be_encoded_as: [MMS.CompactValue, MMS.TextValue]}]} },
+        { {:padding, :a}, {:typed_parameter, {:padding, :a}, [:typed_value, %{cannot_be_encoded_as: [CompactValue, TextValue]}]} },
       ]
 end
