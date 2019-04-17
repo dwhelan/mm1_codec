fdf282125b49b7e81b1a975f439672bde884ce21
Declan Whelan <dwhelan@dpwhelan.com>
2019-04-17 00:00:55 -0400

    Use codec test macros

--- test/data_types/integer_value_test.exs
+++ test/data_types/integer_value_test.exs
@@ -1,22 +1,20 @@
 defmodule MMS.IntegerValueTest do
   use MMS.CodecTest
+  import MMS.IntegerValue
 
-  use MMS.TestExamples,
-      codec: MMS.IntegerValue,
+  codec_examples [
+    { "a", << s(0) >>,           0      },
+    { "b", << s(127) >>,         127    },
+    { "c", << l(1), 128 >>,      128    },
+    { "d", << l(2), 255, 255 >>, 65_535 },
+  ]
 
-      examples: [
-        { << s(0) >>,           0      },
-        { << s(127) >>,         127    },
-        { << l(1), 128 >>,      128    },
-        { << l(2), 255, 255 >>, 65_535 },
-      ],
+  decode_errors [
+    { "bad integer", <<0>>, short_integer: [out_of_range: 0], long_integer: :must_have_at_least_one_data_byte },
+  ]
 
-      decode_errors: [
-        { <<0>>, {:integer_value, <<0>>, [{:short_integer, [out_of_range: 0]}, {:long_integer, :must_have_at_least_one_data_byte}]} },
-      ],
-
-      encode_errors: [
-        { -1, {:integer_value, -1, [short_integer: :out_of_range, long_integer: :out_of_range] } },
-      ]
+  encode_errors [
+    { "negative integer", -1, short_integer: :out_of_range, long_integer: :out_of_range },
+  ]
 end
 
