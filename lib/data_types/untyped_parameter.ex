1daa21f9fd712cd22833b2bde46b8036cd84370d
Declan Whelan <dwhelan@dpwhelan.com>
2019-04-20 01:06:03 -0400

    Add codec macro for tuples

--- lib/data_types/untyped_parameter.ex
+++ lib/data_types/untyped_parameter.ex
@@ -1,21 +1,37 @@
+defmodule MMS.Tuple2 do
+  defmacro defcodec opts do
+    codecs = opts[:as]
+    quote do
+      use MMS.Codec
+      import MMS.Tuple
+
+      def decode bytes do
+        bytes
+        |> MMS.Tuple.decode(unquote codecs)
+      end
+
+      def encode {name, value} do
+        {name, value}
+        |> MMS.Tuple.encode(unquote codecs)
+      end
+    end
+  end  
+
+  defmacro __using__ (_) do
+    quote do
+      use MMS.Codec
+      import MMS.Tuple2
+    end
+  end
+end
+
 defmodule MMS.UntypedParameter do
   @moduledoc """
   8.4.2.4 Parameter
 
   Untyped-parameter = Token-text Untyped-value
   """
+  use MMS.Tuple2
 
-  use MMS.Codec
-
-  alias MMS.{Tuple, TokenText, UntypedValue}
-
-  def decode bytes do
-    bytes
-    |> Tuple.decode([TokenText, UntypedValue])
-  end
-
-  def encode {name, value} do
-    {name, value}
-    |> Tuple.encode([TokenText, UntypedValue])
-  end
+  defcodec as: {MMS.TokenText, MMS.UntypedValue}
 end
--- lib/tuple.ex
+++ lib/tuple.ex
@@ -7,10 +7,49 @@ defmodule MMS.Tuple do
     ~> fn {values, rest} -> ok List.to_tuple(values), rest end
   end
 
+  def decode(bytes, codecs) when is_binary(bytes) and is_tuple(codecs) do
+    bytes
+    |> MMS.List.decode(Tuple.to_list codecs)
+    ~> fn {values, rest} -> ok List.to_tuple(values), rest end
+  end
+
   def encode(values, codecs) when is_tuple(values) and is_list(codecs) do
     values
     |> Tuple.to_list
     |> MMS.List.encode(codecs)
     ~>> fn {_, _, reason} -> error data_type(), values, reason end
   end
+
+  def encode(values, codecs) when is_tuple(values) and is_tuple(codecs) do
+    values
+    |> Tuple.to_list
+    |> MMS.List.encode(Tuple.to_list codecs)
+    ~>> fn {_, _, reason} -> error data_type(), values, reason end
+  end
+
+  defmacro defcodec opts do
+    codecs = opts[:as]
+    quote do
+      use MMS.Codec
+      import MMS.Tuple
+
+      def decode bytes do
+        bytes
+        |> decode(unquote codecs)
+      end
+
+      def encode {name, value} do
+        {name, value}
+        |> encode(unquote codecs)
+      end
+    end
+  end
+
+  defmacro __using__ (_) do
+    quote do
+      use MMS.Codec
+      import MMS.Tuple
+    end
+  end
+
 end
