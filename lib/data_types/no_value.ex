da524eb3b6fd14b297cf2383a8cad0c21d649514
Declan Whelan <dwhelan@dpwhelan.com>
2019-04-20 00:24:04 -0400

    Use defcoded with map

--- lib/data_types/no_value.ex
+++ lib/data_types/no_value.ex
@@ -5,21 +5,9 @@ defmodule MMS.NoValue do
   The following rules are used in encoding parameter values.
   No-value = <Octet 0>
   """
-  use MMS.Codec
+  use MMS.As
 
-  def decode(<<byte, rest::binary>>) when is_no_value_byte(byte) do
-    ok no_value(), rest
-  end
-
-  def decode bytes = <<value, _::binary>> do
-    error bytes, out_of_range: value
-  end
-
-  def encode(no_value) when is_no_value(no_value) do
-    ok <<no_value_byte()>>
-  end
-
-  def encode value do
-    error value, :out_of_range
-  end
+  defcodec as: MMS.Octet, map: %{
+    0 => :no_value
+  }
 end
