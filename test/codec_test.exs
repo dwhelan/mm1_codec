4844dbfb083ced7e2542c25a2f8d4387520c5bcc
Declan Whelan <dwhelan@dpwhelan.com>
2019-04-21 01:52:05 -0400

    Remove CodecErrorTest

--- test/codec_test.exs
+++ test/codec_test.exs
@@ -2,6 +2,19 @@ defmodule MMS.Codec2Test do
   use MMS.CodecTest
   use MMS.Codec
 
+  describe "data_type/1 should" do
+    test "convert all upper case letters to lower case" do
+      assert data_type(A)     == :a
+      assert data_type(AB)    == :ab
+      assert data_type(ABC)   == :abc
+    end
+
+    test "should combine an uppercase with following characters until the next uppercase is found" do
+      assert data_type(Ab1C2) == :ab1_c2
+      assert data_type(AB1C2) == :ab1_c2
+    end
+  end
+
   describe "nest_error" do
     test "should accumulate nested error list" do
       assert nest_error({:data_type, :value, [:data_type2, :reason]}) == [:data_type, :data_type2, :reason]
