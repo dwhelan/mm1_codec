d171f45153b63a8cecd8d7c651eef889c8d7ee1f
Declan Whelan <dwhelan@dpwhelan.com>
2019-04-22 08:52:26 -0400

    Support decode_mapper/2

--- lib/mapper.ex
+++ lib/mapper.ex
@@ -9,8 +9,16 @@ defmodule MMS.Mapper do
     create_mapper decode_mapper, encode_mapper
   end
 
+  defmacro defmapper _ do
+    raise ArgumentError, message: "You must pass a map"
+  end
+
   defmacro defmapper(decode_mapper = {d, _, _}, encode_mapper = {e, _, _}) when d in [:fn, :&] and e in [:fn, :&] do
     create_mapper decode_mapper, encode_mapper
+  end
+
+  defmacro defmapper _, _ do
+    raise ArgumentError, message: "You must pass two functions"
   end
 
   defp invert {:%{}, context, kv_pairs} do
@@ -26,19 +34,33 @@ defmodule MMS.Mapper do
       @behaviour MMS.Mapper
 
       def decode_map result do
+        decode_mapper = unquote(decode_mapper)
         result
         ~> fn {value, rest} ->
-             value
-             |> unquote(decode_mapper).()
-             ~> fn mapped -> ok {mapped, rest} end
-             ~>> fn details -> error {data_type(__MODULE__), value, details || :not_found} end
+             {value, rest}
+             |> do_decode_map(decode_mapper)
+             ~>> & handle_error value, &1
            end
+      end
+
+      defp do_decode_map {value, rest}, decode_mapper do
+        case Function.info(decode_mapper)[:arity] do
+          1 ->
+            decode_mapper.(value)
+            ~> fn value -> ok value, rest end
+          2 ->
+            decode_mapper.(value, rest)
+        end
       end
 
       def encode_map value do
         value
         |> unquote(encode_mapper).()
-        ~>> fn details -> error {data_type(__MODULE__), value, details || :not_found} end
+        ~>> & handle_error value, &1
+      end
+
+      defp handle_error value, details do
+        error {data_type(__MODULE__), value, details || :not_found}
       end
     end
   end
--- test/mapper_test.exs
+++ test/mapper_test.exs
@@ -2,7 +2,7 @@ defmodule MMS.MapperTest do
   use MMS.CodecTest
   import MMS.Mapper
 
-  describe "with functions" do
+  describe "functions with arity 1" do
     defmodule Plus1 do
       import MMS.Mapper
 
@@ -16,6 +16,24 @@ defmodule MMS.MapperTest do
 
     test "encode_map" do
       assert Plus1.encode_map(2) == ok(1)
+    end
+  end
+
+
+  describe "functions with arity 2" do
+    defmodule Plus1a do
+      import MMS.Mapper
+
+      defmapper fn x, _rest -> {x + 1, "rest!"} end, fn x -> x - 1 end
+    end
+
+    test "decode_map" do
+      assert Plus1a.decode_map(ok(1, "rest")) == ok(2, "rest!")
+      assert Plus1a.decode_map(error(:data_type, "bytes", :reason)) == error(:data_type, "bytes", :reason)
+    end
+
+    test "encode_map" do
+      assert Plus1a.encode_map(2) == ok(1)
     end
   end
 
@@ -54,5 +72,35 @@ defmodule MMS.MapperTest do
       assert CodecMap.encode_map(:b) == ok(1)
       assert CodecMap.encode_map(:c) == error {:codec_map, :c, :not_found}
     end
+  end
+
+  test "enforce that map is a map" do
+    assert_code_raise """
+        defmodule ArugmentErrorTest do
+          use MMS.CodecTest
+          import MMS.Mapper
+
+          defmapper "not a map"
+        end
+    """
+  end
+
+  test "enforce that mappers are functions" do
+    assert_code_raise """
+        defmodule ArugmentErrorTest do
+          use MMS.CodecTest
+          import MMS.Mapper
+
+          defmapper "not a function", & &1
+        end
+    """
+    assert_code_raise """
+        defmodule ArugmentErrorTest do
+          use MMS.CodecTest
+          import MMS.Mapper
+
+          defmapper & &1, "not a function"
+        end
+    """
   end
 end
