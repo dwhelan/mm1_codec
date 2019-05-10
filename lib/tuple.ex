20e6cd0cf53abdb3509ca12ffd1bf323f7cd47e3
Declan Whelan <dwhelan@dpwhelan.com>
2019-05-10 01:11:20 -0400

    Added map_value()

--- lib/tuple.ex
+++ lib/tuple.ex
@@ -1,13 +1,23 @@
 defmodule MMS.Tuple do
+  import OkError
+
+  def map_value {:ok, {value, rest}}, function do
+    ok {function.(value), rest}
+  end
+
+  def map_value {:error, error}, _function do
+    error error
+  end
+
   defmacro tuple_codec codecs do
     quote do
       use MMS.Codec
 
       def decode bytes do
         bytes
-          |> MMS.List.decode(unquote codecs)
-          ~> fn {values, rest} -> ok List.to_tuple(values), rest end
-          ~>> fn {_, _, reason} -> error data_type(), bytes, reason end
+        |> MMS.List.decode(unquote codecs)
+        |> map_value(&List.to_tuple/1)
+        ~>> fn {_, _, reason} -> error data_type(), bytes, reason end
       end
 
       def encode value do
