042eaef646e3b180daf095743ea4aaa23a343581
Declan Whelan <dwhelan@dpwhelan.com>
2019-05-10 16:39:12 -0400

    Use new test examples

--- test/data_types/content_general_form_test.exs
+++ test/data_types/content_general_form_test.exs
@@ -1,13 +1,17 @@
 defmodule MMS.ContentGeneralFormTest do
   use MMS.CodecTest
+  import MMS.ContentGeneralForm
 
-  use MMS.TestExamples,
-      codec: MMS.ContentGeneralForm,
+  codec_examples [
+    {"valid length and media", <<l(1), s(0)>>, :"*/*"},
+  ]
 
-      examples: [
-        { << l(1), s(0) >>, :"*/*" },
-      ],
+  decode_errors [
+    {"invalid length", <<l(255)>>},
+    {"invalid media",  <<l(1), "invalid media">>},
+  ]
 
-      decode_errors: [
-      ]
+  encode_errors [
+    {"invalid media", :"invalid media"},
+  ]
 end
