0d633da2c4e48ce98acb1cb132640ca09a2be83e
Declan Whelan <dwhelan@dpwhelan.com>
2019-05-10 21:34:35 -0400

    Use new test examples

--- test/headers/from_test.exs
+++ test/headers/from_test.exs
@@ -9,13 +9,10 @@ defmodule MMS.FromTest do
   ]
 
   decode_errors [
-    {"zero length",   <<l(0)>>, address: :no_bytes},
-    {"invalid token", <<l(3), 0, "@\0" >>, address: :out_of_range },
-    {"address error", <<l(2), 128, "@" >>, address:  [:address, :text, :missing_end_of_string] },
-    {"invalid length",  <<l(4), 128, "@\0" >>, value_length: [
-                short_length: [required_bytes: 4, available_bytes: 3],
-                quoted_length: :does_not_start_with_a_length_quote
-              ]},
+    {"zero length",    <<l(0)>>},
+    {"invalid token",  <<l(3)>>},
+    {"address error",  <<l(2)>>},
+    {"invalid length", <<l(4), 128, "@\0">>},
   ]
 
   encode_errors [
