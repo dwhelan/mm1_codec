5ec6c90607c98b5fbc3415fc524a4023f2c8bc8d
Declan Whelan <dwhelan@dpwhelan.com>
2019-05-10 20:13:43 -0400

    Use new test examples

--- test/data_types/delta_seconds_value_test.exs
+++ test/data_types/delta_seconds_value_test.exs
@@ -3,18 +3,18 @@ defmodule MMS.DeltaSecondsValueTest do
   import MMS.DeltaSecondsValue
 
   codec_examples [
-    { "a", << s(0) >>,           0      },
-    { "b", << s(127) >>,         127    },
-    { "c", << l(1), 128 >>,      128    },
-    { "d", << l(2), 255, 255 >>, 65_535 },
+    {"a", << s(0) >>,           0     },
+    {"b", << s(127) >>,         127   },
+    {"c", << l(1), 128 >>,      128   },
+    {"d", << l(2), 255, 255 >>, 65_535},
   ]
 
   decode_errors [
-    { "x", <<0>>, [:integer_value, {:short_integer, [out_of_range: 0]}, {:long_integer, :must_have_at_least_one_data_byte}] },
+    {"x", <<0>>},
   ]
 
   encode_errors [
-    { "x", -1, {:integer_value, -1, [short_integer: :out_of_range, long_integer: :out_of_range] } },
+    {"x", -1, {:integer_value, -1, [short_integer: :out_of_range, long_integer: :out_of_range] }},
   ]
 end
 
