2b7b8b1b800d52c58b0bc96c7fc75d1706c1725b
Declan Whelan <dwhelan@dpwhelan.com>
2019-05-19 17:07:54 -0400

    Simplify Either

--- lib/either.ex
+++ lib/either.ex
@@ -1,42 +1,29 @@
 defmodule MMS.Either do
-  import OkError
-  import OkError.Operators
-  import MMS.Codec
-
   defmacro defcodec either: codecs do
-    data_type = data_type __CALLER__.module
     quote do
       use MMS.Codec
 
       def decode bytes do
         bytes
-        |> decode(unquote(codecs), unquote(data_type))
+        |> continue_until_ok(unquote(codecs), :decode)
       end
 
       def encode value do
         value
-        |> encode(unquote(codecs), unquote(data_type))
+        |> continue_until_ok(unquote(codecs), :encode)
+      end
+
+      defp continue_until_ok input, codecs, function_name do
+        codecs
+        |> Enum.reduce_while(error([]),
+             fn codec, {:error, errors} ->
+               case apply codec, function_name, [input] do
+                 {:ok, result} -> {:halt, ok result}
+                 {:error, {data_type, _, details}} -> {:cont, error [{data_type, details} | errors]}
+               end
+             end)
+        ~>> fn errors -> error input, Enum.reverse errors end
       end
     end
-  end
-
-  def decode(bytes, codecs, data_type) when is_binary(bytes) and is_list(codecs) and is_atom(data_type) do
-    continue_until_ok bytes, codecs, data_type, :decode
-  end
-
-  def encode(value, codecs, data_type) when is_list(codecs) and is_atom(data_type) do
-    continue_until_ok value, codecs, data_type, :encode
-  end
-
-  defp continue_until_ok input, codecs, data_type, function_name do
-    codecs
-    |> Enum.reduce_while(error([]),
-         fn codec, {:error, errors} ->
-           case apply codec, function_name, [input] do
-             {:ok, result} -> {:halt, ok result}
-             {:error, {codec_data_type, _, details}} -> {:cont, error [{codec_data_type, details} | errors]}
-           end
-         end)
-    ~>> fn errors -> error data_type, input, Enum.reverse errors end
   end
 end
--- test/either_test.exs
+++ test/either_test.exs
@@ -1,4 +1,4 @@
-defmodule MMS.OrTest do
+defmodule MMS.EitherTest do
   use MMS.CodecTest
   import MMS.Either
 
@@ -22,48 +22,48 @@ defmodule MMS.OrTest do
     def encode(value), do: error {:error2, value, 2}
   end
 
-  describe "decode/3 should" do
-    test "return ok from the first codec that returns ok" do
-      assert decode(<<0>>, [Ok1], :data_type) == ok(1, "")
-      assert decode(<<0>>, [Ok1, Error1], :data_type) == ok(1, "")
-      assert decode(<<0>>, [Ok1, Ok2, Error1], :data_type) == ok(1, "")
-
-      assert decode(<<0>>, [Error1, Ok1], :data_type) == ok(1, "")
-      assert decode(<<0>>, [Error1, Error2, Ok2, Ok1], :data_type) == ok(2, "")
-    end
-
-    test "return error if all codecs fail" do
-      assert decode(<<0>>, [Error1], :data_type) == error(:data_type, <<0>>, [error1: 1])
-      assert decode(<<0>>, [Error1, Error2], :data_type) == error(:data_type, <<0>>, [error1: 1, error2: 2])
-    end
-
-    test "should have guard clauses for parameter types" do
-      assert_function_clause_error "MMS.Either.decode(:not_binary, [], :data_type)"
-      assert_function_clause_error "MMS.Either.decode(\"\", :not_a_list, :data_type)"
-      assert_function_clause_error "MMS.Either.decode(\"\", [], 'not an atom')"
-    end
-  end
-
-  describe "encode should" do
-    test "return ok from the first codec that returns ok" do
-      assert encode(0, [Ok1], :data_type) == ok <<1>>
-      assert encode(0, [Ok1, Error1], :data_type) == ok <<1>>
-      assert encode(0, [Ok1, Ok2, Error1], :data_type) == ok <<1>>
-
-      assert encode(0, [Error1, Ok1], :data_type) == ok <<1>>
-      assert encode(0, [Error1, Error2, Ok2, Ok1], :data_type) == ok <<2>>
-    end
-
-    test "return error if all codecs fail" do
-      assert encode(0, [Error1], :data_type) == error {:data_type, 0, [error1: 1]}
-      assert encode(0, [Error1, Error2], :data_type) == error {:data_type, 0, [error1: 1, error2: 2]}
-    end
-
-    test "should have guard clauses for parameter types" do
-      assert_function_clause_error "MMS.Either.encode(\"\", :not_a_list, :data_type)"
-      assert_function_clause_error "MMS.Either.encode(\"\", [], 'not an atom')"
-    end
-  end
+#  describe "decode/3 should" do
+#    test "return ok from the first codec that returns ok" do
+#      assert decode(<<0>>, [Ok1], :data_type) == ok(1, "")
+#      assert decode(<<0>>, [Ok1, Error1], :data_type) == ok(1, "")
+#      assert decode(<<0>>, [Ok1, Ok2, Error1], :data_type) == ok(1, "")
+#
+#      assert decode(<<0>>, [Error1, Ok1], :data_type) == ok(1, "")
+#      assert decode(<<0>>, [Error1, Error2, Ok2, Ok1], :data_type) == ok(2, "")
+#    end
+#
+#    test "return error if all codecs fail" do
+#      assert decode(<<0>>, [Error1], :data_type) == error(:data_type, <<0>>, [error1: 1])
+#      assert decode(<<0>>, [Error1, Error2], :data_type) == error(:data_type, <<0>>, [error1: 1, error2: 2])
+#    end
+#
+#    test "should have guard clauses for parameter types" do
+#      assert_function_clause_error "MMS.Either.decode(:not_binary, [], :data_type)"
+#      assert_function_clause_error "MMS.Either.decode(\"\", :not_a_list, :data_type)"
+#      assert_function_clause_error "MMS.Either.decode(\"\", [], 'not an atom')"
+#    end
+#  end
+#
+#  describe "encode should" do
+#    test "return ok from the first codec that returns ok" do
+#      assert encode(0, [Ok1], :data_type) == ok <<1>>
+#      assert encode(0, [Ok1, Error1], :data_type) == ok <<1>>
+#      assert encode(0, [Ok1, Ok2, Error1], :data_type) == ok <<1>>
+#
+#      assert encode(0, [Error1, Ok1], :data_type) == ok <<1>>
+#      assert encode(0, [Error1, Error2, Ok2, Ok1], :data_type) == ok <<2>>
+#    end
+#
+#    test "return error if all codecs fail" do
+#      assert encode(0, [Error1], :data_type) == error {:data_type, 0, [error1: 1]}
+#      assert encode(0, [Error1, Error2], :data_type) == error {:data_type, 0, [error1: 1, error2: 2]}
+#    end
+#
+#    test "should have guard clauses for parameter types" do
+#      assert_function_clause_error "MMS.Either.encode(\"\", :not_a_list, :data_type)"
+#      assert_function_clause_error "MMS.Either.encode(\"\", [], 'not an atom')"
+#    end
+#  end
 
   describe "using" do
     defmodule Ok1Error1 do
@@ -81,6 +81,7 @@ defmodule MMS.OrTest do
     test "return error if all codecs fail  to decode" do
       assert Error1Error2.decode(<<0>>) == error(:error1_error2, <<0>>, error1: 1, error2: 2)
     end
+
     test "return ok from the first codec that encodes ok" do
       assert Ok1Error1.encode(0) == ok <<1>>
     end
