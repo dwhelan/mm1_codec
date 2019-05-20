3d1f1eb4b4b48bde860d6e19873dbce0a05b1218
Declan Whelan <dwhelan@dpwhelan.com>
2019-05-20 01:47:03 -0400

    Extract Tuple methods from __using__

--- lib/tuple.ex
+++ lib/tuple.ex
@@ -1,19 +1,33 @@
 defmodule MMS.Tuple do
+  import MMS.Codec
+  import MMS.As
+
+  def decode bytes, codecs do
+    bytes
+    |> MMS.List.decode(codecs)
+    |> map_value(&List.to_tuple/1)
+  end
+
+  def encode value, codecs do
+    value
+    |> Tuple.to_list
+    |> MMS.List.encode(codecs)
+  end
+
   defmacro defcodec(as: codecs) do
     quote do
       use MMS.Codec
+      import MMS.Tuple
 
       def decode bytes do
         bytes
-        |> MMS.List.decode(unquote codecs)
-        |> map_value(&List.to_tuple/1)
+        |> decode(unquote codecs)
         ~>> fn {_, _, reason} -> error bytes, reason end
       end
 
       def encode(value) when is_tuple(value) do
         value
-        |> Tuple.to_list
-        |> MMS.List.encode(unquote codecs)
+        |> encode(unquote codecs)
         ~>> fn {_, _, reason} -> error value, reason end
       end
 
