a0d0a6909829a7edd4a01d5f2fb18e3b2863f760
Declan Whelan <dwhelan@dpwhelan.com>
2019-05-19 22:35:58 -0400

    Add errors tests for UntypedParameter

--- lib/tuple.ex
+++ lib/tuple.ex
@@ -1,5 +1,4 @@
 defmodule MMS.Tuple do
-
   defmacro defcodec(as: codecs) do
     quote do
       use MMS.Codec
@@ -11,11 +10,15 @@ defmodule MMS.Tuple do
         ~>> fn {_, _, reason} -> error data_type(), bytes, reason end
       end
 
-      def encode value do
+      def encode(value) when is_tuple(value) do
         value
         |> Tuple.to_list
         |> MMS.List.encode(unquote codecs)
         ~>> fn {_, _, reason} -> error data_type(), value, reason end
+      end
+
+      def encode value do
+        error value, :out_of_range
       end
     end
   end
--- test/data_types/untyped_parameter_test.exs
+++ test/data_types/untyped_parameter_test.exs
@@ -1,17 +1,19 @@
 defmodule MMS.UntypedParameterTest do
   use MMS.CodecTest
+  import MMS.UntypedParameter
 
-  use MMS.TestExamples,
-      codec: MMS.UntypedParameter,
+  codec_examples [
+    {"text value",    <<"name\0", "value\0">>, {"name", "value"}},
+    {"integer value", <<"name\0", s(0)>>,      {"name", 0      }},
+  ]
 
-      examples: [
-        {<<"name\0", "value\0">>, {"name", "value"}},
-        {<<"name\0", s(0)>>,      {"name", 0      }},
-      ],
+  decode_errors [
+    {"token text error",    <<"name", "value\0">>},
+    {"untyped value error", <<"name\0", "value">>},
+  ]
 
-      decode_errors: [
-      ],
-
-      encode_errors: [
-      ]
+  encode_errors [
+    {"token text error",    {:bad_name, "value"}},
+    {"untyped value error", {"name", :bad_value}},
+  ]
 end
